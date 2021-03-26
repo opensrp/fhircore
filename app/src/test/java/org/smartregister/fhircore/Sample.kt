a207ede6afa9d1babef93b03697f04e0c6b0c1c5
Ronald K <43806892+rkodev@users.noreply.github.com>
2021-03-26 13:29:25 +0300

    Execute ci by github actions. Add code coverage and unit tests report (#59)
    * Execute ci by github actions. Add code coverage and unit tests report
    
    * Merge jacoco dependecies
    
    * Change master branch to main
    
    * Update env username and token
    
    * Add env tokens to ci file

---
+++ app/src/test/java/org/smartregister/fhircore/Sample.kt
@@ -0,0 +1,13 @@
+package org.smartregister.fhircore
+
+import org.junit.Assert
+import org.junit.Test
+
+class Sample {
+
+    @Test
+    fun helloTest(){
+        Assert.assertEquals(1,1)
+    }
+
+}
