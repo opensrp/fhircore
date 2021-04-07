a86739af3699f78c03060cec425adbf18028f044
Vincent Karuri <vincentmkaruri@gmail.com>
2021-04-07 11:35:30 +0300

    Fix styling issues in files

--- app/src/main/java/org/smartregister/fhircore/activity/PatientListActivity.kt
+++ app/src/main/java/org/smartregister/fhircore/activity/PatientListActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Ona Systems Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,102 +31,104 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.fhir.FhirEngine
 import com.google.android.material.snackbar.Snackbar
-import org.smartregister.fhircore.*
+import org.smartregister.fhircore.FhirApplication
+import org.smartregister.fhircore.PatientListViewModel
+import org.smartregister.fhircore.PatientListViewModelFactory
+import org.smartregister.fhircore.R
 import org.smartregister.fhircore.adapter.PatientItemRecyclerViewAdapter
 import org.smartregister.fhircore.fragment.PatientDetailFragment
 
 /** An activity representing a list of Patients. */
 class PatientListActivity : AppCompatActivity() {
-    private lateinit var fhirEngine: FhirEngine
-    private lateinit var patientListViewModel: PatientListViewModel
+  private lateinit var fhirEngine: FhirEngine
+  private lateinit var patientListViewModel: PatientListViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d("PatientListActivity", "onCreate() called")
-        setContentView(R.layout.activity_patient_list)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Log.d("PatientListActivity", "onCreate() called")
+    setContentView(R.layout.activity_patient_list)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        toolbar.title = title
+    val toolbar = findViewById<Toolbar>(R.id.toolbar)
+    setSupportActionBar(toolbar)
+    toolbar.title = title
 
-        fhirEngine = FhirApplication.fhirEngine(this)
+    fhirEngine = FhirApplication.fhirEngine(this)
 
-        patientListViewModel = ViewModelProvider(this, PatientListViewModelFactory(
-            this.application, fhirEngine
-        )).get(PatientListViewModel::class.java)
-        val recyclerView: RecyclerView = findViewById(R.id.patient_list)
+    patientListViewModel =
+      ViewModelProvider(this, PatientListViewModelFactory(this.application, fhirEngine))
+        .get(PatientListViewModel::class.java)
+    val recyclerView: RecyclerView = findViewById(R.id.patient_list)
 
-        val adapter = PatientItemRecyclerViewAdapter(this::onPatientItemClicked)
-        recyclerView.adapter = adapter
+    val adapter = PatientItemRecyclerViewAdapter(this::onPatientItemClicked)
+    recyclerView.adapter = adapter
 
-        patientListViewModel.getSearchedPatients().observe(this,
-            {
-                Log.d("PatientListActivity", "Submitting ${it.count()} patient records")
-                adapter.submitList(it)
-            }
-        )
-    }
-
-    // Click handler to help display the details about the patients from the list.
-    private fun onPatientItemClicked(patientItem: PatientListViewModel.PatientItem) {
-        val intent = Intent(this.applicationContext,
-            PatientDetailActivity::class.java).apply {
-            putExtra(PatientDetailFragment.ARG_ITEM_ID, patientItem.id)
+    patientListViewModel
+      .getSearchedPatients()
+      .observe(
+        this,
+        {
+          Log.d("PatientListActivity", "Submitting ${it.count()} patient records")
+          adapter.submitList(it)
         }
-        this.startActivity(intent)
-    }
+      )
+  }
 
-    // To suppress the warning. Seems to be an issue with androidx library.
-    // "MenuBuilder.setOptionalIconsVisible can only be called from within the same library group
-    // prefix (referenced groupId=androidx.appcompat with prefix androidx from groupId=fhir-engine"
-    @SuppressLint("RestrictedApi")
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.list_options_menu, menu)
-        // To ensure that icons show up in the overflow options menu. Icons go missing without this.
-        if (menu is MenuBuilder) {
-            menu.setOptionalIconsVisible(true)
-        }
-        return true
-    }
+  // Click handler to help display the details about the patients from the list.
+  private fun onPatientItemClicked(patientItem: PatientListViewModel.PatientItem) {
+    val intent =
+      Intent(this.applicationContext, PatientDetailActivity::class.java).apply {
+        putExtra(PatientDetailFragment.ARG_ITEM_ID, patientItem.id)
+      }
+    this.startActivity(intent)
+  }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val view: View = findViewById(R.id.app_bar)
-
-        // Handle item selection
-        return when (item.itemId) {
-            R.id.sync_resources -> {
-                syncResources(view)
-                true
-            }
-            R.id.add_patient -> {
-                Snackbar.make(view, "Add Patient", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-                addPatient(view)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+  // To suppress the warning. Seems to be an issue with androidx library.
+  // "MenuBuilder.setOptionalIconsVisible can only be called from within the same library group
+  // prefix (referenced groupId=androidx.appcompat with prefix androidx from groupId=fhir-engine"
+  @SuppressLint("RestrictedApi")
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    val inflater: MenuInflater = menuInflater
+    inflater.inflate(R.menu.list_options_menu, menu)
+    // To ensure that icons show up in the overflow options menu. Icons go missing without this.
+    if (menu is MenuBuilder) {
+      menu.setOptionalIconsVisible(true)
     }
+    return true
+  }
 
-    private fun syncResources(view: View) {
-        Snackbar.make(view, "Getting Patients List", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show()
-        patientListViewModel.searchPatients()
-    }
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    val view: View = findViewById(R.id.app_bar)
 
-    private fun addPatient(view: View) {
-        // TO DO: Open patient registration form
-        val context = view.context
-        context.startActivity(Intent(context, QuestionnaireActivity::class.java).apply {
-            putExtra(
-                    QuestionnaireActivity.QUESTIONNAIRE_TITLE_KEY,
-                    "Patient registration"
-            )
-            putExtra(
-                    QuestionnaireActivity.QUESTIONNAIRE_FILE_PATH_KEY,
-                    "patient-registration.json"
-            )
-        })
+    // Handle item selection
+    return when (item.itemId) {
+      R.id.sync_resources -> {
+        syncResources(view)
+        true
+      }
+      R.id.add_patient -> {
+        Snackbar.make(view, "Add Patient", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        addPatient(view)
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
     }
+  }
+
+  private fun syncResources(view: View) {
+    Snackbar.make(view, "Getting Patients List", Snackbar.LENGTH_LONG)
+      .setAction("Action", null)
+      .show()
+    patientListViewModel.searchPatients()
+  }
+
+  private fun addPatient(view: View) {
+    // TO DO: Open patient registration form
+    val context = view.context
+    context.startActivity(
+      Intent(context, QuestionnaireActivity::class.java).apply {
+        putExtra(QuestionnaireActivity.QUESTIONNAIRE_TITLE_KEY, "Patient registration")
+        putExtra(QuestionnaireActivity.QUESTIONNAIRE_FILE_PATH_KEY, "patient-registration.json")
+      }
+    )
+  }
 }
