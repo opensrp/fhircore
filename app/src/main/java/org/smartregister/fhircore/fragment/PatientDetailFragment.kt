0eef2e0046ed202eadc7f2892aa5c59d2333abee
Vincent Karuri <vincentmkaruri@gmail.com>
2021-04-07 11:35:30 +0300

    Fix styling issues in files

--- app/src/main/java/org/smartregister/fhircore/fragment/PatientDetailFragment.kt
+++ app/src/main/java/org/smartregister/fhircore/fragment/PatientDetailFragment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Ona Systems Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,71 +28,73 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.fhir.FhirEngine
 import com.google.android.material.appbar.CollapsingToolbarLayout
-import org.smartregister.fhircore.*
+import org.smartregister.fhircore.FhirApplication
+import org.smartregister.fhircore.PatientListViewModel
+import org.smartregister.fhircore.PatientListViewModelFactory
+import org.smartregister.fhircore.R
 import org.smartregister.fhircore.adapter.ObservationItemRecyclerViewAdapter
 
 /**
- * A fragment representing a single Patient detail screen.
- * This fragment is contained in a [PatientDetailActivity].
+ * A fragment representing a single Patient detail screen. This fragment is contained in a
+ * [PatientDetailActivity].
  */
 class PatientDetailFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val rootView = inflater.inflate(R.layout.patient_detail, container, false)
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    val rootView = inflater.inflate(R.layout.patient_detail, container, false)
 
-        val recyclerView: RecyclerView = rootView.findViewById(R.id.observation_list)
-        val adapter = ObservationItemRecyclerViewAdapter()
-        recyclerView.adapter = adapter
+    val recyclerView: RecyclerView = rootView.findViewById(R.id.observation_list)
+    val adapter = ObservationItemRecyclerViewAdapter()
+    recyclerView.adapter = adapter
 
-        val fhirEngine: FhirEngine = FhirApplication.fhirEngine(requireContext())
-        var patient: PatientListViewModel.PatientItem? = null
+    val fhirEngine: FhirEngine = FhirApplication.fhirEngine(requireContext())
+    var patient: PatientListViewModel.PatientItem? = null
 
-        val viewModel: PatientListViewModel = ViewModelProvider(this, PatientListViewModelFactory(
-            this.requireActivity().application, fhirEngine
-        ))
-            .get(PatientListViewModel::class.java)
+    val viewModel: PatientListViewModel =
+      ViewModelProvider(
+          this,
+          PatientListViewModelFactory(this.requireActivity().application, fhirEngine)
+        )
+        .get(PatientListViewModel::class.java)
 
-        viewModel.getObservations().observe(viewLifecycleOwner,
-            Observer<List<PatientListViewModel.ObservationItem>> {
-                adapter.submitList(it)
-            })
+    viewModel
+      .getObservations()
+      .observe(
+        viewLifecycleOwner,
+        Observer<List<PatientListViewModel.ObservationItem>> { adapter.submitList(it) }
+      )
 
-        arguments?.let {
-            if (it.containsKey(ARG_ITEM_ID)) {
-                patient = it.getString(ARG_ITEM_ID)?.let {
-                        patient_index -> viewModel.getPatientItem(patient_index)
-                }
-                activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title =
-                    patient?.name
-            }
-        }
-
-        setupPatientData(rootView, patient)
-
-        return rootView
+    arguments?.let {
+      if (it.containsKey(ARG_ITEM_ID)) {
+        patient =
+          it.getString(ARG_ITEM_ID)?.let { patient_index ->
+            viewModel.getPatientItem(patient_index)
+          }
+        activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = patient?.name
+      }
     }
 
-    private fun setupPatientData(view: View, patient: PatientListViewModel.PatientItem?) {
-        if (patient != null) {
-            view.findViewById<TextView>(R.id.patient_detail).text = HtmlCompat.fromHtml(
-                patient.html,
-                HtmlCompat.FROM_HTML_MODE_LEGACY
-            )
-            view.findViewById<TextView>(R.id.name).text = patient.name
-            view.findViewById<TextView>(R.id.dob).text = patient.dob
-            view.findViewById<TextView>(R.id.gender).text = patient.phone
-        }
-    }
+    setupPatientData(rootView, patient)
 
-    companion object {
-        /**
-         * The fragment argument representing the patient item ID that this fragment
-         * represents.
-         */
-        const val ARG_ITEM_ID = "patient_item_id"
+    return rootView
+  }
+
+  private fun setupPatientData(view: View, patient: PatientListViewModel.PatientItem?) {
+    if (patient != null) {
+      view.findViewById<TextView>(R.id.patient_detail).text =
+        HtmlCompat.fromHtml(patient.html, HtmlCompat.FROM_HTML_MODE_LEGACY)
+      view.findViewById<TextView>(R.id.name).text = patient.name
+      view.findViewById<TextView>(R.id.dob).text = patient.dob
+      view.findViewById<TextView>(R.id.gender).text = patient.phone
     }
+  }
+
+  companion object {
+    /** The fragment argument representing the patient item ID that this fragment represents. */
+    const val ARG_ITEM_ID = "patient_item_id"
+  }
 }
