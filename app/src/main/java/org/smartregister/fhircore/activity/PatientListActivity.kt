3866748814bb37a7e8242a2a4dde014e844730bc
Vincent Karuri <vincentmkaruri@gmail.com>
2021-04-27 16:42:07 +0300

    Add edit icons after typing in search field

--- app/src/main/java/org/smartregister/fhircore/activity/PatientListActivity.kt
+++ app/src/main/java/org/smartregister/fhircore/activity/PatientListActivity.kt
@@ -19,12 +19,15 @@ package org.smartregister.fhircore.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.core.widget.doAfterTextChanged
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -52,7 +55,51 @@ class PatientListActivity : AppCompatActivity() {
 
     findViewById<ViewPager2>(R.id.patient_list_pager).adapter = PatientListPagerAdapter(this)
 
+    var editText = findViewById<EditText>(R.id.edit_text_search)
+    editText.doAfterTextChanged {
+      if (it!!.isEmpty()) {
+        editText.setOnTouchListener(null)
+        editText.setCompoundDrawablesWithIntrinsicBounds(getDrawable(R.drawable.ic_search), null, null, null)
+      } else {
+        editText.setCompoundDrawablesWithIntrinsicBounds(getDrawable(R.drawable.ic_back_arrow), null, getDrawable(R.drawable.ic_cancel), null)
+        editText.addOnDrawableClickedListener(DrawablePosition.DRAWABLE_LEFT) {}
+        editText.addOnDrawableClickedListener(DrawablePosition.DRAWABLE_RIGHT) { it.clear() }
+      }
+    }
+
     setupDrawerContent()
+  }
+
+  fun EditText.addOnDrawableClickedListener(drawablePosition: DrawablePosition, onClicked: ()-> Unit) {
+      this.setOnTouchListener(
+        object : View.OnTouchListener {
+
+          override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+            if (event!!.action == MotionEvent.ACTION_UP && isDrawableClicked(drawablePosition, event, v as EditText)) {
+                onClicked()
+                return true;
+            }
+            return false;
+          }
+        }
+      )
+  }
+
+  private fun isDrawableClicked(drawablePosition: DrawablePosition, event: MotionEvent?, view: EditText): Boolean {
+    return when (drawablePosition) {
+      DrawablePosition.DRAWABLE_RIGHT -> event!!.rawX >= (view.right - view.compoundDrawables[drawablePosition.position].bounds.width())
+      DrawablePosition.DRAWABLE_LEFT -> event!!.rawX <= (view.compoundDrawables[drawablePosition.position].bounds.width())
+      else -> {
+        return false
+      }
+    }
+  }
+
+  enum class DrawablePosition (val position: Int) {
+    DRAWABLE_LEFT(0),
+    DRAWABLE_TOP(1),
+    DRAWABLE_RIGHT(2),
+    DRAWABLE_BOTTOM(3)
   }
 
   private fun setupDrawerContent() {
