16473264ff6871c4685ef2c83c9c93566d34dd3f
Vincent Karuri <vincentmkaruri@gmail.com>
2021-04-27 16:56:14 +0300

    Code clean-up

--- app/src/main/java/org/smartregister/fhircore/fragment/PatientListFragment.kt
+++ app/src/main/java/org/smartregister/fhircore/fragment/PatientListFragment.kt
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2021 Ona Systems Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.smartregister.fhircore.fragment
 
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +26,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.fhir.FhirEngine
@@ -23,54 +39,62 @@ import org.smartregister.fhircore.adapter.PatientItemRecyclerViewAdapter
 
 class PatientListFragment : Fragment() {
 
-    private lateinit var patientListViewModel: PatientListViewModel
-    private lateinit var fhirEngine: FhirEngine
+  private lateinit var patientListViewModel: PatientListViewModel
+  private lateinit var fhirEngine: FhirEngine
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_patient_list, container, false)
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.fragment_patient_list, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    fhirEngine = FhirApplication.fhirEngine(requireContext())
+
+    val recyclerView = view.findViewById<RecyclerView>(R.id.patient_list)
+    val adapter = PatientItemRecyclerViewAdapter(this::onPatientItemClicked)
+    recyclerView.adapter = adapter
+
+    requireActivity().findViewById<TextView>(R.id.tv_sync).setOnClickListener {
+      requireActivity()
+        .findViewById<DrawerLayout>(R.id.drawer_layout)
+        .closeDrawer(GravityCompat.START)
+      syncResources()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fhirEngine = FhirApplication.fhirEngine(requireContext())
+    patientListViewModel =
+      ViewModelProvider(
+          this,
+          PatientListViewModelFactory(requireActivity().application, fhirEngine)
+        )
+        .get(PatientListViewModel::class.java)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.patient_list)
-        val adapter = PatientItemRecyclerViewAdapter(this::onPatientItemClicked)
-        recyclerView.adapter = adapter
-
-        requireActivity().findViewById<TextView>(R.id.tv_sync).setOnClickListener {
-            requireActivity().findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
-            syncResources()
+    patientListViewModel
+      .getSearchedPatients()
+      .observe(
+        viewLifecycleOwner,
+        {
+          Log.d("PatientListActivity", "Submitting ${it.count()} patient records")
+          adapter.submitList(it)
         }
+      )
 
-        patientListViewModel =
-                ViewModelProvider(this, PatientListViewModelFactory(requireActivity().application, fhirEngine))
-                        .get(PatientListViewModel::class.java)
+    super.onViewCreated(view, savedInstanceState)
+  }
 
-        patientListViewModel
-                .getSearchedPatients()
-                .observe(
-                        viewLifecycleOwner,
-                        {
-                            Log.d("PatientListActivity", "Submitting ${it.count()} patient records")
-                            adapter.submitList(it)
-                        }
-                )
+  // Click handler to help display the details about the patients from the list.
+  private fun onPatientItemClicked(patientItem: PatientListViewModel.PatientItem) {
+    val intent =
+      Intent(requireActivity(), PatientDetailActivity::class.java).apply {
+        putExtra(PatientDetailFragment.ARG_ITEM_ID, patientItem.id)
+      }
+    this.startActivity(intent)
+  }
 
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    // Click handler to help display the details about the patients from the list.
-    private fun onPatientItemClicked(patientItem: PatientListViewModel.PatientItem) {
-        val intent =
-                Intent(requireActivity(), PatientDetailActivity::class.java).apply {
-                    putExtra(PatientDetailFragment.ARG_ITEM_ID, patientItem.id)
-                }
-        this.startActivity(intent)
-    }
-
-    private fun syncResources() {
-        Toast.makeText(requireContext(), "Syncing...", Toast.LENGTH_LONG).show()
-        patientListViewModel.searchPatients()
-    }
+  private fun syncResources() {
+    Toast.makeText(requireContext(), "Syncing...", Toast.LENGTH_LONG).show()
+    patientListViewModel.searchPatients()
+  }
 }
