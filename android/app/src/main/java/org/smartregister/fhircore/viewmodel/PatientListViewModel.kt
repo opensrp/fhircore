3d9da5ef5dec535dec682a3d677382f8bac200c2
maimoona.kausar <maimoona.kausar@venturedive.com>
2021-05-28 12:43:58 +0500

    Merging latest main branch

--- android/app/src/main/java/org/smartregister/fhircore/viewmodel/PatientListViewModel.kt
+++ android/app/src/main/java/org/smartregister/fhircore/viewmodel/PatientListViewModel.kt
@@ -17,7 +17,12 @@
 package org.smartregister.fhircore.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import ca.uhn.fhir.rest.param.ParamPrefixEnum
 import com.google.android.fhir.FhirEngine
 import com.google.android.fhir.search.Order
