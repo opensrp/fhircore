e6d6c308cb10f538bbd9e5b713d02d557e3e6b50
Owais <62104757+syedowaisali90@users.noreply.github.com>
2021-06-19 03:52:15 +0500

    Current pagination buttons not at a static position (at the bottom of the page ) on the patient list view (#269)
    * fixed pagination button position to static position
    
    * changelog updated
    
    * use ternary style
    
    Co-authored-by: pld <pld@users.noreply.github.com>

--- android/app/src/main/java/org/smartregister/fhircore/adapter/PatientItemRecyclerViewAdapter.kt
+++ android/app/src/main/java/org/smartregister/fhircore/adapter/PatientItemRecyclerViewAdapter.kt
@@ -20,20 +20,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import org.smartregister.fhircore.R
-import org.smartregister.fhircore.domain.Pagination
-import org.smartregister.fhircore.fragment.NavigationDirection
-import org.smartregister.fhircore.viewholder.PaginationViewHolder
 import org.smartregister.fhircore.viewholder.PatientItemViewHolder
 import org.smartregister.fhircore.viewmodel.PatientListViewModel
 
 /** UI Controller helper class to monitor Patient viewmodel and display list of patients. */
 class PatientItemRecyclerViewAdapter(
   private val onItemClicked: (PatientListViewModel.PatientItem) -> Unit,
-  private val paginationListener: (NavigationDirection, Int) -> Unit,
   private val onRecordVaccineClicked: (PatientListViewModel.PatientItem) -> Unit
-) : ListAdapter<Any, RecyclerView.ViewHolder>(PatientItemDiffCallback()) {
+) : ListAdapter<Any, PatientItemViewHolder>(PatientItemDiffCallback()) {
 
   class PatientItemDiffCallback : DiffUtil.ItemCallback<Any>() {
     override fun areItemsTheSame(oldItem: Any, newItem: Any): Boolean =
@@ -47,33 +42,15 @@ class PatientItemRecyclerViewAdapter(
         oldItem.id == newItem.id
   }
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-    return if (viewType == 1) {
-      PaginationViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.list_pagination, parent, false)
-      )
-    } else {
-      PatientItemViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.patient_list_item, parent, false)
-      )
-    }
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PatientItemViewHolder {
+    return PatientItemViewHolder(
+      LayoutInflater.from(parent.context).inflate(R.layout.patient_list_item, parent, false)
+    )
   }
 
-  override fun getItemViewType(position: Int): Int {
-    return if (position == (currentList.size - 1)) 1 else 0
-  }
-
-  override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+  override fun onBindViewHolder(holder: PatientItemViewHolder, position: Int) {
     val item = currentList[position]
 
-    when (holder) {
-      is PatientItemViewHolder ->
-        holder.bindTo(
-          item as PatientListViewModel.PatientItem,
-          onItemClicked,
-          onRecordVaccineClicked
-        )
-      is PaginationViewHolder -> holder.bindTo(item as Pagination, paginationListener)
-    }
+    holder.bindTo(item as PatientListViewModel.PatientItem, onItemClicked, onRecordVaccineClicked)
   }
 }
