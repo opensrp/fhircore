37ec11f4eb07b2e631b95545b525e673bc8afaa9
syedowaisali <owais.ali@venturedive.com>
2021-07-02 15:51:23 +0500

    add util and viewmodel test cases

---
+++ android/app/src/test/java/org/smartregister/fhircore/viewmodel/LoginViewModelTest.kt
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.viewmodel
+
+import android.accounts.AccountManager
+import android.accounts.AccountManagerFuture
+import android.os.Bundle
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.spyk
+import org.junit.Assert
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import org.smartregister.fhircore.FhirApplication
+import org.smartregister.fhircore.RobolectricTest
+import org.smartregister.fhircore.auth.OAuthResponse
+import org.smartregister.fhircore.auth.account.AccountHelper
+import org.smartregister.fhircore.auth.secure.Credentials
+import org.smartregister.fhircore.auth.secure.FakeKeyStore
+import retrofit2.Call
+import retrofit2.Response
+
+class LoginViewModelTest : RobolectricTest() {
+
+  private lateinit var viewModel: LoginViewModel
+
+  @Before
+  fun setUp() {
+    viewModel = LoginViewModel(FhirApplication.getContext())
+  }
+
+  @Test
+  fun testShouldEnableLoginShouldReturnTrue() {
+    setUsernameAndPassword()
+    Assert.assertTrue(viewModel.shouldEnableLogin())
+  }
+
+  @Test
+  fun testAllowLoginShouldBeTrue() {
+
+    setUsernameAndPassword()
+
+    viewModel.credentialsWatcher.afterTextChanged(mockk())
+    Assert.assertTrue(viewModel.allowLogin.value!!)
+  }
+
+  @Test
+  fun testOnResponseShouldVerifyInternalCalls() {
+
+    val call = mockk<Call<OAuthResponse>>()
+    val response = mockk<Response<OAuthResponse>>(relaxed = true)
+    val body = mockk<OAuthResponse>(relaxed = true)
+
+    every { response.isSuccessful } returns false
+    every { response.errorBody() } returns mockk()
+
+    setUsernameAndPassword()
+    viewModel.secureConfig.saveCredentials(Credentials("testuser", charArrayOf('a'), "dummy_token"))
+    viewModel.goHome.value = false
+    viewModel.onResponse(call, response)
+
+    Assert.assertTrue(viewModel.loginFailed.value!!)
+    Assert.assertTrue(viewModel.allowLogin.value!!)
+    Assert.assertTrue(viewModel.goHome.value!!)
+
+    every { response.isSuccessful } returns true
+    every {
+      hint(OAuthResponse::class)
+      response.body()
+    } returns body
+    every { body.accessToken } returns "dummy_token"
+
+    val field = viewModel.javaClass.getDeclaredField("accountManager")
+    field.isAccessible = true
+    val accountManager = spyk(field.get(viewModel) as AccountManager)
+    field.set(viewModel, accountManager)
+
+    every { accountManager.notifyAccountAuthenticated(any()) } returns true
+
+    viewModel.goHome.value = false
+    viewModel.onResponse(call, response)
+
+    Assert.assertFalse(viewModel.loginFailed.value!!)
+    Assert.assertEquals(0, viewModel.loginUser.password.size)
+    Assert.assertTrue(viewModel.goHome.value!!)
+  }
+
+  @Test
+  fun testOnFailureShouldVerifyInternalCalls() {
+
+    mockkStatic(Throwable::stackTraceToString)
+
+    val call = mockk<Call<OAuthResponse>>()
+    val t = mockk<Throwable>()
+
+    every { t.stackTraceToString() } returns ""
+
+    setUsernameAndPassword()
+    viewModel.secureConfig.saveCredentials(Credentials("testuser", charArrayOf('a'), "dummy_token"))
+    viewModel.goHome.value = false
+    viewModel.allowLogin.value = false
+    viewModel.onFailure(call, t)
+
+    Assert.assertTrue(viewModel.goHome.value!!)
+
+    viewModel.secureConfig.deleteCredentials()
+    viewModel.onFailure(call, t)
+
+    Assert.assertTrue(viewModel.allowLogin.value!!)
+  }
+
+  @Test
+  fun testRunShouldSetGotoHomeValueTrue() {
+    val future = mockk<AccountManagerFuture<Bundle>>()
+    val bundle = Bundle()
+
+    bundle.putString(AccountManager.KEY_AUTHTOKEN, "dummy_authtoken")
+    viewModel.goHome.value = false
+
+    val field = viewModel.javaClass.getDeclaredField("accountHelper")
+    field.isAccessible = true
+    val accountHelper = spyk(field.get(viewModel) as AccountHelper)
+    field.set(viewModel, accountHelper)
+
+    every { accountHelper.isSessionActive(any()) } returns true
+    every {
+      hint(Bundle::class)
+      future.result
+    } returns bundle
+
+    viewModel.run(future)
+
+    Assert.assertTrue(viewModel.goHome.value!!)
+  }
+
+  private fun setUsernameAndPassword() {
+    viewModel.loginUser.username = "testuser"
+    viewModel.loginUser.password = charArrayOf('a')
+  }
+
+  companion object {
+    @JvmStatic
+    @BeforeClass
+    fun beforeClass() {
+      FakeKeyStore.setup
+    }
+  }
+}
