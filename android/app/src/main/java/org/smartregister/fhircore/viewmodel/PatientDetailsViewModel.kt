6ecf513c7172de4a1c2dfe564d7ca9c126231a63
Elly Kitoto <junkmailstoelly@gmail.com>
2021-08-05 13:13:03 +0300

    Refactor PatientDetailsViewModel to use DefaultDispatcherProvider

--- android/app/src/main/java/org/smartregister/fhircore/viewmodel/PatientDetailsViewModel.kt
+++ android/app/src/main/java/org/smartregister/fhircore/viewmodel/PatientDetailsViewModel.kt
@@ -5,12 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.fhir.FhirEngine
 import com.google.android.fhir.search.search
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.hl7.fhir.r4.model.Immunization
 import org.hl7.fhir.r4.model.Patient
+import org.smartregister.fhircore.util.DefaultDispatcherProvider
+import org.smartregister.fhircore.util.DispatcherProvider
 
-class PatientDetailsViewModel(val fhirEngine: FhirEngine, val patientId: String) : ViewModel() {
+class PatientDetailsViewModel(
+    var dispatcher: DispatcherProvider = DefaultDispatcherProvider,
+    val fhirEngine: FhirEngine,
+    val patientId: String
+) : ViewModel() {
 
     val patientDemographics = MutableLiveData<Patient>()
 
@@ -18,21 +23,19 @@ class PatientDetailsViewModel(val fhirEngine: FhirEngine, val patientId: String)
 
     fun fetchDemographics() {
         if (patientId.isNotEmpty())
-            viewModelScope.launch(Dispatchers.IO) {
-                patientDemographics.postValue(fhirEngine.load(Patient::class.java, patientId))
+            viewModelScope.launch(dispatcher.io()) {
+                val patient = fhirEngine.load(Patient::class.java, patientId)
+                patientDemographics.postValue(patient)
             }
     }
 
     fun fetchImmunizations() {
         if (patientId.isNotEmpty())
-            viewModelScope.launch(Dispatchers.IO) {
-                patientImmunizations.postValue(
-                    fhirEngine.search {
-                        filter(Immunization.PATIENT) {
-                            value = "Patient/$patientId"
-                        }
-                    }
-                )
+            viewModelScope.launch(dispatcher.io()) {
+                val immunizations: List<Immunization> = fhirEngine.search {
+                    filter(Immunization.PATIENT) { value = "Patient/$patientId" }
+                }
+                patientImmunizations.postValue(immunizations)
             }
     }
 }
