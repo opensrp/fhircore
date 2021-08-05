6634a907e8ac950ef567cb22e913b05fc41d95fd
Elly Kitoto <junkmailstoelly@gmail.com>
2021-08-05 13:11:06 +0300

    Implement CoroutineTestRule
    CoroutineTestRule allows provision of TestDispatcher so as to run
    coroutine tests on the thread running the tests.

---
+++ android/app/src/test/java/org/smartregister/fhircore/CoroutineTestRules.kt
@@ -0,0 +1,35 @@
+package org.smartregister.fhircore
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import org.smartregister.fhircore.util.DispatcherProvider
+
+@ExperimentalCoroutinesApi
+class CoroutineTestRule(val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) :
+    TestRule, TestCoroutineScope by TestCoroutineScope(testDispatcher) {
+
+    val testDispatcherProvider = object : DispatcherProvider {
+        override fun default() = testDispatcher
+        override fun io() = testDispatcher
+        override fun main() = testDispatcher
+        override fun unconfined() = testDispatcher
+    }
+
+    override fun apply(base: Statement?, description: Description?) =
+        object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                Dispatchers.setMain(testDispatcher)
+                base?.evaluate()
+                Dispatchers.resetMain()
+                testDispatcher.cleanupTestCoroutines()
+            }
+        }
+}
