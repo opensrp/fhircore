3e1f132060fcd370d876a66c0bb43a2b5584d339
Ephraim Kigamba <ekigamba@ona.io>
2021-08-17 18:17:12 +0300

    Implement patient search by WHO identifier
    - Fixes #481

--- android/app/src/main/java/org/smartregister/fhircore/util/Utils.kt
+++ android/app/src/main/java/org/smartregister/fhircore/util/Utils.kt
@@ -38,6 +38,7 @@ import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 import org.hl7.fhir.r4.model.DateTimeType
+import org.hl7.fhir.r4.model.Identifier
 import org.hl7.fhir.r4.model.Immunization
 import org.hl7.fhir.r4.model.Patient
 import org.hl7.fhir.r4.model.ResourceType
@@ -77,6 +78,18 @@ object Utils {
     search.filter(Patient.ADDRESS_CITY) {
       modifier = StringFilterModifier.CONTAINS
       value = "NAIROBI"
+    }
+  }
+
+  fun Search.addWHOIdentifierFilter(query: String?) {
+    if (query?.isNotBlank() == true) {
+      filter(
+        Patient.IDENTIFIER,
+        Identifier().apply {
+          value = query
+          system = "http://who.int/ddcc/hcid"
+        }
+      )
     }
   }
 
