99de842624995d9fada0bfe8217259a83dc0eab2
Elly Kitoto <junkmailstoelly@gmail.com>
2021-08-19 05:28:37 +0300

    Hide or show password on login page

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/login/LoginScreen.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/login/LoginScreen.kt
@@ -12,12 +12,20 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,6 +33,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,7 +70,7 @@ fun LoginPage(
   onLoginButtonClicked: () -> Unit,
   modifier: Modifier = Modifier,
 ) {
-
+  var showPassword by remember { mutableStateOf(false) }
   Column(modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp)) {
     Spacer(modifier = modifier.height(60.dp))
     Image(
@@ -103,9 +112,14 @@ fun LoginPage(
           modifier = modifier.padding(vertical = 4.dp)
         )
       },
-      visualTransformation = PasswordVisualTransformation(),
+      visualTransformation =
+        if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
       keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-      modifier = modifier.fillMaxWidth().padding(vertical = 4.dp)
+      modifier = modifier.fillMaxWidth().padding(vertical = 4.dp),
+      trailingIcon = {
+        val image = if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+        IconButton(onClick = { showPassword = !showPassword }) { Icon(imageVector = image, "") }
+      }
     )
     Spacer(modifier = modifier.height(40.dp))
     Button(
