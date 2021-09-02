0de98f4797a532bf7fdda9fbc18cd6a2d5d2bef8
Elly Kitoto <junkmailstoelly@gmail.com>
2021-09-02 13:19:58 +0300

    Make DomainMapper unidirectional
    We do not need to convert a business model to say a FHIR resource
    directly, the extraction is already provided via the FHIR SDK.

--- android/engine/src/main/java/org/smartregister/fhircore/engine/data/domain/util/DomainMapper.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/data/domain/util/DomainMapper.kt
@@ -1,9 +1,10 @@
 package org.smartregister.fhircore.engine.data.domain.util
 
-/** Mapper util for transforming model of type [T] to [DomainModel] and vice versa */
+/**
+ * Mapper util for transforming model of type [T] to [DomainModel]. An example is transforming FHIR
+ * resource to an entity class
+ */
 interface DomainMapper<T, DomainModel> {
 
   fun mapToDomainModel(dto: T): DomainModel
-
-  fun mapFromDomainModel(domainModel: DomainModel): T
 }
