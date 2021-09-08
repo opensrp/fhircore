80c491af7a4d4c198a343687bef8e8d1643793c6
kimigxfoxy <dkimigz@gmail.com>
2021-09-08 14:42:30 +0300

    Run ./gradlew spotlessApply and ./gradlew spotlessCheck

--- android/engine/src/main/java/org/smartregister/fhircore/engine/cql/LibraryEvaluator.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/cql/LibraryEvaluator.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.smartregister.fhircore.engine.cql
 
 import ca.uhn.fhir.context.FhirContext
@@ -7,10 +23,11 @@ import ca.uhn.fhir.model.valueset.BundleTypeEnum
 import ca.uhn.fhir.parser.IParser
 import ca.uhn.fhir.rest.api.BundleLinks
 import com.google.common.collect.Lists
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 import org.apache.commons.lang3.tuple.Pair
 import org.cqframework.cql.cql2elm.CqlTranslatorOptions
 import org.cqframework.cql.cql2elm.ModelManager
-import org.cqframework.cql.elm.execution.Library
 import org.cqframework.cql.elm.execution.VersionedIdentifier
 import org.hl7.fhir.instance.model.api.IBaseBundle
 import org.hl7.fhir.instance.model.api.IBaseResource
@@ -29,130 +46,130 @@ import org.opencds.cqf.cql.evaluator.engine.terminology.BundleTerminologyProvide
 import org.opencds.cqf.cql.evaluator.fhir.adapter.r4.AdapterFactory
 import org.opencds.cqf.cql.evaluator.library.CqlFhirParametersConverter
 import org.opencds.cqf.cql.evaluator.library.LibraryEvaluator
-import java.io.ByteArrayInputStream
-import java.io.InputStream
-import java.util.*
 
 /**
- * This class contains methods to run CQL evaluators given Fhir expressions
- * It borrows code from https://github.com/DBCG/CqlEvaluatorSampleApp
- * See also https://www.hl7.org/fhir/
+ * This class contains methods to run CQL evaluators given Fhir expressions It borrows code from
+ * https://github.com/DBCG/CqlEvaluatorSampleApp See also https://www.hl7.org/fhir/
  */
 class LibraryEvaluator {
-    var fhirContext = FhirContext.forCached(FhirVersionEnum.R4)
-    var adapterFactory = AdapterFactory()
-    var fhirTypeConverter: FhirTypeConverter? = null
-    var cqlFhirParametersConverter: CqlFhirParametersConverter? = null
-    var libraryVersionSelector: LibraryVersionSelector? = null
-    var contentProvider: LibraryContentProvider? = null
-    var terminologyProvider: BundleTerminologyProvider? = null
-    var bundleRetrieveProvider: BundleRetrieveProvider? = null
-    var cqlEvaluator: CqlEvaluator? = null
-    var libraryEvaluator: LibraryEvaluator? = null
+  var fhirContext = FhirContext.forCached(FhirVersionEnum.R4)
+  var adapterFactory = AdapterFactory()
+  var fhirTypeConverter: FhirTypeConverter? = null
+  var cqlFhirParametersConverter: CqlFhirParametersConverter? = null
+  var libraryVersionSelector: LibraryVersionSelector? = null
+  var contentProvider: LibraryContentProvider? = null
+  var terminologyProvider: BundleTerminologyProvider? = null
+  var bundleRetrieveProvider: BundleRetrieveProvider? = null
+  var cqlEvaluator: CqlEvaluator? = null
+  var libraryEvaluator: LibraryEvaluator? = null
 
-    /**
-     * This method loads configurations for CQL evaluation
-     * @param libraryData Fhir resource type Library
-     * @param helperData Fhir resource type LibraryHelper
-     * @param valueSetData Fhir resource type ValueSet
-     * @param testData Fhir resource to evaluate e.g Patient
-     */
-    private fun loadConfigs(
-        libraryData: String,
-        helperData: String,
-        valueSetData: String,
-        testData: String
-    ) {
-        var parser: IParser? = null
-        parser = fhirContext.newJsonParser()
-        parser.setPrettyPrint(true)
-        libraryVersionSelector = LibraryVersionSelector(adapterFactory)
-        fhirTypeConverter = FhirTypeConverterFactory().create(fhirContext.version.version)
-        cqlFhirParametersConverter =
-            CqlFhirParametersConverter(fhirContext, adapterFactory, fhirTypeConverter)
-        // Load Library Content and create a LibraryContentProvider, which is the interface used by the LibraryLoader for getting library CQL/ELM/etc.
-        val libraryStream: InputStream = ByteArrayInputStream(libraryData.toByteArray())
-        val fhirHelpersStream: InputStream = ByteArrayInputStream(helperData.toByteArray())
-        val library = parser.parseResource(libraryStream)
-        val fhirHelpersLibrary = parser.parseResource(fhirHelpersStream)
-        val resources: List<IBaseResource> = Lists.newArrayList(library, fhirHelpersLibrary)
-        val bundleFactory = fhirContext.newBundleFactory()
-        val bundleLinks = BundleLinks("", null, true, BundleTypeEnum.COLLECTION)
-        bundleFactory.addRootPropertiesToBundle(
-            "bundled-directory",
-            bundleLinks,
-            resources.size,
-            null
-        )
-        bundleFactory.addResourcesToBundle(
-            resources, BundleTypeEnum.COLLECTION, "",
-            BundleInclusionRule.BASED_ON_INCLUDES, null
-        )
-        contentProvider = BundleFhirLibraryContentProvider(
-            fhirContext,
-            bundleFactory.resourceBundle as IBaseBundle,
-            adapterFactory,
-            libraryVersionSelector
-        )
+  /**
+   * This method loads configurations for CQL evaluation
+   * @param libraryData Fhir resource type Library
+   * @param helperData Fhir resource type LibraryHelper
+   * @param valueSetData Fhir resource type ValueSet
+   * @param testData Fhir resource to evaluate e.g Patient
+   */
+  private fun loadConfigs(
+    libraryData: String,
+    helperData: String,
+    valueSetData: String,
+    testData: String
+  ) {
+    var parser: IParser? = null
+    parser = fhirContext.newJsonParser()
+    parser.setPrettyPrint(true)
+    libraryVersionSelector = LibraryVersionSelector(adapterFactory)
+    fhirTypeConverter = FhirTypeConverterFactory().create(fhirContext.version.version)
+    cqlFhirParametersConverter =
+      CqlFhirParametersConverter(fhirContext, adapterFactory, fhirTypeConverter)
+    // Load Library Content and create a LibraryContentProvider, which is the interface used by the
+    // LibraryLoader for getting library CQL/ELM/etc.
+    val libraryStream: InputStream = ByteArrayInputStream(libraryData.toByteArray())
+    val fhirHelpersStream: InputStream = ByteArrayInputStream(helperData.toByteArray())
+    val library = parser.parseResource(libraryStream)
+    val fhirHelpersLibrary = parser.parseResource(fhirHelpersStream)
+    val resources: List<IBaseResource> = Lists.newArrayList(library, fhirHelpersLibrary)
+    val bundleFactory = fhirContext.newBundleFactory()
+    val bundleLinks = BundleLinks("", null, true, BundleTypeEnum.COLLECTION)
+    bundleFactory.addRootPropertiesToBundle("bundled-directory", bundleLinks, resources.size, null)
+    bundleFactory.addResourcesToBundle(
+      resources,
+      BundleTypeEnum.COLLECTION,
+      "",
+      BundleInclusionRule.BASED_ON_INCLUDES,
+      null
+    )
+    contentProvider =
+      BundleFhirLibraryContentProvider(
+        fhirContext,
+        bundleFactory.resourceBundle as IBaseBundle,
+        adapterFactory,
+        libraryVersionSelector
+      )
 
-        // Load terminology content, and create a TerminologyProvider which is the interface used by the evaluator for resolving terminology
-        val valueSetStream: InputStream = ByteArrayInputStream(valueSetData.toByteArray())
-        val valueSetBundle = parser.parseResource(valueSetStream)
-        terminologyProvider = BundleTerminologyProvider(fhirContext, valueSetBundle as IBaseBundle)
+    // Load terminology content, and create a TerminologyProvider which is the interface used by the
+    // evaluator for resolving terminology
+    val valueSetStream: InputStream = ByteArrayInputStream(valueSetData.toByteArray())
+    val valueSetBundle = parser.parseResource(valueSetStream)
+    terminologyProvider = BundleTerminologyProvider(fhirContext, valueSetBundle as IBaseBundle)
 
-        // Load data content, and create a RetrieveProvider which is the interface used for implementations of CQL retrieves.
-        val dataStream: InputStream = ByteArrayInputStream(testData.toByteArray())
-        val dataBundle = parser.parseResource(dataStream)
-        bundleRetrieveProvider = BundleRetrieveProvider(fhirContext, dataBundle as IBaseBundle)
-        bundleRetrieveProvider!!.terminologyProvider = terminologyProvider
-        bundleRetrieveProvider!!.isExpandValueSets = true
-        cqlEvaluator = CqlEvaluator(
-            object : TranslatingLibraryLoader(
-                ModelManager(), listOf(contentProvider),
-                CqlTranslatorOptions.defaultOptions()
-            ) { },
-            object : HashMap<String?, DataProvider?>() {
-                init {
-                    put(
-                        "http://hl7.org/fhir",
-                        CompositeDataProvider(R4FhirModelResolver(), bundleRetrieveProvider)
-                    )
-                }
-            }, terminologyProvider
-        )
-        libraryEvaluator = LibraryEvaluator(cqlFhirParametersConverter, cqlEvaluator)
-    }
+    // Load data content, and create a RetrieveProvider which is the interface used for
+    // implementations of CQL retrieves.
+    val dataStream: InputStream = ByteArrayInputStream(testData.toByteArray())
+    val dataBundle = parser.parseResource(dataStream)
+    bundleRetrieveProvider = BundleRetrieveProvider(fhirContext, dataBundle as IBaseBundle)
+    bundleRetrieveProvider!!.terminologyProvider = terminologyProvider
+    bundleRetrieveProvider!!.isExpandValueSets = true
+    cqlEvaluator =
+      CqlEvaluator(
+        object :
+          TranslatingLibraryLoader(
+            ModelManager(),
+            listOf(contentProvider),
+            CqlTranslatorOptions.defaultOptions()
+          ) {},
+        object : HashMap<String?, DataProvider?>() {
+          init {
+            put(
+              "http://hl7.org/fhir",
+              CompositeDataProvider(R4FhirModelResolver(), bundleRetrieveProvider)
+            )
+          }
+        },
+        terminologyProvider
+      )
+    libraryEvaluator = LibraryEvaluator(cqlFhirParametersConverter, cqlEvaluator)
+  }
 
-    /**
-     * This method is used to run a CQL Evaluation
-     * @param libraryData Fhir resource type Library
-     * @param helperData Fhir resource type LibraryHelper
-     * @param valueSetData Fhir resource type ValueSet
-     * @param testData Fhir resource to evaluate e.g Patient
-     * @param evaluatorId Outcome Id of evaluation e.g ANCRecommendationA2
-     * @param context Fhir context, eg. Patient
-     * @param contextLabel Fhir context e.g. mom-with-anemia
-     * @return JSON String Fhir Resource type Parameter
-     */
-    fun runCql(
-        libraryData: String,
-        helperData: String,
-        valueSetData: String,
-        testData: String,
-        evaluatorId: String?,
-        context: String,
-        contextLabel: String
-    ): String {
-        loadConfigs(
-            libraryData,
-            helperData,
-            valueSetData,
-            testData
-        )
-        val result = libraryEvaluator!!.evaluate(
-            VersionedIdentifier().withId(evaluatorId),
-            Pair.of(context, contextLabel), null, null
-        )
-        return fhirContext.newJsonParser().encodeResourceToString(result)
-    }
+  /**
+   * This method is used to run a CQL Evaluation
+   * @param libraryData Fhir resource type Library
+   * @param helperData Fhir resource type LibraryHelper
+   * @param valueSetData Fhir resource type ValueSet
+   * @param testData Fhir resource to evaluate e.g Patient
+   * @param evaluatorId Outcome Id of evaluation e.g ANCRecommendationA2
+   * @param context Fhir context, eg. Patient
+   * @param contextLabel Fhir context e.g. mom-with-anemia
+   * @return JSON String Fhir Resource type Parameter
+   */
+  fun runCql(
+    libraryData: String,
+    helperData: String,
+    valueSetData: String,
+    testData: String,
+    evaluatorId: String?,
+    context: String,
+    contextLabel: String
+  ): String {
+    loadConfigs(libraryData, helperData, valueSetData, testData)
+    val result =
+      libraryEvaluator!!.evaluate(
+        VersionedIdentifier().withId(evaluatorId),
+        Pair.of(context, contextLabel),
+        null,
+        null
+      )
+    return fhirContext.newJsonParser().encodeResourceToString(result)
+  }
 }
--- android/engine/src/test/java/org/smartregister/fhircore/engine/cql/LibraryEvaluatorTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/cql/LibraryEvaluatorTest.kt
@@ -1,67 +1,90 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.smartregister.fhircore.engine.cql
 
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStreamReader
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import java.io.*
 
 class LibraryEvaluatorTest {
-    var libraryEvaluator: LibraryEvaluator? = null
-    var libraryData = ""
-    var helperData = ""
-    var valueSetData = ""
-    var testData = ""
-    var result = ""
-    var evaluatorId = "ANCRecommendationA2"
-    var context = "Patient"
-    var contextLabel = "mom-with-anemia"
-    @Before
-    fun setUp() {
-        try {
-            libraryData = readJsonFile(ASSET_BASE_PATH + "library.json")
-            helperData = readJsonFile(ASSET_BASE_PATH + "helper.json")
-            valueSetData = readJsonFile(ASSET_BASE_PATH + "valueSet.json")
-            testData = readJsonFile(ASSET_BASE_PATH + "patient.json")
-            result = readJsonFile(ASSET_BASE_PATH + "result.json")
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
+  var libraryEvaluator: LibraryEvaluator? = null
+  var libraryData = ""
+  var helperData = ""
+  var valueSetData = ""
+  var testData = ""
+  var result = ""
+  var evaluatorId = "ANCRecommendationA2"
+  var context = "Patient"
+  var contextLabel = "mom-with-anemia"
+  @Before
+  fun setUp() {
+    try {
+      libraryData = readJsonFile(ASSET_BASE_PATH + "library.json")
+      helperData = readJsonFile(ASSET_BASE_PATH + "helper.json")
+      valueSetData = readJsonFile(ASSET_BASE_PATH + "valueSet.json")
+      testData = readJsonFile(ASSET_BASE_PATH + "patient.json")
+      result = readJsonFile(ASSET_BASE_PATH + "result.json")
+    } catch (e: IOException) {
+      e.printStackTrace()
     }
+  }
 
-    @Test
-    fun runCql() {
-        libraryEvaluator = LibraryEvaluator()
-        val auxResult = libraryEvaluator!!.runCql(
-            libraryData,
-            helperData,
-            valueSetData,
-            testData,
-            evaluatorId,
-            context,
-            contextLabel
-        )
-        Assert.assertEquals(result, auxResult)
-    }
+  @Test
+  fun runCql() {
+    libraryEvaluator = LibraryEvaluator()
+    val auxResult =
+      libraryEvaluator!!.runCql(
+        libraryData,
+        helperData,
+        valueSetData,
+        testData,
+        evaluatorId,
+        context,
+        contextLabel
+      )
+    Assert.assertEquals(result, auxResult)
+  }
 
-    @Throws(IOException::class)
-    private fun readJsonFile(filename: String): String {
-        val br = BufferedReader(
-            InputStreamReader(
-                FileInputStream(filename)
-            )
-        )
-        val sb = StringBuilder()
-        var line = br.readLine()
-        while (line != null) {
-            sb.append(line)
-            line = br.readLine()
-        }
-        return sb.toString()
+  @Throws(IOException::class)
+  private fun readJsonFile(filename: String): String {
+    val br = BufferedReader(InputStreamReader(FileInputStream(filename)))
+    val sb = StringBuilder()
+    var line = br.readLine()
+    while (line != null) {
+      sb.append(line)
+      line = br.readLine()
     }
+    return sb.toString()
+  }
 
-    companion object {
-        val ASSET_BASE_PATH = (System.getProperty("user.dir")
-                + File.separator + "src" + File.separator
-                + "test" + File.separator + "resources" + File.separator)
-    }
+  companion object {
+    val ASSET_BASE_PATH =
+      (System.getProperty("user.dir") +
+        File.separator +
+        "src" +
+        File.separator +
+        "test" +
+        File.separator +
+        "resources" +
+        File.separator)
+  }
 }
