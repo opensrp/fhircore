bff541c8552a03105512ed4c4aad7b202e542259
Elly Kitoto <junkmailstoelly@gmail.com>
2021-09-10 23:32:55 +0300

    Fix gender extration from Patient resource

--- android/eir/src/main/java/org/smartregister/fhircore/eir/ui/patient/register/components/PatientRegisterList.kt
+++ android/eir/src/main/java/org/smartregister/fhircore/eir/ui/patient/register/components/PatientRegisterList.kt
@@ -52,7 +52,6 @@ fun PatientRegisterList(
           item {
             ErrorMessage(
               message = loadStateError.error.localizedMessage!!,
-              modifier = modifier.fillParentMaxSize(),
               onClickRetry = { retry() }
             )
           }
