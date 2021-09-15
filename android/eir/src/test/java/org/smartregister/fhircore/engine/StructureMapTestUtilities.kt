5a8276f2b6b1029c3ffdf06dd96b7a27584d5afe
syedowaisali <owais.ali@venturedive.com>
2021-09-16 00:17:24 +0500

    fixed package path

--- android/eir/src/test/java/org/smartregister/fhircore/engine/StructureMapTestUtilities.kt
+++ android/eir/src/test/java/org/smartregister/fhircore/engine/StructureMapTestUtilities.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.smartregister.fhircore
+package org.smartregister.fhircore.engine
 
 import ca.uhn.fhir.context.FhirContext
 import ca.uhn.fhir.parser.IParser
