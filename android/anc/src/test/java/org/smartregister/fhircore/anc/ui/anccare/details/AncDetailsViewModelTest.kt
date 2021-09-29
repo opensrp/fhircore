1fb3f38c3da60dceec244512fb1574c84aa4f9dc
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2021-09-29 19:33:31 +0500

    - spotlessApply

--- android/anc/src/test/java/org/smartregister/fhircore/anc/ui/anccare/details/AncDetailsViewModelTest.kt
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/anccare/details/AncDetailsViewModelTest.kt
@@ -25,21 +25,27 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.spyk
-import java.text.SimpleDateFormat
+import java.util.Date
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
-import org.bouncycastle.asn1.x500.style.RFC4519Style.title
-import org.hl7.fhir.r4.model.*
-import org.junit.*
+import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.CarePlan
+import org.hl7.fhir.r4.model.DateType
+import org.hl7.fhir.r4.model.Patient
+import org.hl7.fhir.r4.model.Reference
+import org.hl7.fhir.r4.model.Resource
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
 import org.smartregister.fhircore.anc.coroutine.CoroutineTestRule
 import org.smartregister.fhircore.anc.data.anc.AncPatientRepository
 import org.smartregister.fhircore.anc.data.anc.model.AncPatientDetailItem
 import org.smartregister.fhircore.anc.data.anc.model.AncPatientItem
-import org.smartregister.fhircore.anc.data.anc.model.CarePlanItem
 import org.smartregister.fhircore.engine.data.remote.fhir.resource.FhirResourceDataSource
 import org.smartregister.fhircore.engine.util.DateUtils.getDate
 import org.smartregister.fhircore.engine.util.extension.plusWeeksAsString
-import java.util.*
 
 @ExperimentalCoroutinesApi
 internal class AncDetailsViewModelTest {
@@ -170,25 +176,23 @@ internal class AncDetailsViewModelTest {
     val carePlanList = ancDetailsViewModel.fetchCarePlan().value
 
     Assert.assertEquals(1, carePlanList!!.size)
-    with(carePlanList!!.first()) {
-      Assert.assertEquals(cpTitle, title)
-    }
+    with(carePlanList!!.first()) { Assert.assertEquals(cpTitle, title) }
   }
 
-    private fun buildCarePlanWithActive(subject: String): CarePlan {
-        val date = DateType(Date())
-        val end = date.plusWeeksAsString(4).getDate("yyyy-MM-dd")
-        return CarePlan().apply {
-            this.id = "11190"
-            this.status = CarePlan.CarePlanStatus.ACTIVE
-            this.period.start = date.value
-            this.period.end = end
-            this.subject = Reference().apply { reference = "Patient/$subject" }
-            this.addActivity().detail.apply {
-                this.description = "First Care Plan"
-                this.scheduledPeriod.start = Date()
-                this.status = CarePlan.CarePlanActivityStatus.SCHEDULED
-            }
-        }
+  private fun buildCarePlanWithActive(subject: String): CarePlan {
+    val date = DateType(Date())
+    val end = date.plusWeeksAsString(4).getDate("yyyy-MM-dd")
+    return CarePlan().apply {
+      this.id = "11190"
+      this.status = CarePlan.CarePlanStatus.ACTIVE
+      this.period.start = date.value
+      this.period.end = end
+      this.subject = Reference().apply { reference = "Patient/$subject" }
+      this.addActivity().detail.apply {
+        this.description = "First Care Plan"
+        this.scheduledPeriod.start = Date()
+        this.status = CarePlan.CarePlanActivityStatus.SCHEDULED
+      }
     }
+  }
 }
--- android/anc/src/test/java/org/smartregister/fhircore/anc/ui/anccare/details/CarePlanAdapterTest.kt
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/anccare/details/CarePlanAdapterTest.kt
@@ -22,8 +22,6 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.verify
-import java.text.SimpleDateFormat
-import java.util.Date
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -60,20 +58,20 @@ class CarePlanAdapterTest : RobolectricTest() {
   fun testAdapterDiffUtilEquatesDifferentObjectsWithSameId() {
 
     val diffCallback = CarePlanAdapter.ImmunizationItemDiffCallback
-    val item = CarePlanItem("1111","1110","first", due = false, overdue = true)
+    val item = CarePlanItem("1111", "1110", "first", due = false, overdue = true)
 
     // change title only
-    val itemDifferentVaccine = CarePlanItem("1111","1110","second", due = false, overdue = true)
+    val itemDifferentVaccine = CarePlanItem("1111", "1110", "second", due = false, overdue = true)
     Assert.assertFalse(diffCallback.areItemsTheSame(item, itemDifferentVaccine))
     Assert.assertFalse(diffCallback.areContentsTheSame(item, itemDifferentVaccine))
 
     // same title with different content
-    val itemWithMatchingVaccine = CarePlanItem("1111","1110","first", due = true, overdue = false)
+    val itemWithMatchingVaccine = CarePlanItem("1111", "1110", "first", due = true, overdue = false)
     Assert.assertTrue(diffCallback.areItemsTheSame(item, itemWithMatchingVaccine))
     Assert.assertFalse(diffCallback.areContentsTheSame(item, itemWithMatchingVaccine))
 
     // identical items
-    val identical = CarePlanItem("1111","1110","first", due = false, overdue = true)
+    val identical = CarePlanItem("1111", "1110", "first", due = false, overdue = true)
     Assert.assertTrue(diffCallback.areItemsTheSame(item, identical))
     Assert.assertTrue(diffCallback.areContentsTheSame(item, identical))
   }
