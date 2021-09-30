e5f7f3d5f2f943eeed7e0617beadc3746ef0b546
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2021-09-30 11:26:59 +0500

    - Add Test cases for adapters

---
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/madx/details/adapter/CarePlanAdapterTest.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.anc.ui.madx.details.adapter
+
+import android.view.ViewGroup
+import androidx.test.core.app.ApplicationProvider
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.smartregister.fhircore.anc.data.madx.model.CarePlanItem
+import org.smartregister.fhircore.anc.robolectric.RobolectricTest
+
+class CarePlanAdapterTest : RobolectricTest() {
+
+  private lateinit var adapter: CarePlanAdapter
+
+  @Before
+  fun setUp() {
+    adapter = CarePlanAdapter()
+  }
+
+  @Test
+  fun testVerifyAdapterMethodsCall() {
+
+    val viewGroup = mockk<ViewGroup>()
+    every { viewGroup.context } returns ApplicationProvider.getApplicationContext()
+
+    val list = listOf(mockk<CarePlanItem>())
+    adapter.submitList(list)
+
+    val viewHolder = spyk(adapter.createViewHolder(viewGroup, 0))
+    Assert.assertNotNull(viewHolder)
+
+    every { viewHolder.bindTo(any()) } answers {}
+    adapter.bindViewHolder(viewHolder, 0)
+    verify(exactly = 1) { viewHolder.bindTo(any()) }
+  }
+
+  @Test
+  fun testAdapterDiffUtilEquatesDifferentObjectsWithSameId() {
+
+    val diffCallback = CarePlanAdapter.ImmunizationItemDiffCallback
+    val item = CarePlanItem("1111", "1110", "first", due = false, overdue = true)
+
+    // change title only
+    val itemDifferentVaccine = CarePlanItem("1111", "1110", "second", due = false, overdue = true)
+    Assert.assertFalse(diffCallback.areItemsTheSame(item, itemDifferentVaccine))
+    Assert.assertFalse(diffCallback.areContentsTheSame(item, itemDifferentVaccine))
+
+    // same title with different content
+    val itemWithMatchingVaccine = CarePlanItem("1111", "1110", "first", due = true, overdue = false)
+    Assert.assertTrue(diffCallback.areItemsTheSame(item, itemWithMatchingVaccine))
+    Assert.assertFalse(diffCallback.areContentsTheSame(item, itemWithMatchingVaccine))
+
+    // identical items
+    val identical = CarePlanItem("1111", "1110", "first", due = false, overdue = true)
+    Assert.assertTrue(diffCallback.areItemsTheSame(item, identical))
+    Assert.assertTrue(diffCallback.areContentsTheSame(item, identical))
+  }
+}
