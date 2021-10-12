821e3cb747e7d151a278ad71dd5aacb3aabf74ee
AbdulWahabMemon <abdulwahab.memon@venturedive.com>
2021-10-12 14:33:41 +0500

    spotlessApply & spotlessCheck

--- android/anc/src/main/java/org/smartregister/fhircore/anc/ui/madx/bmicompute/BmiComputeActivity.kt
+++ android/anc/src/main/java/org/smartregister/fhircore/anc/ui/madx/bmicompute/BmiComputeActivity.kt
@@ -29,100 +29,93 @@ import org.smartregister.fhircore.engine.ui.questionnaire.QuestionnaireActivity
 
 class BmiComputeActivity : QuestionnaireActivity() {
 
-    lateinit var bmiComputeViewModel: BmiComputeViewModel
-    internal lateinit var patientBmiRepository: BmiPatientRepository
-    private var encounterID = QuestionnaireUtils.getUniqueId()
+  lateinit var bmiComputeViewModel: BmiComputeViewModel
+  internal lateinit var patientBmiRepository: BmiPatientRepository
+  private var encounterID = QuestionnaireUtils.getUniqueId()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        patientBmiRepository =
-            BmiPatientRepository(AncApplication.getContext().fhirEngine, BmiPatientItemMapper)
-        bmiComputeViewModel =
-            BmiComputeViewModel.get(this, application as AncApplication, patientBmiRepository)
-        encounterID = QuestionnaireUtils.getUniqueId()
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    patientBmiRepository =
+      BmiPatientRepository(AncApplication.getContext().fhirEngine, BmiPatientItemMapper)
+    bmiComputeViewModel =
+      BmiComputeViewModel.get(this, application as AncApplication, patientBmiRepository)
+    encounterID = QuestionnaireUtils.getUniqueId()
+  }
+
+  override fun handleQuestionnaireResponse(questionnaireResponse: QuestionnaireResponse) {
+    lifecycleScope.launch {
+      val isUnitModeMetric = bmiComputeViewModel.isUnitModeMetric(questionnaireResponse)
+      val inputHeight = bmiComputeViewModel.getInputHeight(questionnaireResponse, isUnitModeMetric)
+      val inputWeight = bmiComputeViewModel.getInputWeight(questionnaireResponse, isUnitModeMetric)
+      val computedBMI = bmiComputeViewModel.calculateBMI(inputHeight, inputWeight, isUnitModeMetric)
+      if (computedBMI < 0)
+        showErrorAlert(getString(R.string.try_again), getString(R.string.error_saving_form))
+      else {
+        val patientId = intent.getStringExtra(QUESTIONNAIRE_ARG_PATIENT_KEY)!!
+        val height = bmiComputeViewModel.getHeightAsPerSIUnit(inputHeight, isUnitModeMetric)
+        val weight = bmiComputeViewModel.getWeightAsPerSIUnit(inputWeight, isUnitModeMetric)
+        showBmiDataAlert(questionnaireResponse, patientId, height, weight, computedBMI)
+      }
     }
+  }
 
-    override fun handleQuestionnaireResponse(questionnaireResponse: QuestionnaireResponse) {
-        lifecycleScope.launch {
-            val isUnitModeMetric = bmiComputeViewModel.isUnitModeMetric(questionnaireResponse)
-            val inputHeight =
-                bmiComputeViewModel.getInputHeight(questionnaireResponse, isUnitModeMetric)
-            val inputWeight =
-                bmiComputeViewModel.getInputWeight(questionnaireResponse, isUnitModeMetric)
-            val computedBMI =
-                bmiComputeViewModel.calculateBMI(inputHeight, inputWeight, isUnitModeMetric)
-            if (computedBMI < 0) showErrorAlert(
-                getString(R.string.try_again),
-                getString(R.string.error_saving_form)
-            )
-            else {
-                val patientId = intent.getStringExtra(QUESTIONNAIRE_ARG_PATIENT_KEY)!!
-                val height = bmiComputeViewModel.getHeightAsPerSIUnit(inputHeight, isUnitModeMetric)
-                val weight = bmiComputeViewModel.getWeightAsPerSIUnit(inputWeight, isUnitModeMetric)
-                showBmiDataAlert(questionnaireResponse, patientId, height, weight, computedBMI)
-            }
-        }
+  private fun showErrorAlert(title: String, message: String) {
+    AlertDialog.Builder(this)
+      .setTitle(title)
+      .setMessage(message)
+      .setCancelable(true)
+      .setPositiveButton("OK") { dialogInterface, _ -> dialogInterface.dismiss() }
+      .show()
+  }
+
+  private fun exitForm() {
+    this.finish()
+  }
+
+  private fun showBmiDataAlert(
+    questionnaireResponse: QuestionnaireResponse,
+    patientId: String,
+    height: Double,
+    weight: Double,
+    computedBMI: Double
+  ) {
+    val message = bmiComputeViewModel.getBMIResult(computedBMI, this)
+    AlertDialog.Builder(this)
+      .setTitle(getString(R.string.your_bmi) + " $computedBMI")
+      .setMessage(message)
+      .setCancelable(false)
+      .setNegativeButton(R.string.re_compute) { dialogInterface, _ -> dialogInterface.dismiss() }
+      .setPositiveButton(R.string.str_save) { dialogInterface, _ ->
+        dialogInterface.dismiss()
+        proceedRecordBMI(questionnaireResponse, patientId, height, weight, computedBMI)
+      }
+      .show()
+  }
+
+  private fun proceedRecordBMI(
+    questionnaireResponse: QuestionnaireResponse,
+    patientId: String,
+    height: Double,
+    weight: Double,
+    computedBMI: Double
+  ) {
+    lifecycleScope.launch {
+      val success =
+        bmiComputeViewModel.saveComputedBMI(
+          questionnaire!!,
+          questionnaireResponse,
+          patientId,
+          encounterID,
+          height,
+          weight,
+          computedBMI
+        )
+
+      if (success) {
+        exitForm()
+      } else {
+        showErrorAlert(getString(R.string.try_again), getString(R.string.error_saving_form))
+      }
     }
-
-    private fun showErrorAlert(title: String, message: String) {
-        AlertDialog.Builder(this)
-            .setTitle(title)
-            .setMessage(message)
-            .setCancelable(true)
-            .setPositiveButton("OK") { dialogInterface, _ -> dialogInterface.dismiss() }
-            .show()
-    }
-
-    private fun exitForm() {
-        this.finish()
-    }
-
-    private fun showBmiDataAlert(
-        questionnaireResponse: QuestionnaireResponse,
-        patientId: String,
-        height: Double,
-        weight: Double,
-        computedBMI: Double
-    ) {
-        val message = bmiComputeViewModel.getBMIResult(computedBMI, this)
-        AlertDialog.Builder(this)
-            .setTitle(getString(R.string.your_bmi) + " $computedBMI")
-            .setMessage(message)
-            .setCancelable(false)
-            .setNegativeButton(R.string.re_compute) { dialogInterface, _ -> dialogInterface.dismiss() }
-            .setPositiveButton(R.string.str_save) { dialogInterface, _ ->
-                dialogInterface.dismiss()
-                proceedRecordBMI(questionnaireResponse, patientId, height, weight, computedBMI)
-            }
-            .show()
-    }
-
-    private fun proceedRecordBMI(
-        questionnaireResponse: QuestionnaireResponse,
-        patientId: String,
-        height: Double,
-        weight: Double,
-        computedBMI: Double
-    ) {
-        lifecycleScope.launch {
-            val success = bmiComputeViewModel.saveComputedBMI(
-                questionnaire!!,
-                questionnaireResponse,
-                patientId,
-                encounterID,
-                height,
-                weight,
-                computedBMI
-            )
-
-            if (success) {
-                exitForm()
-            } else {
-                showErrorAlert(
-                    getString(R.string.try_again),
-                    getString(R.string.error_saving_form)
-                )
-            }
-        }
-    }
+  }
 }
--- android/anc/src/test/java/org/smartregister/fhircore/anc/data/madx/model/MadxModelsTest.kt
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/data/madx/model/MadxModelsTest.kt
@@ -23,80 +23,76 @@ import org.smartregister.fhircore.anc.robolectric.RobolectricTest
 
 class MadxModelsTest : RobolectricTest() {
 
-    private lateinit var ancPatientItem: AncPatientItem
-    private lateinit var ancPatientItemHead: AncPatientItem
-    private lateinit var encounterItem: EncounterItem
-    private lateinit var upcomingServiceItem: UpcomingServiceItem
-    private lateinit var ancPatientDetailItem: AncPatientDetailItem
-    private lateinit var patientBMIItem: PatientBMIItem
+  private lateinit var ancPatientItem: AncPatientItem
+  private lateinit var ancPatientItemHead: AncPatientItem
+  private lateinit var encounterItem: EncounterItem
+  private lateinit var upcomingServiceItem: UpcomingServiceItem
+  private lateinit var ancPatientDetailItem: AncPatientDetailItem
+  private lateinit var patientBMIItem: PatientBMIItem
 
-    @Before
-    fun setUp() {
-        ancPatientItem =
-            AncPatientItem("111", "anb", "M", "25", "PD", "none", "xyz", AncVisitStatus.PLANNED)
-        ancPatientItemHead =
-            AncPatientItem("111", "anb", "M", "25", "PD", "none", "xyz", AncVisitStatus.PLANNED)
-        encounterItem = EncounterItem("111", "111", "abc", "2020-02-12")
-        upcomingServiceItem = UpcomingServiceItem("111", "111", "1bc", "2020-02-12")
-        ancPatientDetailItem = AncPatientDetailItem(ancPatientItem, ancPatientItemHead)
-        patientBMIItem = PatientBMIItem("1111", "testBMI1", "5'7", "50lbs", "22.22")
-    }
+  @Before
+  fun setUp() {
+    ancPatientItem =
+      AncPatientItem("111", "anb", "M", "25", "PD", "none", "xyz", AncVisitStatus.PLANNED)
+    ancPatientItemHead =
+      AncPatientItem("111", "anb", "M", "25", "PD", "none", "xyz", AncVisitStatus.PLANNED)
+    encounterItem = EncounterItem("111", "111", "abc", "2020-02-12")
+    upcomingServiceItem = UpcomingServiceItem("111", "111", "1bc", "2020-02-12")
+    ancPatientDetailItem = AncPatientDetailItem(ancPatientItem, ancPatientItemHead)
+    patientBMIItem = PatientBMIItem("1111", "testBMI1", "5'7", "50lbs", "22.22")
+  }
 
-    @Test
-    fun testAncPatientItem() {
-        Assert.assertEquals("111", ancPatientItem.patientIdentifier)
-        Assert.assertEquals("anb", ancPatientItem.name)
-        Assert.assertEquals("M", ancPatientItem.gender)
-        Assert.assertEquals("PD", ancPatientItem.demographics)
-        Assert.assertEquals("none", ancPatientItem.atRisk)
-        Assert.assertEquals("xyz", ancPatientItem.address)
-        Assert.assertEquals(AncVisitStatus.PLANNED, ancPatientItem.visitStatus)
-    }
+  @Test
+  fun testAncPatientItem() {
+    Assert.assertEquals("111", ancPatientItem.patientIdentifier)
+    Assert.assertEquals("anb", ancPatientItem.name)
+    Assert.assertEquals("M", ancPatientItem.gender)
+    Assert.assertEquals("PD", ancPatientItem.demographics)
+    Assert.assertEquals("none", ancPatientItem.atRisk)
+    Assert.assertEquals("xyz", ancPatientItem.address)
+    Assert.assertEquals(AncVisitStatus.PLANNED, ancPatientItem.visitStatus)
+  }
 
-    @Test
-    fun testUpcomingServiceItem() {
-        Assert.assertEquals("111", encounterItem.encounterIdentifier)
-        Assert.assertEquals("111", encounterItem.patientIdentifier)
-        Assert.assertEquals("abc", encounterItem.title)
-        Assert.assertEquals("2020-02-12", encounterItem.date)
-    }
+  @Test
+  fun testUpcomingServiceItem() {
+    Assert.assertEquals("111", encounterItem.encounterIdentifier)
+    Assert.assertEquals("111", encounterItem.patientIdentifier)
+    Assert.assertEquals("abc", encounterItem.title)
+    Assert.assertEquals("2020-02-12", encounterItem.date)
+  }
 
-    @Test
-    fun testEncounterItem() {
-        Assert.assertEquals("111", upcomingServiceItem.encounterIdentifier)
-        Assert.assertEquals("111", upcomingServiceItem.patientIdentifier)
-        Assert.assertEquals("1bc", upcomingServiceItem.title)
-        Assert.assertEquals("2020-02-12", upcomingServiceItem.date)
-    }
+  @Test
+  fun testEncounterItem() {
+    Assert.assertEquals("111", upcomingServiceItem.encounterIdentifier)
+    Assert.assertEquals("111", upcomingServiceItem.patientIdentifier)
+    Assert.assertEquals("1bc", upcomingServiceItem.title)
+    Assert.assertEquals("2020-02-12", upcomingServiceItem.date)
+  }
 
-    @Test
-    fun testAncPatientDetailItem() {
-        Assert.assertEquals("111", ancPatientDetailItem.patientDetails.patientIdentifier)
-        Assert.assertEquals("anb", ancPatientDetailItem.patientDetails.name)
-        Assert.assertEquals("M", ancPatientDetailItem.patientDetails.gender)
-        Assert.assertEquals("PD", ancPatientDetailItem.patientDetails.demographics)
-        Assert.assertEquals("none", ancPatientDetailItem.patientDetails.atRisk)
-        Assert.assertEquals("xyz", ancPatientDetailItem.patientDetails.address)
-        Assert.assertEquals(AncVisitStatus.PLANNED, ancPatientDetailItem.patientDetails.visitStatus)
-        Assert.assertEquals("111", ancPatientDetailItem.patientDetailsHead.patientIdentifier)
-        Assert.assertEquals("anb", ancPatientDetailItem.patientDetailsHead.name)
-        Assert.assertEquals("M", ancPatientDetailItem.patientDetailsHead.gender)
-        Assert.assertEquals("PD", ancPatientDetailItem.patientDetailsHead.demographics)
-        Assert.assertEquals("none", ancPatientDetailItem.patientDetailsHead.atRisk)
-        Assert.assertEquals("xyz", ancPatientDetailItem.patientDetailsHead.address)
-        Assert.assertEquals(
-            AncVisitStatus.PLANNED,
-            ancPatientDetailItem.patientDetailsHead.visitStatus
-        )
-    }
+  @Test
+  fun testAncPatientDetailItem() {
+    Assert.assertEquals("111", ancPatientDetailItem.patientDetails.patientIdentifier)
+    Assert.assertEquals("anb", ancPatientDetailItem.patientDetails.name)
+    Assert.assertEquals("M", ancPatientDetailItem.patientDetails.gender)
+    Assert.assertEquals("PD", ancPatientDetailItem.patientDetails.demographics)
+    Assert.assertEquals("none", ancPatientDetailItem.patientDetails.atRisk)
+    Assert.assertEquals("xyz", ancPatientDetailItem.patientDetails.address)
+    Assert.assertEquals(AncVisitStatus.PLANNED, ancPatientDetailItem.patientDetails.visitStatus)
+    Assert.assertEquals("111", ancPatientDetailItem.patientDetailsHead.patientIdentifier)
+    Assert.assertEquals("anb", ancPatientDetailItem.patientDetailsHead.name)
+    Assert.assertEquals("M", ancPatientDetailItem.patientDetailsHead.gender)
+    Assert.assertEquals("PD", ancPatientDetailItem.patientDetailsHead.demographics)
+    Assert.assertEquals("none", ancPatientDetailItem.patientDetailsHead.atRisk)
+    Assert.assertEquals("xyz", ancPatientDetailItem.patientDetailsHead.address)
+    Assert.assertEquals(AncVisitStatus.PLANNED, ancPatientDetailItem.patientDetailsHead.visitStatus)
+  }
 
-    @Test
-    fun testPatientBMIItem() {
-        Assert.assertEquals("1111", patientBMIItem.patientIdentifier)
-        Assert.assertEquals("testBMI1", patientBMIItem.name)
-        Assert.assertEquals("5'7", patientBMIItem.height)
-        Assert.assertEquals("50lbs", patientBMIItem.weight)
-        Assert.assertEquals("22.22", patientBMIItem.bmi)
-    }
-
+  @Test
+  fun testPatientBMIItem() {
+    Assert.assertEquals("1111", patientBMIItem.patientIdentifier)
+    Assert.assertEquals("testBMI1", patientBMIItem.name)
+    Assert.assertEquals("5'7", patientBMIItem.height)
+    Assert.assertEquals("50lbs", patientBMIItem.weight)
+    Assert.assertEquals("22.22", patientBMIItem.bmi)
+  }
 }
