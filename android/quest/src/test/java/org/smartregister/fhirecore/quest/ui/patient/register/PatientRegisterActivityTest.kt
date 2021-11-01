b337b99db51fddcc325d05d25899eb786cf4355a
Syed Owais Ali <owais.ali@venturedive.com>
2021-11-01 15:49:55 +0500

    Added QuestPatientDetailScreen test class and added more tests in other test classes

--- android/quest/src/test/java/org/smartregister/fhirecore/quest/ui/patient/register/PatientRegisterActivityTest.kt
+++ android/quest/src/test/java/org/smartregister/fhirecore/quest/ui/patient/register/PatientRegisterActivityTest.kt
@@ -36,6 +36,7 @@ import org.robolectric.util.ReflectionHelpers
 import org.smartregister.fhircore.engine.configuration.view.loadRegisterViewConfiguration
 import org.smartregister.fhircore.engine.databinding.BaseRegisterActivityBinding
 import org.smartregister.fhircore.engine.ui.questionnaire.QuestionnaireActivity
+import org.smartregister.fhircore.engine.ui.register.model.RegisterItem
 import org.smartregister.fhircore.engine.ui.userprofile.UserProfileFragment
 import org.smartregister.fhircore.quest.R
 import org.smartregister.fhircore.quest.ui.patient.register.PatientRegisterActivity
@@ -168,6 +169,21 @@ class PatientRegisterActivityTest : ActivityRobolectricTest() {
     Assert.assertEquals(2, fragments.size)
     Assert.assertTrue(fragments.containsKey(PatientRegisterFragment.TAG))
     Assert.assertTrue(fragments.containsKey(UserProfileFragment.TAG))
+  }
+
+  @Test
+  fun testRegistersListShouldReturnOnlyOneItemList() {
+    val list =
+      ReflectionHelpers.callInstanceMethod<List<RegisterItem>>(
+        patientRegisterActivity,
+        "registersList"
+      )
+    Assert.assertEquals(1, list.size)
+    with(list[0]) {
+      Assert.assertEquals(PatientRegisterFragment.TAG, uniqueTag)
+      Assert.assertEquals(patientRegisterActivity.getString(R.string.clients), title)
+      Assert.assertTrue(isSelected)
+    }
   }
 
   override fun getActivity(): Activity {
