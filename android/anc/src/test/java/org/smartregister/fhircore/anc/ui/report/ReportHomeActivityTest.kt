1624740649b3912913f9b5d0f177bbbe5adb3f9d
AbdulWahabMemon <66945489+AbdulWahabMemon@users.noreply.github.com>
2021-11-10 19:52:05 +0500

    675: Reports Home Listing (#732)
    * 675: Reports Home Listing
    
    add ReportActivity as composable screen and ReportItem UI component
    
    * anc test fail spotlessApply
    
    * fix anc test fail in PR
    
    * fix anc test fail
    
    * spotlessApply & Check
    
    * fix anc test fail
    
    * fix anc test fail ignore
    
    * fixes as per reviewer comments
    
    * spotlessApply/Check
    
    * report home screen data test
    
    * fix anc test fail + spotlessApply
    
    * refactoring and fixes as per reviewer comments
    
    * update unit test for patch coverage
    
    * fix reviewer comment
    
    * spotlessApply
    
    * changelog
    ui package folder reports -> report
    
    Co-authored-by: Elly Kitoto <junkmailstoelly@gmail.com>

---
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/report/ReportHomeActivityTest.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.anc.ui.report
+
+import android.app.Activity
+import io.mockk.spyk
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.robolectric.Robolectric
+import org.robolectric.annotation.Config
+import org.smartregister.fhircore.anc.activity.ActivityRobolectricTest
+import org.smartregister.fhircore.anc.shadow.AncApplicationShadow
+
+@Config(shadows = [AncApplicationShadow::class])
+class ReportHomeActivityTest : ActivityRobolectricTest() {
+
+  private lateinit var activity: ReportHomeActivity
+  private lateinit var activitySpy: ReportHomeActivity
+
+  @Before
+  fun setUp() {
+    activity = Robolectric.buildActivity(ReportHomeActivity::class.java).create().get()
+    activitySpy = spyk(objToCopy = activity)
+  }
+
+  @Test
+  fun testActivityNotNull() {
+    Assert.assertNotNull(activity)
+  }
+
+  override fun getActivity(): Activity {
+    return activity
+  }
+}
