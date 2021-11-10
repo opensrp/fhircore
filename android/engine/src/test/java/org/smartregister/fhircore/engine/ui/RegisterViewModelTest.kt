f539ed8ca46cbd3e8f88ffe14cd4567cea7b9b22
Owais <62104757+owais-vd@users.noreply.github.com>
2021-11-10 11:46:59 +0500

    Add code coverage for current work pushed to master branch on FHIR Core (#722)
    * added test case in PatientRepository
    
    * added more tests
    
    * Add AncOverviewConfiguationTest
    
    * added more tests
    
    * update UserProfileViewModelTest cases
    
    * fixed test case

---
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/ui/RegisterViewModelTest.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.engine.ui
+
+import android.app.Application
+import android.os.Looper
+import androidx.test.core.app.ApplicationProvider
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.spyk
+import io.mockk.unmockkStatic
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.robolectric.Shadows.shadowOf
+import org.smartregister.fhircore.engine.robolectric.RobolectricTest
+import org.smartregister.fhircore.engine.ui.register.RegisterViewModel
+import org.smartregister.fhircore.engine.ui.register.model.RegisterFilterType
+import org.smartregister.fhircore.engine.util.DefaultDispatcherProvider
+import org.smartregister.fhircore.engine.util.DispatcherProvider
+import org.smartregister.fhircore.engine.util.LAST_SYNC_TIMESTAMP
+import org.smartregister.fhircore.engine.util.SharedPreferencesHelper
+import org.smartregister.fhircore.engine.util.extension.runOneTimeSync
+
+class RegisterViewModelTest : RobolectricTest() {
+
+  private lateinit var viewModel: RegisterViewModel
+
+  @Before
+  fun setUp() {
+
+    val context = ApplicationProvider.getApplicationContext<Application>()
+    SharedPreferencesHelper.init(context)
+
+    val dispatcher =
+      mockk<DispatcherProvider> {
+        every { io() } returns DefaultDispatcherProvider.main()
+        every { main() } returns DefaultDispatcherProvider.main()
+      }
+
+    viewModel = RegisterViewModel(context, mockk(), dispatcher)
+  }
+
+  @Test
+  fun testUpdateViewConfigurationsShouldUpdateGlobalVariable() {
+    viewModel.updateViewConfigurations(
+      mockk {
+        every { id } returns "1"
+        every { appTitle } returns "Covax"
+      }
+    )
+
+    assertEquals("1", viewModel.registerViewConfiguration.value?.id)
+    assertEquals("Covax", viewModel.registerViewConfiguration.value?.appTitle)
+  }
+
+  @Test
+  fun testLoadLanguagesShouldLoadEnglishLocaleOnly() {
+    viewModel.loadLanguages()
+
+    Assert.assertEquals(1, viewModel.languages.size)
+    Assert.assertEquals("English", viewModel.languages[0].displayName)
+  }
+
+  @Test
+  fun testRunSyncShouldRunOnlyOnce() {
+
+    mockkStatic(Application::runOneTimeSync)
+
+    val app = mockk<Application> { coEvery { runOneTimeSync(any()) } returns Unit }
+
+    val viewModelSpy = spyk(viewModel) { every { getApplication<Application>() } returns app }
+
+    viewModelSpy.runSync()
+
+    shadowOf(Looper.getMainLooper()).idle()
+    coVerify(exactly = 1) { app.runOneTimeSync(any()) }
+
+    unmockkStatic(Application::runOneTimeSync)
+  }
+
+  @Test
+  fun testUpdateFilterValueShouldUpdateGlobalFilter() {
+    viewModel.updateFilterValue(RegisterFilterType.OVERDUE_FILTER, true)
+
+    assertEquals(RegisterFilterType.OVERDUE_FILTER, viewModel.filterValue.value?.first)
+    assertTrue(viewModel.filterValue.value?.second as Boolean)
+  }
+
+  @Test
+  fun testSetRefreshRegisterDataShouldUpdateGlobalRegisterDate() {
+    viewModel.setRefreshRegisterData(true)
+    assertTrue(viewModel.refreshRegisterData.value!!)
+  }
+
+  @Test
+  fun testSetLastSyncTimestampShouldUpdateGlobalSyncTimestamp() {
+    viewModel.setLastSyncTimestamp("12345")
+    assertEquals("12345", viewModel.lastSyncTimestamp.value)
+    assertEquals("12345", SharedPreferencesHelper.read(LAST_SYNC_TIMESTAMP, null))
+  }
+}
