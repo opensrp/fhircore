c7a5589cebb64f05989d2089b2db7c4010c648bb
kimigxfoxy <dkimigz@gmail.com>
2021-11-16 13:38:19 +0300

    address pr issues

--- android/engine/src/main/java/org/smartregister/fhircore/engine/util/FileUtil.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/util/FileUtil.kt
@@ -25,7 +25,6 @@ import java.io.FileWriter
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.Exception
 import java.util.ArrayList
 import java.util.Properties
 
