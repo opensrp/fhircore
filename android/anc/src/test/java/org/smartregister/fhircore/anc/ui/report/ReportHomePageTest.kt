f27ddba326012d8c2fbb30c5329e8370b9792a0e
AbdulWahabMemon <abdulwahab.memon@venturedive.com>
2021-11-19 18:51:36 +0500

    spotlessApply

--- android/anc/src/test/java/org/smartregister/fhircore/anc/ui/report/ReportHomePageTest.kt
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/report/ReportHomePageTest.kt
@@ -27,6 +27,7 @@ import io.mockk.mockk
 import io.mockk.spyk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.smartregister.fhircore.anc.R
@@ -67,7 +68,7 @@ class ReportHomePageTest : RobolectricTest() {
       )
   }
 
-  // @Ignore("Fix tracked on https://github.com/opensrp/fhircore/issues/760")
+  @Ignore("Fix tracked on https://github.com/opensrp/fhircore/issues/760")
   @Test
   fun testReportHomeScreenComponents() {
     composeRule.setContent { ReportHomeScreen(viewModel = viewModel) }
