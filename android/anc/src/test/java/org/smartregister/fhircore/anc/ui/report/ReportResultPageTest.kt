1d82045801fdad973191fc9492c45cf4c230b294
AbdulWahabMemon <abdulwahab.memon@venturedive.com>
2021-11-25 12:10:25 +0500

    spotlessApply

--- android/anc/src/test/java/org/smartregister/fhircore/anc/ui/report/ReportResultPageTest.kt
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/report/ReportResultPageTest.kt
@@ -26,7 +26,6 @@ import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.smartregister.fhircore.anc.coroutine.CoroutineTestRule
