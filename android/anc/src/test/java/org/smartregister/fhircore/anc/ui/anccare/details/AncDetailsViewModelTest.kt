4395a9e79873ddb377d10fbbbc17d108aee7b2a7
owais-vd <owais.ali@venturedive.com>
2021-12-07 17:54:50 +0500

    added more tests

--- android/anc/src/test/java/org/smartregister/fhircore/anc/ui/anccare/details/AncDetailsViewModelTest.kt
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/anccare/details/AncDetailsViewModelTest.kt
@@ -27,21 +27,22 @@ import io.mockk.spyk
 import java.util.Date
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
-import org.hl7.fhir.r4.model.CarePlan
-import org.hl7.fhir.r4.model.DateType
+import org.hl7.fhir.r4.model.Encounter
+import org.hl7.fhir.r4.model.Observation
 import org.hl7.fhir.r4.model.Patient
-import org.hl7.fhir.r4.model.Reference
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.smartregister.fhircore.anc.app.fakes.FakeModel
 import org.smartregister.fhircore.anc.coroutine.CoroutineTestRule
+import org.smartregister.fhircore.anc.data.model.CarePlanItem
+import org.smartregister.fhircore.anc.data.model.EncounterItem
 import org.smartregister.fhircore.anc.data.model.PatientDetailItem
 import org.smartregister.fhircore.anc.data.model.PatientItem
+import org.smartregister.fhircore.anc.data.model.UpcomingServiceItem
 import org.smartregister.fhircore.anc.data.patient.PatientRepository
 import org.smartregister.fhircore.anc.robolectric.RobolectricTest
-import org.smartregister.fhircore.engine.util.DateUtils.getDate
-import org.smartregister.fhircore.engine.util.extension.plusWeeksAsString
 
 @ExperimentalCoroutinesApi
 @HiltAndroidTest
@@ -106,33 +107,74 @@ internal class AncDetailsViewModelTest : RobolectricTest() {
   @Test
   fun testFetchCarePlanShouldReturnExpectedCarePlan() {
 
-    val cpTitle = "First Care Plan"
-
-    coEvery { patientRepository.fetchCarePlan(any()) } returns
-      listOf(buildCarePlanWithActive("1111"))
+    coEvery { patientRepository.searchCarePlan(any()) } returns mockk()
+    coEvery { patientRepository.fetchCarePlanItem(any()) } returns
+      listOf(CarePlanItem("1", "First Care Plan", due = false, overdue = false))
 
     val carePlanList = ancDetailsViewModel.fetchCarePlan(patientId).value
 
     if (carePlanList != null && carePlanList.isNotEmpty()) {
       Assert.assertEquals(1, carePlanList.size)
-      with(carePlanList.first()) { Assert.assertEquals(cpTitle, title) }
+      with(carePlanList.first()) {
+        Assert.assertEquals("1", carePlanIdentifier)
+        Assert.assertEquals("First Care Plan", title)
+        Assert.assertFalse(due)
+        Assert.assertFalse(overdue)
+      }
     }
   }
 
-  private fun buildCarePlanWithActive(subject: String): CarePlan {
-    val date = DateType(Date())
-    val end = date.plusWeeksAsString(4).getDate("yyyy-MM-dd")
-    return CarePlan().apply {
-      this.id = "11190"
-      this.status = CarePlan.CarePlanStatus.ACTIVE
-      this.period.start = date.value
-      this.period.end = end
-      this.subject = Reference().apply { reference = "Patient/$subject" }
-      this.addActivity().detail.apply {
-        this.description = "First Care Plan"
-        this.scheduledPeriod.start = Date()
-        this.status = CarePlan.CarePlanActivityStatus.SCHEDULED
-      }
+  @Test
+  fun testFetchObservationShouldReturnExpectedAncOverviewItem() {
+    coEvery { patientRepository.fetchObservations(any(), "edd") } returns Observation()
+    coEvery { patientRepository.fetchObservations(any(), "risk") } returns
+      FakeModel.getObservation(testValue = 1)
+    coEvery { patientRepository.fetchObservations(any(), "fetuses") } returns
+      FakeModel.getObservation(testValue = 2)
+    coEvery { patientRepository.fetchObservations(any(), "ga") } returns
+      FakeModel.getObservation(testValue = 25)
+
+    val ancOverviewItem = ancDetailsViewModel.fetchObservation("").value
+
+    with(ancOverviewItem) {
+      Assert.assertEquals("", this?.edd)
+      Assert.assertEquals("25", this?.ga)
+      Assert.assertEquals("2", this?.noOfFetuses)
+      Assert.assertEquals("1", this?.risk)
+    }
+  }
+
+  @Test
+  fun testFetchUpcomingServicesShouldReturnExpectedUpcomingServiceItemList() {
+
+    coEvery { patientRepository.fetchCarePlan(any()) } returns mockk()
+    coEvery { patientRepository.fetchUpcomingServiceItem(any()) } returns
+      listOf(UpcomingServiceItem("1", "Upcoming Service Title", ""))
+
+    val upcomingServiceList = ancDetailsViewModel.fetchUpcomingServices(patientId).value
+
+    Assert.assertEquals(1, upcomingServiceList?.size)
+    with(upcomingServiceList?.first()) {
+      Assert.assertEquals("1", this?.encounterIdentifier)
+      Assert.assertEquals("Upcoming Service Title", this?.title)
+      Assert.assertEquals("", this?.date)
+    }
+  }
+
+  @Test
+  fun testFetchLastSeenShouldReturnExpectedEncounterItemList() {
+
+    coEvery { patientRepository.fetchEncounters(any()) } returns mockk()
+    coEvery { patientRepository.fetchLastSeenItem(any()) } returns
+      listOf(EncounterItem("1", Encounter.EncounterStatus.FINISHED, "completed", Date()))
+
+    val lastSeenList = ancDetailsViewModel.fetchLastSeen(patientId).value
+
+    Assert.assertEquals(1, lastSeenList?.size)
+    with(lastSeenList?.first()) {
+      Assert.assertEquals("1", this?.id)
+      Assert.assertEquals(Encounter.EncounterStatus.FINISHED, this?.status)
+      Assert.assertEquals("completed", this?.display)
     }
   }
 }
