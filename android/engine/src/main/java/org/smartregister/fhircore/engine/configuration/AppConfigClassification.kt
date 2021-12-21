152dd7a85eede8ae26f1c9312a33caf66815614b
Ephraim Kigamba <31766075+ekigamba@users.noreply.github.com>
2021-12-21 16:05:08 +0300

    Implement language switcher on the profile page for quest and anc (#887)
    * Implement language switcher on the profile page for quest and anc
    
    * Add UserProfileFragmentTest, UserProfileScreenKtTest, UserProfileViewModelTest and AndroidExtensionTest
    
    * Update changelog with #887 changes
    
    * Remove RegisterConfiguration#switchLanguages
    
    - Refactor the language switcher functionalities to check the number of languages configured instead
    
    Co-authored-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/AppConfigClassification.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/AppConfigClassification.kt
@@ -19,5 +19,6 @@ package org.smartregister.fhircore.engine.configuration
 enum class AppConfigClassification : ConfigClassification {
   APPLICATION,
   LOGIN;
+
   override val classification: String = name.lowercase()
 }
