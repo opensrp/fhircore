b696ca741165e7929ab793c9db0d030e8fd1ce7d
Fikri Yurcel Milano <fikrimilano1@gmail.com>
2021-12-21 16:02:28 +0700

    Add BitmapExtension

---
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/util/extension/BitmapExtension.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.engine.util.extension
+
+import android.graphics.Bitmap
+import java.io.ByteArrayOutputStream
+
+fun Bitmap.encodeToByteArray(): ByteArray {
+  ByteArrayOutputStream().use { outputStream ->
+    this.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+    return outputStream.toByteArray()
+  }
+}
