5eba937855883371b5e5ce849605b301a2533863
Syed Owais Ali <owais.ali@venturedive.com>
2022-01-04 16:16:17 +0500

    added test case for handleErrorMessage method in LoginViewModelTest

--- android/engine/src/test/java/org/smartregister/fhircore/engine/ui/login/LoginViewModelTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/ui/login/LoginViewModelTest.kt
@@ -25,6 +25,8 @@ import io.mockk.just
 import io.mockk.runs
 import io.mockk.spyk
 import io.mockk.verify
+import java.io.IOException
+import java.net.UnknownHostException
 import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
@@ -33,6 +35,8 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.robolectric.annotation.Config
+import org.robolectric.util.ReflectionHelpers
+import org.smartregister.fhircore.engine.R
 import org.smartregister.fhircore.engine.app.fakes.FakeModel.authCredentials
 import org.smartregister.fhircore.engine.auth.AccountAuthenticator
 import org.smartregister.fhircore.engine.data.remote.model.response.OAuthResponse
@@ -200,5 +204,29 @@ internal class LoginViewModelTest : RobolectricTest() {
     Assert.assertEquals("", loginViewModel.loginError.value)
     loginViewModel.showProgressBar.value?.let { Assert.assertTrue(it) }
     verify { accountAuthenticatorSpy.fetchToken("testUser", "51r1K4l1".toCharArray()) }
+  }
+
+  @Test
+  fun testHandleErrorMessageShouldVerifyExpectedMessage() {
+
+    ReflectionHelpers.callInstanceMethod<Any>(
+      loginViewModel,
+      "handleErrorMessage",
+      ReflectionHelpers.ClassParameter(Throwable::class.java, UnknownHostException())
+    )
+    Assert.assertEquals(
+      loginViewModel.app.getString(R.string.login_call_fail_error_message),
+      loginViewModel.loginError.value
+    )
+
+    ReflectionHelpers.callInstanceMethod<Any>(
+      loginViewModel,
+      "handleErrorMessage",
+      ReflectionHelpers.ClassParameter(Throwable::class.java, IOException())
+    )
+    Assert.assertEquals(
+      loginViewModel.app.getString(R.string.invalid_login_credentials),
+      loginViewModel.loginError.value
+    )
   }
 }
