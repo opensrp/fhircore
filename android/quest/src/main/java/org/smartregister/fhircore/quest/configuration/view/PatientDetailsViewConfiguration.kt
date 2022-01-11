f688a996e485f5a726a04b5d82e435c82e6dc962
Syed Owais Ali <owais.ali@venturedive.com>
2022-01-11 21:19:59 +0500

    removed unused info

--- android/quest/src/main/java/org/smartregister/fhircore/quest/configuration/view/PatientDetailsViewConfiguration.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/configuration/view/PatientDetailsViewConfiguration.kt
@@ -25,25 +25,21 @@ import org.smartregister.fhircore.engine.configuration.Configuration
 data class PatientDetailsViewConfiguration(
   override val appId: String,
   override val classification: String,
-  val clientInfo: String = "Patient name, F, 30",
   val contentTitle: String = "Responses",
   val valuePrefix: String = "G6PD ",
-  val parser: String,
-  val rows: List<List<Filter>>? = null
+  val parser: String
 ) : Configuration
 
 @Stable
 fun patientDetailsViewConfigurationOf(
   appId: String = "quest",
   classification: String = "patient_details",
-  clientInfo: String = "Patient name, F, 30",
   contentTitle: String = "Responses",
   valuePrefix: String = "G6PD "
 ) =
   PatientDetailsViewConfiguration(
     appId = appId,
     classification = classification,
-    clientInfo = clientInfo,
     contentTitle = contentTitle,
     valuePrefix = valuePrefix,
     parser = "G6PDDetailConfigParser"
