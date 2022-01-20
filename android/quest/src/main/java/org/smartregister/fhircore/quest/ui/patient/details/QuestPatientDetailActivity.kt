862403ad5f6cb1513e931ee4e65e5f01330e6776
maimoona.kausar <maimoonak.mk@gmail.com>
2022-01-20 14:57:56 +0500

    970 - temporary solution to open details after questionnaire

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/patient/details/QuestPatientDetailActivity.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/patient/details/QuestPatientDetailActivity.kt
@@ -16,12 +16,14 @@
 
 package org.smartregister.fhircore.quest.ui.patient.details
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.StringRes
+import ca.uhn.fhir.context.FhirContext
 import com.google.android.fhir.logicalId
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -34,6 +36,7 @@ import org.smartregister.fhircore.engine.cql.LibraryEvaluator.Companion.OUTPUT_P
 import org.smartregister.fhircore.engine.ui.base.AlertDialogue
 import org.smartregister.fhircore.engine.ui.base.BaseMultiLanguageActivity
 import org.smartregister.fhircore.engine.ui.questionnaire.QuestionnaireActivity
+import org.smartregister.fhircore.engine.ui.questionnaire.QuestionnaireActivity.Companion.QUESTIONNAIRE_RESPONSE
 import org.smartregister.fhircore.engine.ui.questionnaire.QuestionnaireConfig
 import org.smartregister.fhircore.engine.ui.theme.AppTheme
 import org.smartregister.fhircore.engine.util.extension.showToast
@@ -134,9 +137,29 @@ class QuestPatientDetailActivity : BaseMultiLanguageActivity() {
       )
   }
 
+  // TODO https://github.com/opensrp/fhircore/issues/961
+  // allow handling the data back and forth between activities via workflow or config
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (resultCode == Activity.RESULT_OK)
+      if (configurationRegistry.appId == "g6pd") {
+        data?.getStringExtra(QUESTIONNAIRE_RESPONSE)?.let {
+          val response = FhirContext.forR4().newJsonParser().parseResource(it) as QuestionnaireResponse
+          response.contained.find { it.resourceType == ResourceType.Encounter }?.logicalId?.let {
+            startActivity(
+              Intent(this, SimpleDetailsActivity::class.java).apply {
+                putExtra(RECORD_ID_ARG, it.replace("#", ""))
+              }
+            )
+          }
+        }
+      }
+  }
+
   private fun launchQuestionnaireForm(questionnaireConfig: QuestionnaireConfig?) {
     if (questionnaireConfig != null) {
-      startActivity(
+      startActivityForResult(
         Intent(this, QuestionnaireActivity::class.java).apply {
           putExtras(
             QuestionnaireActivity.intentArgs(
@@ -144,7 +167,8 @@ class QuestPatientDetailActivity : BaseMultiLanguageActivity() {
               formName = questionnaireConfig.identifier
             )
           )
-        }
+        },
+        0
       )
     }
   }
