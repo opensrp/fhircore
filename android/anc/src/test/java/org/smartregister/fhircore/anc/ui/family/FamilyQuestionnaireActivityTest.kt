4d495713822764b444cc77041a80e0f49507c606
AbdulWahabMemon <abdulwahab.memon@venturedive.com>
2022-02-11 18:20:33 +0500

    spotlessApply

--- android/anc/src/test/java/org/smartregister/fhircore/anc/ui/family/FamilyQuestionnaireActivityTest.kt
+++ android/anc/src/test/java/org/smartregister/fhircore/anc/ui/family/FamilyQuestionnaireActivityTest.kt
@@ -39,7 +39,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.robolectric.Robolectric
