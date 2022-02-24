d20423e9b1b7f7f92a8a97830bb9f3f9a1ef8fe0
owais <owais.ali@venturedive.com>
2022-02-24 23:42:25 +0500

    remove unused classification from QuestConfigClassification

--- android/quest/src/main/java/org/smartregister/fhircore/quest/util/QuestConfigClassification.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/util/QuestConfigClassification.kt
@@ -24,7 +24,6 @@ enum class QuestConfigClassification : ConfigClassification {
   TEST_RESULT_DETAIL_VIEW,
   PATIENT_DETAILS_VIEW,
   CONTROL_TEST_DETAILS_VIEW,
-  REGISTER_NAVIGATION,
   RESULT_DETAILS_NAVIGATION,
   PATIENT_TASK_REGISTER;
   override val classification: String = name.lowercase()
