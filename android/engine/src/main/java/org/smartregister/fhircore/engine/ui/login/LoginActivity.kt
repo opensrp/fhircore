836ecd9ca3df05ef375d4517f85530a47f87027b
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2022-03-07 12:16:13 +0100

    Pin login redirection issue fix (#1127)
    * - After user/password login user will be directed to home
    
    * - refactor
    
    * - refactor
    
    * - Removed failing test.
    
    * - Failing test fix.
    
    * - Add test cases
    
    * - Update test cases

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/login/LoginActivity.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/login/LoginActivity.kt
@@ -49,18 +49,20 @@ class LoginActivity :
     super.onCreate(savedInstanceState)
     loginService.loginActivity = this
     loginViewModel.apply {
-      navigateToHome.observe(
-        this@LoginActivity,
-        {
-          syncBroadcaster.runSync()
-          if (loginViewModel.loginViewConfiguration.value?.enablePin == true) {
-            loginService.navigateToPinLogin(goForSetup = true)
-          } else {
+      navigateToHome.observe(this@LoginActivity) {
+        if (loginViewModel.loginViewConfiguration.value?.enablePin == true) {
+          val lastPinExist = loginViewModel.accountAuthenticator.hasActivePin()
+          if (lastPinExist) {
+            loginViewModel.sharedPreferences.write(FORCE_LOGIN_VIA_USERNAME, false)
             loginService.navigateToHome()
+          } else {
+            loginService.navigateToPinLogin(goForSetup = true)
           }
+        } else {
+          loginService.navigateToHome()
         }
-      )
-      launchDialPad.observe(this@LoginActivity, { if (!it.isNullOrEmpty()) launchDialPad(it) })
+      }
+      launchDialPad.observe(this@LoginActivity) { if (!it.isNullOrEmpty()) launchDialPad(it) }
       appLogoResourceFile = getApplicationConfiguration().appLogoIconResourceFile
     }
 
