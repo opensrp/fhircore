e11bdc2ef3bf4017f799542020a95de06e19c6f3
Elly Kitoto <junkmailstoelly@gmail.com>
2022-04-01 20:52:17 +0300

    Update child screen UI
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/anc/src/main/java/org/smartregister/fhircore/anc/ui/details/child/ChildProfileViewDataMapper.kt
+++ android/anc/src/main/java/org/smartregister/fhircore/anc/ui/details/child/ChildProfileViewDataMapper.kt
@@ -23,10 +23,10 @@ import javax.inject.Inject
 import org.hl7.fhir.r4.model.Patient
 import org.hl7.fhir.r4.model.Task
 import org.smartregister.fhircore.anc.R
-import org.smartregister.fhircore.anc.ui.details.child.components.InfoColor
 import org.smartregister.fhircore.anc.ui.details.child.model.ChildProfileRowItem
 import org.smartregister.fhircore.anc.ui.details.child.model.ChildProfileViewData
 import org.smartregister.fhircore.engine.data.domain.util.DomainMapper
+import org.smartregister.fhircore.engine.ui.theme.BlueTextColor
 import org.smartregister.fhircore.engine.ui.theme.OverdueColor
 import org.smartregister.fhircore.engine.util.extension.extractAge
 import org.smartregister.fhircore.engine.util.extension.extractGender
@@ -46,22 +46,24 @@ constructor(
     val child = dto.child
     val tasks = dto.tasks
 
+    val name = child.extractName()
     return ChildProfileViewData(
       id = child.logicalId,
       identifier = child.identifierFirstRep.value ?: child.logicalId,
-      name = child.extractName(),
+      name = name,
       sex = child.extractGender(context) ?: "",
-      status = "STATUSS", // TODO??????
+      status = "",
       age = child.extractAge(),
       dob = child.birthDate.makeItReadable(),
       tasks =
         tasks.map {
           ChildProfileRowItem(
             id = it.id,
-            title = ">>>>>>>>",
-            subtitle = it.description,
+            title = context.getString(R.string.child_routine_visit),
+            subtitle =
+              context.getString(R.string.due_on, it.executionPeriod.start.makeItReadable()),
             actionButtonText = context.getString(R.string.child_visit_button_title),
-            actionButtonColor = if (!it.hasPastEnd()) InfoColor else OverdueColor,
+            actionButtonColor = if (!it.hasPastEnd()) BlueTextColor else OverdueColor,
           )
         }
     )
--- android/anc/src/main/java/org/smartregister/fhircore/anc/ui/details/child/components/PersonalData.kt
+++ android/anc/src/main/java/org/smartregister/fhircore/anc/ui/details/child/components/PersonalData.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,6 +35,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.smartregister.fhircore.anc.R
 import org.smartregister.fhircore.anc.ui.details.child.model.ChildProfileViewData
 
@@ -42,47 +44,44 @@ fun PersonalData(
   childProfileViewData: ChildProfileViewData,
   modifier: Modifier = Modifier,
 ) {
-  val statusTextColor = Color.Gray.copy(alpha = 0.8f)
-
-  Column(modifier = modifier.fillMaxWidth().padding(16.dp)) {
-    Text(
-      text = childProfileViewData.name,
-      fontWeight = FontWeight.Bold,
-    )
-    if (childProfileViewData.status.isNotEmpty()) {
+  val statusTextColor = Color.Gray
+  Card(elevation = 3.dp) {
+    Column(modifier = modifier.fillMaxWidth().padding(16.dp)) {
+      Text(text = childProfileViewData.name, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+      if (childProfileViewData.status.isNotEmpty()) {
+        Text(
+          text = childProfileViewData.status,
+          color = Color.Gray,
+          modifier = modifier.padding(vertical = 14.dp),
+          fontSize = 20.sp
+        )
+      }
       Text(
-        text = childProfileViewData.status,
-        color = Color.Gray,
-        modifier = modifier.padding(vertical = 10.dp)
+        text = stringResource(R.string.id, childProfileViewData.identifier),
+        color = statusTextColor,
+        fontSize = 20.sp
       )
-    }
-    Text(
-      text = stringResource(R.string.id, childProfileViewData.identifier),
-      color = statusTextColor
-    )
-    Spacer(modifier = modifier.height(16.dp))
-    Row(
-      horizontalArrangement = Arrangement.SpaceBetween,
-      modifier =
-        modifier
-          .clip(RoundedCornerShape(size = 8.dp))
-          .background(Color.LightGray.copy(alpha = 0.3f))
-    ) {
-      OtherDetailsItem(
-        title = stringResource(R.string.sex),
-        value = childProfileViewData.sex,
-        statusTextColor = statusTextColor
-      )
-      OtherDetailsItem(
-        title = stringResource(R.string.age),
-        value = childProfileViewData.age,
-        statusTextColor = statusTextColor
-      )
-      OtherDetailsItem(
-        title = stringResource(R.string.dob),
-        value = childProfileViewData.dob,
-        statusTextColor = statusTextColor
-      )
+      Spacer(modifier = modifier.height(16.dp))
+      Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.clip(RoundedCornerShape(size = 8.dp)).background(Color(0xFFF5F5F5))
+      ) {
+        OtherDetailsItem(
+          title = stringResource(R.string.sex),
+          value = childProfileViewData.sex,
+          statusTextColor = statusTextColor
+        )
+        OtherDetailsItem(
+          title = stringResource(R.string.age),
+          value = childProfileViewData.age,
+          statusTextColor = statusTextColor
+        )
+        OtherDetailsItem(
+          title = stringResource(R.string.dob),
+          value = childProfileViewData.dob,
+          statusTextColor = statusTextColor
+        )
+      }
     }
   }
 }
@@ -95,8 +94,8 @@ private fun OtherDetailsItem(
   statusTextColor: Color
 ) {
   Column(modifier = modifier.padding(16.dp)) {
-    Text(text = title, modifier.padding(bottom = 4.dp), color = statusTextColor)
-    Text(text = value)
+    Text(text = title, modifier.padding(bottom = 4.dp), color = statusTextColor, fontSize = 20.sp)
+    Text(text = value, fontSize = 20.sp)
   }
 }
 
