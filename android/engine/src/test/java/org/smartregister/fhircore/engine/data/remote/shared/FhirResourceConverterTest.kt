8808497d45c0fc3b2363ec7f1ba18d3192db34ea
Martin Ndegwa <ndegwamartin@gmail.com>
2022-04-27 09:00:57 +0300

    Add unit tests ✅ - More test cases - Refactor deprecated capitalization implementation

--- android/engine/src/test/java/org/smartregister/fhircore/engine/data/remote/shared/FhirResourceConverterTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/data/remote/shared/FhirResourceConverterTest.kt
@@ -19,20 +19,53 @@ package org.smartregister.fhircore.engine.data.remote.shared
 import ca.uhn.fhir.context.FhirContext
 import ca.uhn.fhir.context.FhirVersionEnum
 import com.google.android.fhir.logicalId
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import java.lang.reflect.Type
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.hl7.fhir.r4.model.Patient
 import org.hl7.fhir.r4.model.StringType
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.smartregister.fhircore.engine.data.remote.fhir.resource.FhirConverter
+import org.smartregister.fhircore.engine.data.remote.fhir.resource.FhirConverterFactory
+import retrofit2.Retrofit
 
 class FhirResourceConverterTest {
+  @MockK lateinit var type: Type
+
+  @MockK lateinit var retrofit: Retrofit
+
+  lateinit var annotations: Array<Annotation>
+  @Before
+  fun setUp() {
+    MockKAnnotations.init(this, relaxed = true, relaxUnitFun = true)
+
+    annotations = arrayOf()
+  }
+
   @Test
   fun testFhirConverterShouldConvertResourceCorrectly() {
     val parser = FhirContext.forCached(FhirVersionEnum.R4).newJsonParser()
     val input = parser.encodeResourceToString(buildPatient()).toByteArray().toResponseBody()
 
     val result = FhirConverter(parser).convert(input) as Patient
+    Assert.assertEquals("John", result.nameFirstRep.given[0].value)
+    Assert.assertEquals("Doe", result.nameFirstRep.family)
+    Assert.assertEquals("12345", result.logicalId)
+  }
+
+  @Test
+  fun testFhirConverterFactoryShouldConvertResourceCorrectly() {
+    val parser = FhirContext.forCached(FhirVersionEnum.R4).newJsonParser()
+    val input = parser.encodeResourceToString(buildPatient()).toByteArray().toResponseBody()
+
+    val result =
+      FhirConverterFactory(parser)
+        .responseBodyConverter(type, annotations, retrofit)
+        .convert(input) as
+        Patient
     Assert.assertEquals("John", result.nameFirstRep.given[0].value)
     Assert.assertEquals("Doe", result.nameFirstRep.family)
     Assert.assertEquals("12345", result.logicalId)
