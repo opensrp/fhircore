12caee98f491723046ba73871b2da0358bb8dd54
Fikri Yurcel Milano <fikrimilano1@gmail.com>
2022-05-06 18:06:33 +0700

    Address review comments

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/appsetting/AppSettingActivity.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/appsetting/AppSettingActivity.kt
@@ -81,12 +81,11 @@ class AppSettingActivity : AppCompatActivity() {
 
     with(appSettingViewModel) {
       this.fetchConfigs.observe(this@AppSettingActivity) {
-        if (it == true) {
-          if (appId.value?.isNotBlank() == true)
-            lifecycleScope.launch(dispatcherProvider.io()) {
-              fetchConfigurations(appId.value!!, this@AppSettingActivity)
-            }
-        } else {
+        if (it == true && appId.value?.isNotBlank() == true)
+          lifecycleScope.launch(dispatcherProvider.io()) {
+            fetchConfigurations(appId.value!!, this@AppSettingActivity)
+          }
+        else {
           loadConfigurations(true)
         }
       }
