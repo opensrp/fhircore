7bcdb0617c2e32bea94ead7b5114b64d5f2c96a6
Qazi Abubakar <qma_2007@yahoo.com>
2022-08-01 14:33:43 +0500

    - Remove mockedGender

--- android/quest/src/test/java/org/smartregister/fhircore/quest/util/mappers/MeasureReportPatientViewDataMapperTest.kt
+++ android/quest/src/test/java/org/smartregister/fhircore/quest/util/mappers/MeasureReportPatientViewDataMapperTest.kt
@@ -16,12 +16,8 @@
 
 package org.smartregister.fhircore.quest.util.mappers
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import io.mockk.every
-import io.mockk.mockk
 import javax.inject.Inject
 import org.hl7.fhir.r4.model.Enumerations
 import org.hl7.fhir.r4.model.Patient
@@ -29,7 +25,6 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.smartregister.fhircore.engine.R
 import org.smartregister.fhircore.engine.domain.model.ResourceData
 import org.smartregister.fhircore.quest.robolectric.RobolectricTest
 
@@ -43,25 +38,15 @@ class MeasureReportPatientViewDataMapperTest : RobolectricTest() {
   @Before
   fun setup() {
     hiltRule.inject()
-    val mockedGender = mockk<Enumerations.AdministrativeGender>("MALE")
-    every { mockedGender.translateGender(ApplicationProvider.getApplicationContext()) } returns
-      "MALE"
   }
-
-  fun Enumerations.AdministrativeGender.translateGender(context: Context) =
-    when (this) {
-      Enumerations.AdministrativeGender.MALE -> context.getString(R.string.male)
-      Enumerations.AdministrativeGender.FEMALE -> context.getString(R.string.female)
-      else -> context.getString(R.string.unknown)
-    }
 
   @Test
   fun testMapToOutputModelPatient() {
     val dto =
       ResourceData(
         baseResource =
-          "patient-registration-questionnaire/sample/patient.json".parseSampleResourceFromFile() as
-            Patient
+          "patient-registration-questionnaire/sample/patient.json".parseSampleResourceFromFile()
+            as Patient
       )
     val profileViewDataHiv = measureReportPatientViewDataMapper.transformInputToOutputModel(dto)
     with(profileViewDataHiv) {
