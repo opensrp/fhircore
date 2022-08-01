c90048869c88ef7703190956c0f986a359d36e61
fikrimilano <fikrimilano1@gmail.com>
2022-08-01 16:20:28 +0700

    Load register configs from Asset

--- android/engine/src/test/java/org/smartregister/fhircore/engine/data/local/register/RegisterRepositoryTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/data/local/register/RegisterRepositoryTest.kt
@@ -16,9 +16,13 @@
 
 package org.smartregister.fhircore.engine.data.local.register
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import com.google.android.fhir.FhirEngine
 import com.google.android.fhir.logicalId
 import com.google.android.fhir.search.Search
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
@@ -27,6 +31,7 @@ import io.mockk.mockkObject
 import io.mockk.spyk
 import io.mockk.unmockkObject
 import io.mockk.verify
+import javax.inject.Inject
 import kotlinx.coroutines.runBlocking
 import org.hl7.fhir.r4.model.CarePlan
 import org.hl7.fhir.r4.model.Condition
@@ -38,26 +43,36 @@ import org.hl7.fhir.r4.model.Reference
 import org.hl7.fhir.r4.model.ResourceType
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.smartregister.fhircore.engine.app.fakes.Faker
 import org.smartregister.fhircore.engine.configuration.ConfigurationRegistry
+import org.smartregister.fhircore.engine.robolectric.RobolectricTest
 import org.smartregister.fhircore.engine.rulesengine.RulesFactory
 import org.smartregister.fhircore.engine.util.DefaultDispatcherProvider
 import org.smartregister.fhircore.engine.util.fhirpath.FhirPathDataExtractor
 
-class RegisterRepositoryTest {
+@HiltAndroidTest
+class RegisterRepositoryTest : RobolectricTest() {
+
+  @get:Rule var hiltRule = HiltAndroidRule(this)
+
+  var context: Context = ApplicationProvider.getApplicationContext()
 
   private lateinit var fhirEngine: FhirEngine
   private lateinit var dispatcherProvider: DefaultDispatcherProvider
-  private lateinit var configurationRegistry: ConfigurationRegistry
+
+  @Inject lateinit var configurationRegistry: ConfigurationRegistry
+
   private lateinit var rulesFactory: RulesFactory
   private lateinit var registerRepository: RegisterRepository
 
   @Before
   fun setUp() {
+    hiltRule.inject()
+
     fhirEngine = mockk()
     dispatcherProvider = DefaultDispatcherProvider()
-    configurationRegistry = mockk()
+    //    configurationRegistry = mockk()
     rulesFactory = mockk()
     registerRepository =
       spyk(
@@ -72,9 +87,6 @@ class RegisterRepositoryTest {
 
   @Test
   fun loadRegisterDataGivenRelatedResourceHasNoFhirPathExpression() {
-    every { registerRepository.retrieveRegisterConfiguration("patientRegister") } returns
-      Faker.generatePatientRegisterConfiguration()
-
     val patient =
       Patient().apply {
         id = "12345"
@@ -101,6 +113,7 @@ class RegisterRepositoryTest {
       )
 
     runBlocking {
+      configurationRegistry.loadConfigurations("app/debug", context) { Assert.assertTrue(it) }
       val listResourceData = registerRepository.loadRegisterData(1, "patientRegister")
       val resourceData = listResourceData.first()
 
@@ -135,9 +148,6 @@ class RegisterRepositoryTest {
 
   @Test
   fun loadRegisterDataGivenRelatedResourceHasFhirPathExpression() {
-    every { registerRepository.retrieveRegisterConfiguration("householdRegister") } returns
-      Faker.generateHouseholdRegisterConfiguration()
-
     val patient =
       Patient().apply {
         id = "12345"
@@ -177,6 +187,7 @@ class RegisterRepositoryTest {
     } returns mapOf(Pair("familyName", "Snow"))
 
     runBlocking {
+      configurationRegistry.loadConfigurations("app/debug", context) { Assert.assertTrue(it) }
       val listResourceData = registerRepository.loadRegisterData(1, "householdRegister")
       val resourceData = listResourceData.first()
 
