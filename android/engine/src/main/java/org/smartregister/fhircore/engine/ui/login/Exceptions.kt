34e6fdbbbcddb6e63a2243b91774580531c9b83c
Christopher Seven Phiri <chris7phiri@gmail.com>
2022-09-06 14:39:41 +0200

    Update Exceptions.kt

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/login/Exceptions.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/login/Exceptions.kt
@@ -16,6 +16,7 @@
 
 package org.smartregister.fhircore.engine.ui.login
 
-class InvalidCredentialsException(cause: Throwable? = null) : Exception("Invalid login credentials", cause)
+class InvalidCredentialsException(cause: Throwable? = null) :
+  Exception("Invalid login credentials", cause)
 
 class LoginNetworkException(cause: Throwable? = null) : Exception("Network call failed", cause)
