c16f97eec095de8a5a710113d6d8f8ac10a3d354
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2022-09-15 16:22:13 +0300

    [MWCore] Add finish visit functionality (#1591)
    * 1585: Add finish visit functionality
    
    * 1585: Adjust height and size of finish visit button

--- android/quest/src/main/java/org/smartregister/fhircore/quest/util/mappers/ProfileViewDataMapper.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/util/mappers/ProfileViewDataMapper.kt
@@ -98,6 +98,7 @@ class ProfileViewDataMapper @Inject constructor(@ApplicationContext val context:
                   else it.status.retrieveColorCode(),
                 actionButtonColor = it.status.retrieveColorCode(),
                 actionButtonText = it.description,
+                subtitleStatus = it.status.name
               )
             }
         )
