fcb808618004792d6a412450283b46aa78881422
qaziabubakar-vd <72507786+qaziabubakar-vd@users.noreply.github.com>
2022-09-15 12:56:31 +0500

    1619 - Fix language switching from UserSettingScreen (#1621)
    * Fix language switching from UserSettingScreen.kt
    Update the UserSettingViewModel.kt to follow event pattern as quest app
    
    * Resolve conflicts
    
    * Update tests for UserSettingViewModel.kt
    
    * Update event name
    
    * Update activity refresh
    
    * Fix tests for UserSettingScreen.kt
    
    * Update params for UserSettingScreen
    
    * Fix tests for UserSettingScreen.kt
    
    * Fix tests for UserSettingScreen.kt
    
    * Update tests for UserSettingScreen.kt
    
    Co-authored-by: Qazi Abubakar <qma_2007@yahoo.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/usersetting/UserSettingViewModel.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/usersetting/UserSettingViewModel.kt
@@ -16,19 +16,20 @@
 
 package org.smartregister.fhircore.engine.ui.usersetting
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.util.Locale
 import javax.inject.Inject
 import org.smartregister.fhircore.engine.auth.AccountAuthenticator
 import org.smartregister.fhircore.engine.configuration.ConfigurationRegistry
-import org.smartregister.fhircore.engine.domain.model.Language
 import org.smartregister.fhircore.engine.sync.SyncBroadcaster
 import org.smartregister.fhircore.engine.util.SecureSharedPreference
 import org.smartregister.fhircore.engine.util.SharedPreferenceKey
 import org.smartregister.fhircore.engine.util.SharedPreferencesHelper
 import org.smartregister.fhircore.engine.util.extension.fetchLanguages
+import org.smartregister.fhircore.engine.util.extension.getActivity
+import org.smartregister.fhircore.engine.util.extension.refresh
+import org.smartregister.fhircore.engine.util.extension.setAppLocale
 
 @HiltViewModel
 class UserSettingViewModel
@@ -43,19 +44,6 @@ constructor(
 
   val languages by lazy { configurationRegistry.fetchLanguages() }
 
-  val onLogout = MutableLiveData<Boolean?>(null)
-
-  val language = MutableLiveData<Language?>(null)
-
-  fun runSync() {
-    syncBroadcaster.runSync()
-  }
-
-  fun logoutUser() {
-    onLogout.postValue(true)
-    accountAuthenticator.logout()
-  }
-
   fun retrieveUsername(): String? = secureSharedPreference.retrieveSessionUsername()
 
   fun allowSwitchingLanguages() = languages.size > 1
@@ -67,8 +55,21 @@ constructor(
       )
       .displayName
 
-  fun setLanguage(language: Language) {
-    sharedPreferencesHelper.write(SharedPreferenceKey.LANG.name, language.tag)
-    this.language.postValue(language)
+  fun onEvent(event: UserSettingsEvent) {
+    when (event) {
+      is UserSettingsEvent.Logout -> {
+        accountAuthenticator.logout()
+      }
+      is UserSettingsEvent.SyncData -> {
+        syncBroadcaster.runSync()
+      }
+      is UserSettingsEvent.SwitchLanguage -> {
+        sharedPreferencesHelper.write(SharedPreferenceKey.LANG.name, event.language.tag)
+        event.context.run {
+          setAppLocale(event.language.tag)
+          getActivity()?.refresh()
+        }
+      }
+    }
   }
 }
