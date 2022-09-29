3cfc5755e99a530dfad5733dd79817ae57f48f79
Francis Odhiambo Otieno <4540684+f-odhiambo@users.noreply.github.com>
2022-09-29 10:14:51 +0300

    Updated engine build.gradle files with latest SDK artifacts

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/main/AppMainActivity.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/main/AppMainActivity.kt
@@ -46,6 +46,7 @@ import org.smartregister.fhircore.quest.R
 import org.smartregister.fhircore.quest.navigation.NavigationArg
 import org.smartregister.fhircore.quest.ui.questionnaire.QuestionnaireActivity
 import timber.log.Timber
+import java.time.temporal.ChronoUnit
 
 @AndroidEntryPoint
 @ExperimentalMaterialApi
@@ -161,6 +162,10 @@ open class AppMainActivity : BaseMultiLanguageActivity(), OnSyncListener {
       }
       is State.Finished -> {
         showToast(getString(R.string.sync_completed))
+        showToast(getString(R.string.sync_completed))
+        val totalTime = ChronoUnit.SECONDS.between(appMainViewModel.syncStartedAt, state.result.timestamp)
+        //showToast(getString(R.string.sync_completed) + " in "+totalTime + " seconds")
+        Timber.d(getString(R.string.sync_completed) + " in "+totalTime + " seconds")
         appMainViewModel.run {
           onEvent(
             AppMainEvent.UpdateSyncState(state, formatLastSyncTimestamp(state.result.timestamp))
