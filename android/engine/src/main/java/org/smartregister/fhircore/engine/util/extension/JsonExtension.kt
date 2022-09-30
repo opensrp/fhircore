84b1aa03574d232948b70bb12734e5b5ee0177f7
maimoonak <maimoonak.mk@gmail.com>
2022-09-30 18:41:04 +0500

    Add kdoc for json extension

--- android/engine/src/main/java/org/smartregister/fhircore/engine/util/extension/JsonExtension.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/util/extension/JsonExtension.kt
@@ -28,14 +28,26 @@ val json = Json {
   useAlternativeNames = true
 }
 
-/** Decode string to an entity of type [T] */
+/**
+ * Decode string to an entity of type [T]
+ * @param jsonInstance the custom json object used to decode the specified string
+ * @return the decoded object of type [T]
+ */
 inline fun <reified T> String.decodeJson(jsonInstance: Json? = null): T =
   jsonInstance?.decodeFromString(this) ?: json.decodeFromString(this)
 
-/** Decode string to an entity of type [T] */
+/**
+ * Decode string to an entity of type [T] or return null if json is invalid
+ * @param jsonInstance the custom json object used to decode the specified string
+ * @return the decoded object of type [T]
+ */
 inline fun <reified T> String.tryDecodeJson(jsonInstance: Json? = null): T? =
   kotlin.runCatching { this.decodeJson<T>(jsonInstance) }.onFailure { Timber.w(it) }.getOrNull()
 
-/** Encode the type [T] into a Json string */
+/**
+ * Encode the type [T] into a Json string
+ * @param jsonInstance the custom json object used to decode the specified string
+ * @return the encoded json string from given type [T]
+ */
 inline fun <reified T> T.encodeJson(jsonInstance: Json? = null): String =
   jsonInstance?.encodeToString(this) ?: json.encodeToString(this)
