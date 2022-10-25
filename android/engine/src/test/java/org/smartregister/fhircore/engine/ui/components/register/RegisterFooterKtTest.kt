8a4199748221048f5c88c44f5c98f3c23a0bcff8
allay <a.onchuru@gmail.com>
2022-10-25 14:24:23 +0300

    Add RegisterFooter view test

--- android/engine/src/test/java/org/smartregister/fhircore/engine/ui/components/register/RegisterFooterKtTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/ui/components/register/RegisterFooterKtTest.kt
@@ -1,8 +1,14 @@
 package org.smartregister.fhircore.engine.ui.components.register
 
+import android.app.Application
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.core.app.ApplicationProvider
 import org.junit.Rule
 import org.junit.Test
+import org.smartregister.fhircore.engine.R
 import org.smartregister.fhircore.engine.robolectric.RobolectricTest
 
 class RegisterFooterKtTest : RobolectricTest() {
@@ -14,13 +20,45 @@ class RegisterFooterKtTest : RobolectricTest() {
     composeRule.setContent {
       RegisterFooter(
         resultCount = 10,
-        currentPage = 1,
+        currentPage = 2,
         pagesCount = 5,
         previousButtonClickListener = {},
         nextButtonClickListener = {}
       )
     }
 
-    
+    composeRule.onNodeWithTag(SEARCH_FOOTER_TAG).assertExists()
+    composeRule.onNodeWithTag(SEARCH_FOOTER_TAG).assertIsDisplayed()
+
+    // Since {currentPage} > 1
+    composeRule.onNodeWithTag(SEARCH_FOOTER_PREVIOUS_BUTTON_TAG).assertExists()
+    composeRule.onNodeWithTag(SEARCH_FOOTER_PREVIOUS_BUTTON_TAG).assertIsDisplayed()
+
+    composeRule
+            .onNodeWithTag(SEARCH_FOOTER_PREVIOUS_BUTTON_TAG)
+            .assertTextEquals(
+                    ApplicationProvider.getApplicationContext<Application>().getString(R.string.str_previous)
+            )
+
+    composeRule.onNodeWithTag(SEARCH_FOOTER_PAGINATION_TAG).assertExists()
+    composeRule.onNodeWithTag(SEARCH_FOOTER_PAGINATION_TAG).assertIsDisplayed()
+
+    composeRule
+            .onNodeWithTag(SEARCH_FOOTER_PAGINATION_TAG)
+            .assertTextEquals(
+                    ApplicationProvider.getApplicationContext<Application>().getString(R.string.str_page_info, 2, 5)
+            )
+
+    composeRule.onNodeWithTag(SEARCH_FOOTER_NEXT_BUTTON_TAG).assertExists()
+    composeRule.onNodeWithTag(SEARCH_FOOTER_NEXT_BUTTON_TAG).assertIsDisplayed()
+
+    composeRule
+            .onNodeWithTag(SEARCH_FOOTER_NEXT_BUTTON_TAG)
+            .assertTextEquals(
+                    ApplicationProvider.getApplicationContext<Application>().getString(R.string.str_next)
+            )
+
+
+
   }
 }
