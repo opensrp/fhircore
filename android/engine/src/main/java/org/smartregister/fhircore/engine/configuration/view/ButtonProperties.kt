4527bcac7265e9ba6e1d6932381d27349cc79e8d
Elly Kitoto <junkmailstoelly@gmail.com>
2022-10-26 17:11:59 +0300

    Add fill maximum width attribute
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/view/ButtonProperties.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/view/ButtonProperties.kt
@@ -29,4 +29,5 @@ data class ButtonProperties(
   val smallSized: Boolean = false,
   val fontSize: Float = 10.0f,
   val actions: List<ActionConfig> = emptyList(),
+  val fillMaxWidth: Boolean = true
 ) : ViewProperties()
