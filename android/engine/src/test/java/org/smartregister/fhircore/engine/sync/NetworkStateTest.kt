b6990797dfff0b213a7cb267e059234dad5e8bf0
Comfort Mwalija <42068747+calmwalija@users.noreply.github.com>
2022-11-01 15:20:37 +0200

    [Quest/Malawi Core] Check internet connectivity before sync (#1796)
    * Check internet connectivity before sync

---
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/sync/NetworkStateTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.engine.sync
+
+import android.app.Application
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Rule
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.shadows.ShadowNetworkCapabilities
+import org.smartregister.fhircore.engine.robolectric.RobolectricTest
+
+internal class NetworkStateTest : RobolectricTest() {
+
+  private val context: Application = ApplicationProvider.getApplicationContext()
+
+  @get:Rule var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+  @Suppress("DEPRECATION")
+  @Test
+  fun invoke() {
+    val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
+
+    val networkCapabilities = ShadowNetworkCapabilities.newInstance()
+    shadowOf(networkCapabilities).addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+    shadowOf(networkCapabilities).addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+    shadowOf(connectivityManager)
+      .setNetworkCapabilities(connectivityManager.activeNetwork, networkCapabilities)
+
+    assertTrue(connectivityManager.activeNetworkInfo?.type != ConnectivityManager.TYPE_WIFI)
+    assertTrue(connectivityManager.activeNetworkInfo?.type == ConnectivityManager.TYPE_MOBILE)
+    assertTrue(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI))
+    assertTrue(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+  }
+}
