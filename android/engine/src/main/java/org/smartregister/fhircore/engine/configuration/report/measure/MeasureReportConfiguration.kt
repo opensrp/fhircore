45ed62ec7759cd03af5e1fcdfb98b85fdf41456b
Elly Kitoto <junkmailstoelly@gmail.com>
2022-11-11 14:30:33 +0300

    Add reportModule property to MeasureReportConfiguration
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/report/measure/MeasureReportConfiguration.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/report/measure/MeasureReportConfiguration.kt
@@ -28,5 +28,6 @@ data class MeasureReportConfiguration(
   val registerId: String,
   val registerDate: String? = null,
   val showFixedRangeSelection: Boolean? = null,
+  val reportModule: String? = null,
   val reports: List<MeasureReportConfig> = emptyList()
 ) : Configuration()
