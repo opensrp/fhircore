77821689900e25acb7f1db2b67214155815e2723
SebaMutuku <sebastian.muthiani@gmail.com>
2022-11-18 16:17:03 +0300

    Amending package.json

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/fhircore/__docusaurus/debug',
+    component: ComponentCreator('/fhircore/__docusaurus/debug', '955'),
+    exact: true
+  },
+  {
+    path: '/fhircore/__docusaurus/debug/config',
+    component: ComponentCreator('/fhircore/__docusaurus/debug/config', 'bdb'),
+    exact: true
+  },
+  {
+    path: '/fhircore/__docusaurus/debug/content',
+    component: ComponentCreator('/fhircore/__docusaurus/debug/content', 'aef'),
+    exact: true
+  },
+  {
+    path: '/fhircore/__docusaurus/debug/globalData',
+    component: ComponentCreator('/fhircore/__docusaurus/debug/globalData', '3ef'),
+    exact: true
+  },
+  {
+    path: '/fhircore/__docusaurus/debug/metadata',
+    component: ComponentCreator('/fhircore/__docusaurus/debug/metadata', 'c1c'),
+    exact: true
+  },
+  {
+    path: '/fhircore/__docusaurus/debug/registry',
+    component: ComponentCreator('/fhircore/__docusaurus/debug/registry', 'fe9'),
+    exact: true
+  },
+  {
+    path: '/fhircore/__docusaurus/debug/routes',
+    component: ComponentCreator('/fhircore/__docusaurus/debug/routes', 'cb2'),
+    exact: true
+  },
+  {
     path: '/fhircore/',
     component: ComponentCreator('/fhircore/', '8e8'),
     routes: [
