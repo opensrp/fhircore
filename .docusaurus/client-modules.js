8f65305ddc73831571167f38fe0d826cd68f8439
SebaMutuku <sebastian.muthiani@gmail.com>
2022-11-18 12:06:24 +0300

    restoring files

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,5 @@
+export default [
+  require('/home/smutuku/Documents/Github/fhircore/node_modules/infima/dist/css/default/default.css'),
+  require('/home/smutuku/Documents/Github/fhircore/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/smutuku/Documents/Github/fhircore/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+];
--- docusaurus-web/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require('/home/smutuku/Documents/Github/fhircore/docusaurus-web/node_modules/infima/dist/css/default/default.css'),
-  require('/home/smutuku/Documents/Github/fhircore/docusaurus-web/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/smutuku/Documents/Github/fhircore/docusaurus-web/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-];
