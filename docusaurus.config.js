bb4abb0b8f678442c66bdcb9ea7fb9022b838045
SebaMutuku <sebastian.muthiani@gmail.com>
2022-11-22 10:03:37 +0300

    Amendment of name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Opensrp FhirCore Documentation',
-  tagline: 'Opensrp FhirCore Documentation',
+  title: 'FhirCore Documentation',
+  tagline: 'FhirCore Documentation',
   url: 'https://opensrp.github.io',
   baseUrl: '/fhircore/',
   trailingSlash:false,
