a3b8a480b2625337ab559a31947e3a7ce6f20c95
SebaMutuku <sebastian.muthiani@gmail.com>
2022-11-22 15:01:01 +0300

    clean push to gh-pages

--- .docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require('/home/smutuku/Documents/Github/fhircore/node_modules/infima/dist/css/default/default.css'),
-  require('/home/smutuku/Documents/Github/fhircore/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/smutuku/Documents/Github/fhircore/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-];
