75f58cb622d6ae1d5c5c5748f4cae495ad0f9a8c
Qazi Abubakar <qma_2007@yahoo.com>
2022-11-23 11:40:56 +0500

    Remove unnecessary OkHttpClientQualifiers

--- android/engine/src/main/java/org/smartregister/fhircore/engine/di/NetworkModule.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/di/NetworkModule.kt
@@ -43,24 +43,9 @@ class NetworkModule {
   @Provides fun provideGson(): Gson = GsonBuilder().setLenient().create()
 
   @Provides
-  @AuthOkHttpClientQualifier
   fun provideAuthOkHttpClient(oAuthInterceptor: OAuthInterceptor) =
     OkHttpClient.Builder()
       .addInterceptor(oAuthInterceptor)
-      .addInterceptor(
-        HttpLoggingInterceptor().apply {
-          level = HttpLoggingInterceptor.Level.BASIC
-          redactHeader(AUTHORIZATION)
-          redactHeader(COOKIE)
-        }
-      )
-      .build()
-
-  @Provides
-  @OkHttpClientQualifier
-  fun provideOkHttpClient(interceptor: OAuthInterceptor) =
-    OkHttpClient.Builder()
-      .addInterceptor(interceptor)
       .addInterceptor(
         HttpLoggingInterceptor().apply {
           level = HttpLoggingInterceptor.Level.BASIC
@@ -75,7 +60,7 @@ class NetworkModule {
 
   @Provides
   fun provideOauthService(
-    @AuthOkHttpClientQualifier okHttpClient: OkHttpClient,
+    okHttpClient: OkHttpClient,
     configService: ConfigService,
     gson: Gson
   ): OAuthService =
@@ -91,7 +76,7 @@ class NetworkModule {
   @Provides
   fun provideFhirResourceService(
     parser: IParser,
-    @AuthOkHttpClientQualifier okHttpClient: OkHttpClient,
+    okHttpClient: OkHttpClient,
     configService: ConfigService,
     gson: Gson,
   ): FhirResourceService =
