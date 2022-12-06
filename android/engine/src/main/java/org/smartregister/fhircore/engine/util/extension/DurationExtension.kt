a260ae477561ca99b2fa2322069942b68b0716ee
allay <a.onchuru@gmail.com>
2022-12-07 02:53:40 +0300

    Add Duration parse extension function

---
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/util/extension/DurationExtension.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.engine.util.extension
+
+import java.time.Duration
+import kotlin.time.toJavaDuration
+
+/**
+ * Parses a string that represents a duration in ISO-8601 format and returns the parsed Duration
+ * value. If parsing fails a default of 1 day duration value is returned
+ */
+fun kotlin.time.Duration.Companion.tryParse(durationString: String): Duration {
+  return try {
+    parse(durationString).toJavaDuration()
+  } catch (ex: IllegalArgumentException) {
+    return Duration.ofDays(1)
+  }
+}
