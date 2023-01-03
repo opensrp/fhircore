0569751c06c558a1de9fd0bae6dfee41e503fe25
sharon2719 <otiennosharon@gmail.com>
2023-01-03 12:09:49 +0300

    Add test to GeoWidgetConfiguration class

---
+++ android/engine/src/test/java/GeoWidgetConfigurationTest.kt
@@ -0,0 +1,29 @@
+import org.junit.Assert
+import org.junit.Test
+import org.smartregister.fhircore.engine.configuration.geowidget.GeoWidgetConfiguration
+import org.smartregister.fhircore.engine.configuration.QuestionnaireConfig
+import org.smartregister.fhircore.engine.domain.model.FhirResourceConfig
+import org.smartregister.fhircore.engine.domain.model.ResourceConfig
+
+class GeoWidgetConfigurationTest {
+    @Test
+    fun testGeoWidgetConfiguration() {
+        val appId = "testAppId"
+        val id = "testId"
+        val profileId = "testProfileId"
+        val registrationQuestionnaire = QuestionnaireConfig("testQuestionnaireId")
+        val resourceConfig = FhirResourceConfig(baseResource = ResourceConfig("name","resource"))
+        val geoWidgetConfiguration = GeoWidgetConfiguration(
+                appId, id = id, profileId = profileId,
+                registrationQuestionnaire = registrationQuestionnaire, resourceConfig = resourceConfig
+        )
+
+        Assert.assertEquals(appId, geoWidgetConfiguration.appId)
+        Assert.assertEquals("geoWidget", geoWidgetConfiguration.configType)
+        Assert.assertEquals(id, geoWidgetConfiguration.id)
+        Assert.assertEquals(profileId, geoWidgetConfiguration.profileId)
+        Assert.assertEquals(registrationQuestionnaire, geoWidgetConfiguration.registrationQuestionnaire)
+        Assert.assertEquals(resourceConfig, geoWidgetConfiguration.resourceConfig)
+    }
+}
+
