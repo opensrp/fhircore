204ff174195403f82250f959e958bd494b3401c2
Allan Onchuru <16164649+allan-on@users.noreply.github.com>
2023-01-08 07:05:46 +0300

    Mark tasks ready (#1860)
    * Add update tasks periodic job interval config
    
    * Update App config test
    
    * Update task from REQUESTED to READY
    
    * Add WorkManager worker testing dependency
    
    * Add FhirTaskPlanWorker test
    
    * Test TaskExtension task.isready returns correct values
    
    * Update TaskExtension.hasStarted test
    
    * Add TaskExtension tests
    
    * Trigger CI
    
    * Trigger CI
    
    * Update TaskExtension tests to work with timezone
    
    * Add Duration parse extension function
    
    * Schedule task update with ISO-8601 format string duration config
    
    * Update tests
    
    * Trigger CI
    
    * Alias Kotlin Duration for use in extension function
    
    * Move DurationExtensionTest to engine module
    
    * Fix check for when a Task is ready
    
    * Fix App Config & spotless check issues
    
    * Fix failing test
    
    * Update test to prevent failing annually
    
    * Refactor Task.hasStarted to reuse startIsBefore
    
    * Apply Spotless formatting
    
    * Fix failing test
    
    Co-authored-by: Francis Odhiambo Otieno <4540684+f-odhiambo@users.noreply.github.com>
    Co-authored-by: Peter Lubell-Doughtie <peter@ona.io>

---
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/util/extension/DurationExtensionKtTest.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.engine.util.extension
+
+import java.time.Duration
+import kotlin.time.Duration as KotlinDuration
+import org.junit.Assert
+import org.junit.Test
+import org.smartregister.fhircore.engine.robolectric.RobolectricTest
+
+class DurationExtensionKtTest : RobolectricTest() {
+
+  @Test
+  fun `parsing an ISO-8601 format string returns the correct duration`() {
+    val durationString = "PT02H"
+    Assert.assertEquals(Duration.ofHours(2), KotlinDuration.tryParse(durationString))
+  }
+
+  @Test
+  fun `parsing a wrong ISO-8601 format string returns the default duration of 1 day`() {
+    val durationString = "PTH2"
+    Assert.assertEquals(Duration.ofDays(1), KotlinDuration.tryParse(durationString))
+  }
+}
