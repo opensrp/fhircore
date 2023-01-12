d5e6268173e36297416fa913c0faade1a8a39bec
fikrimilano <fikrimilano1@gmail.com>
2023-01-12 15:35:26 +0700

    Rename annotation to also have the Generated word

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/appsetting/AppSettingScreen.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/appsetting/AppSettingScreen.kt
@@ -39,7 +39,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.smartregister.fhircore.engine.R
 import org.smartregister.fhircore.engine.ui.components.CircularProgressBar
-import org.smartregister.fhircore.engine.util.annotation.PreviewExcludeWithBackground
+import org.smartregister.fhircore.engine.util.annotation.PreviewExcludeWithBackgroundFromJacocoGeneratedReport
 
 const val APP_ID_TEXT_INPUT_TAG = "appIdTextInputTag"
 
@@ -106,7 +106,7 @@ fun AppSettingScreen(
   }
 }
 
-@PreviewExcludeWithBackground
+@PreviewExcludeWithBackgroundFromJacocoGeneratedReport
 @Composable
 private fun AppSettingScreenPreview() {
   AppSettingScreen(appId = "", onAppIdChanged = {}, onLoadConfigurations = {})
