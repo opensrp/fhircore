cab85635561f5dbd3919963049d3e8e26ca6e83a
Elly Kitoto <junkmailstoelly@gmail.com>
2023-01-16 17:46:36 +0300

    Update app setting UI design
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/appsetting/AppSettingScreen.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/appsetting/AppSettingScreen.kt
@@ -24,13 +24,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -41,6 +45,7 @@ import androidx.compose.ui.unit.sp
 import org.smartregister.fhircore.engine.R
 import org.smartregister.fhircore.engine.ui.components.CircularProgressBar
 import org.smartregister.fhircore.engine.util.annotation.ExcludeFromJacocoGeneratedReport
+import org.smartregister.fhircore.engine.util.extension.appVersion
 
 const val APP_ID_TEXT_INPUT_TAG = "appIdTextInputTag"
 
@@ -50,60 +55,71 @@ fun AppSettingScreen(
   appId: String,
   onAppIdChanged: (String) -> Unit,
   onLoadConfigurations: (Boolean) -> Unit,
-  showProgressBar: Boolean = false
+  showProgressBar: Boolean = false,
+  appVersionPair: Pair<Int, String>? = null
 ) {
+  val context = LocalContext.current
+  val (versionCode, versionName) = remember { appVersionPair ?: context.appVersion() }
 
-  Column(
-    verticalArrangement = Arrangement.Center,
-    modifier = modifier.fillMaxSize().padding(horizontal = 20.dp)
-  ) {
-    Text(
-      text = stringResource(R.string.fhir_core_app),
-      fontWeight = FontWeight.Bold,
-      textAlign = TextAlign.Center,
-      fontSize = 32.sp,
-      modifier = modifier.padding(vertical = 8.dp).align(Alignment.CenterHorizontally)
-    )
-    Spacer(modifier = modifier.height(20.dp))
-    Text(
-      text = stringResource(R.string.application_id),
-      modifier = modifier.padding(vertical = 8.dp)
-    )
-    OutlinedTextField(
-      onValueChange = onAppIdChanged,
-      value = appId,
-      maxLines = 1,
-      singleLine = true,
-      placeholder = {
-        Text(
-          color = Color.LightGray,
-          text = stringResource(R.string.enter_app_id),
-        )
-      },
-      modifier = modifier.testTag(APP_ID_TEXT_INPUT_TAG).fillMaxWidth().padding(vertical = 2.dp)
-    )
-    Text(
-      text = stringResource(R.string.app_id_sample),
-      fontSize = 12.sp,
-      modifier = modifier.padding(vertical = 8.dp)
-    )
-    Spacer(modifier = modifier.height(20.dp))
-    Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxWidth()) {
-      Button(
-        onClick = { onLoadConfigurations(true) },
-        enabled = !showProgressBar && appId.isNotEmpty(),
-        modifier = modifier.fillMaxWidth()
-      ) {
-        Text(
-          color = Color.White,
-          text = stringResource(id = R.string.load_configurations),
-          modifier = modifier.padding(8.dp)
-        )
-      }
-      if (showProgressBar) {
-        CircularProgressBar(modifier = modifier.matchParentSize().padding(4.dp))
+  Column(modifier = modifier.fillMaxSize()) {
+    Column(
+      verticalArrangement = Arrangement.Center,
+      modifier = modifier.weight(1f).padding(horizontal = 20.dp)
+    ) {
+      Text(
+        text = stringResource(R.string.fhir_core_app),
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        fontSize = 32.sp,
+        modifier = modifier.padding(vertical = 8.dp).align(Alignment.CenterHorizontally)
+      )
+      Spacer(modifier = modifier.height(80.dp))
+      Text(
+        text = stringResource(R.string.application_id),
+        modifier = modifier.padding(vertical = 4.dp)
+      )
+      OutlinedTextField(
+        onValueChange = onAppIdChanged,
+        value = appId,
+        maxLines = 1,
+        singleLine = true,
+        placeholder = {
+          Text(
+            color = Color.LightGray,
+            text = stringResource(R.string.app_id_sample),
+          )
+        },
+        modifier = modifier.testTag(APP_ID_TEXT_INPUT_TAG).fillMaxWidth().padding(vertical = 2.dp)
+      )
+
+      Spacer(modifier = modifier.height(30.dp))
+      Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxWidth()) {
+        Button(
+          onClick = { onLoadConfigurations(true) },
+          enabled = !showProgressBar && appId.isNotEmpty(),
+          modifier = modifier.fillMaxWidth(),
+          colors =
+            ButtonDefaults.buttonColors(
+              disabledContentColor = Color.Gray,
+              contentColor = Color.White
+            )
+        ) {
+          Text(
+            text = stringResource(id = R.string.load_configurations),
+            modifier = modifier.padding(8.dp)
+          )
+        }
+        if (showProgressBar) {
+          CircularProgressBar(modifier = modifier.matchParentSize().padding(4.dp))
+        }
       }
     }
+    Text(
+      color = Color.Gray,
+      fontSize = 16.sp,
+      text = stringResource(id = R.string.app_version, versionCode, versionName),
+      modifier = modifier.padding(16.dp).wrapContentWidth().align(Alignment.End),
+    )
   }
 }
 
@@ -111,5 +127,10 @@ fun AppSettingScreen(
 @Preview(showBackground = true)
 @ExcludeFromJacocoGeneratedReport
 private fun AppSettingScreenPreview() {
-  AppSettingScreen(appId = "", onAppIdChanged = {}, onLoadConfigurations = {})
+  AppSettingScreen(
+    appId = "",
+    onAppIdChanged = {},
+    onLoadConfigurations = {},
+    appVersionPair = Pair(1, "0.0.1")
+  )
 }
