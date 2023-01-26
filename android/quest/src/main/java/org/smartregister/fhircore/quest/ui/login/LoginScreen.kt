e8b2d85b16cec5902584e0254a1bcbd21d207818
SebaMutuku <sebastian.muthiani@gmail.com>
2023-01-26 18:36:20 +0300

    List

---
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/login/LoginScreen.kt
@@ -0,0 +1,445 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package org.smartregister.fhircore.quest.ui.login
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.smartregister.fhircore.engine.R
+import org.smartregister.fhircore.engine.configuration.app.ApplicationConfiguration
+import org.smartregister.fhircore.engine.ui.theme.LoginDarkColor
+import org.smartregister.fhircore.engine.ui.theme.LoginFieldBackgroundColor
+import org.smartregister.fhircore.engine.util.annotation.PreviewWithBackgroundExcludeGenerated
+import org.smartregister.fhircore.engine.util.extension.appVersion
+
+const val APP_NAME_TEXT_TAG = "aapNameTextTag"
+const val USERNAME_FIELD_TAG = "usernameFieldTag"
+const val PASSWORD_FIELD_TAG = "passwordFieldTag"
+const val LOGIN_BUTTON_TAG = "loginButtonTag"
+const val LOGIN_ERROR_TEXT_TAG = "loginErrorTextTag"
+const val LOGIN_FOOTER = "loginFooter"
+const val APP_LOGO_TAG = "appLogoTag"
+const val PASSWORD_FORGOT_DIALOG = "forgotPassWordDialog"
+
+@Composable
+fun LoginScreen(loginViewModel: LoginViewModel, appVersionPair: Pair<Int, String>? = null) {
+  val applicationConfiguration = remember { loginViewModel.applicationConfiguration }
+  val username by loginViewModel.username.observeAsState("")
+  val password by loginViewModel.password.observeAsState("")
+  val loginErrorState by loginViewModel.loginErrorState.observeAsState(null)
+  val showProgressBar by loginViewModel.showProgressBar.observeAsState(false)
+
+  LoginPage(
+    applicationConfiguration = applicationConfiguration,
+    username = username,
+    onUsernameChanged = { loginViewModel.onUsernameUpdated(it) },
+    password = password,
+    onPasswordChanged = { loginViewModel.onPasswordUpdated(it) },
+    forgotPassword = { loginViewModel.forgotPassword() },
+    onLoginButtonClicked = { loginViewModel.attemptRemoteLogin() },
+    loginErrorState = loginErrorState,
+    showProgressBar = showProgressBar,
+    appVersionPair = appVersionPair
+  )
+}
+
+@Composable
+fun LoginPage(
+  applicationConfiguration: ApplicationConfiguration,
+  username: String,
+  onUsernameChanged: (String) -> Unit,
+  password: String,
+  onPasswordChanged: (String) -> Unit,
+  forgotPassword: () -> Unit,
+  onLoginButtonClicked: () -> Unit,
+  modifier: Modifier = Modifier,
+  loginErrorState: LoginErrorState? = null,
+  showProgressBar: Boolean = false,
+  appVersionPair: Pair<Int, String>? = null
+) {
+  var showPassword by remember { mutableStateOf(false) }
+  var showForgotPasswordDialog by remember { mutableStateOf(false) }
+  val context = LocalContext.current
+  val (versionCode, versionName) = remember { appVersionPair ?: context.appVersion() }
+
+  val coroutineScope = rememberCoroutineScope()
+  val bringIntoViewRequester = BringIntoViewRequester()
+  val focusManager = LocalFocusManager.current
+  val usernameFocusRequester = remember { FocusRequester() }
+  val passwordFocusRequester = remember { FocusRequester() }
+
+  LaunchedEffect(Unit) {
+    delay(300)
+    usernameFocusRequester.requestFocus()
+  }
+
+  Surface(
+    modifier =
+      modifier
+        .fillMaxSize()
+        .scrollable(orientation = Orientation.Vertical, state = rememberScrollState()),
+    color = Color.White,
+    contentColor = contentColorFor(backgroundColor = Color.DarkGray)
+  ) {
+    if (showForgotPasswordDialog) {
+      ForgotPasswordDialog(
+        forgotPassword = forgotPassword,
+        onDismissDialog = { showForgotPasswordDialog = false }
+      )
+    }
+    Column(
+      modifier =
+        modifier.padding(horizontal = 16.dp).fillMaxHeight().verticalScroll(rememberScrollState()),
+      verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+      Spacer(modifier = modifier.height(20.dp))
+      Column(modifier = modifier.padding(4.dp), verticalArrangement = Arrangement.Center) {
+        // TODO Add configurable logo. Images to be downloaded from server
+        if (applicationConfiguration.loginConfig.showLogo) {
+          Image(
+            painter = painterResource(R.drawable.ic_app_logo),
+            contentDescription = stringResource(id = R.string.app_logo),
+            modifier =
+              modifier
+                .align(Alignment.CenterHorizontally)
+                .requiredHeight(120.dp)
+                .requiredWidth(140.dp)
+                .testTag(APP_LOGO_TAG),
+          )
+        }
+        Text(
+          color = if (applicationConfiguration.useDarkTheme) Color.White else LoginDarkColor,
+          text = applicationConfiguration.appTitle,
+          fontWeight = FontWeight.Bold,
+          fontSize = 32.sp,
+          modifier =
+            modifier
+              .wrapContentWidth()
+              .padding(vertical = 8.dp)
+              .align(Alignment.CenterHorizontally)
+              .testTag(APP_NAME_TEXT_TAG)
+        )
+        Spacer(modifier = modifier.height(40.dp))
+        Text(text = stringResource(R.string.username), modifier = modifier.padding(vertical = 4.dp))
+        OutlinedTextField(
+          value = username,
+          onValueChange = onUsernameChanged,
+          maxLines = 1,
+          singleLine = true,
+          placeholder = {
+            Text(
+              color = Color.LightGray,
+              text = stringResource(R.string.username_sample),
+            )
+          },
+          modifier =
+            modifier
+              .fillMaxWidth()
+              .padding(vertical = 4.dp)
+              .background(color = Color.Unspecified)
+              .testTag(USERNAME_FIELD_TAG)
+              .focusRequester(usernameFocusRequester),
+          keyboardActions = KeyboardActions(onDone = { passwordFocusRequester.requestFocus() })
+        )
+        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = modifier.fillMaxWidth()) {
+          Text(
+            text = stringResource(R.string.password),
+            modifier = modifier.wrapContentWidth().padding(vertical = 4.dp)
+          )
+          Text(
+            text = stringResource(R.string.forgot_password),
+            color = MaterialTheme.colors.primary,
+            style = TextStyle(textDecoration = TextDecoration.Underline),
+            modifier =
+              modifier.wrapContentWidth().padding(vertical = 8.dp).clickable {
+                showForgotPasswordDialog = !showForgotPasswordDialog
+              }
+          )
+        }
+        OutlinedTextField(
+          value = password,
+          onValueChange = onPasswordChanged,
+          maxLines = 1,
+          singleLine = true,
+          placeholder = {
+            Text(
+              color = Color.LightGray,
+              text = "********",
+            )
+          },
+          visualTransformation =
+            if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+          modifier =
+            modifier
+              .fillMaxWidth()
+              .onFocusEvent { event ->
+                if (event.isFocused) {
+                  coroutineScope.launch { bringIntoViewRequester.bringIntoView() }
+                }
+              }
+              .padding(vertical = 4.dp)
+              .background(color = Color.Unspecified)
+              .testTag(PASSWORD_FIELD_TAG)
+              .focusRequester(passwordFocusRequester),
+          trailingIcon = {
+            val image = if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+            IconButton(onClick = { showPassword = !showPassword }) {
+              Icon(imageVector = image, "", tint = Color.DarkGray)
+            }
+          },
+          keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
+        )
+        Text(
+          fontSize = 14.sp,
+          color = MaterialTheme.colors.error,
+          text =
+            when (loginErrorState) {
+              LoginErrorState.UNKNOWN_HOST ->
+                stringResource(
+                  id = R.string.login_error,
+                  stringResource(R.string.login_call_fail_error_message)
+                )
+              LoginErrorState.INVALID_CREDENTIALS ->
+                stringResource(
+                  id = R.string.login_error,
+                  stringResource(R.string.invalid_login_credentials)
+                )
+              null -> ""
+              LoginErrorState.MULTI_USER_LOGIN_ATTEMPT ->
+                stringResource(
+                  id = R.string.login_error,
+                  stringResource(R.string.multi_user_login_attempt)
+                )
+            },
+          modifier =
+            modifier
+              .wrapContentWidth()
+              .padding(vertical = 10.dp)
+              .align(Alignment.Start)
+              .testTag(LOGIN_ERROR_TEXT_TAG)
+        )
+        Spacer(modifier = modifier.height(0.dp))
+        Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxWidth()) {
+          Button(
+            enabled = !showProgressBar && username.isNotEmpty() && password.isNotEmpty(),
+            colors =
+              ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.primary,
+                disabledContentColor =
+                  if (applicationConfiguration.useDarkTheme) LoginFieldBackgroundColor
+                  else Color.Gray,
+                contentColor = Color.White
+              ),
+            onClick = onLoginButtonClicked,
+            modifier =
+              modifier
+                .fillMaxWidth()
+                .bringIntoViewRequester(bringIntoViewRequester)
+                .testTag(LOGIN_BUTTON_TAG),
+            elevation = null
+          ) {
+            Text(
+              text = if (!showProgressBar) stringResource(id = R.string.login_text) else "",
+              modifier = modifier.padding(8.dp)
+            )
+          }
+          if (showProgressBar) {
+            CircularProgressIndicator(
+              modifier = modifier.align(Alignment.Center).size(18.dp),
+              strokeWidth = 1.6.dp,
+              color = Color.White
+            )
+          }
+        }
+      }
+      Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.fillMaxWidth().padding(vertical = 20.dp),
+        verticalAlignment = Alignment.Bottom
+      ) {
+        Column {
+          Text(
+            text = stringResource(id = R.string.powered_by),
+            modifier = modifier.wrapContentWidth().padding(vertical = 8.dp).align(Alignment.Start)
+          )
+          Image(
+            painter = painterResource(id = R.drawable.ic_opensrp_logo),
+            contentDescription = stringResource(id = R.string.app_logo),
+            modifier = modifier.align(Alignment.CenterHorizontally).requiredHeight(40.dp)
+          )
+        }
+
+        Text(
+          fontSize = 16.sp,
+          text = stringResource(id = R.string.app_version, versionCode, versionName),
+          modifier = modifier.wrapContentWidth().padding(0.dp).testTag(LOGIN_FOOTER)
+        )
+      }
+    }
+  }
+}
+
+@Composable
+fun ForgotPasswordDialog(
+  forgotPassword: () -> Unit,
+  onDismissDialog: () -> Unit,
+  modifier: Modifier = Modifier
+) {
+  AlertDialog(
+    onDismissRequest = onDismissDialog,
+    title = {
+      Text(
+        text = stringResource(R.string.forgot_password_title),
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+      )
+    },
+    text = {
+      Text(text = stringResource(R.string.call_supervisor, "012-3456-789"), fontSize = 16.sp)
+    },
+    buttons = {
+      Row(
+        modifier = modifier.fillMaxWidth().padding(vertical = 20.dp),
+        horizontalArrangement = Arrangement.End
+      ) {
+        Text(
+          text = stringResource(R.string.cancel),
+          modifier = modifier.padding(horizontal = 10.dp).clickable { onDismissDialog() }
+        )
+        Text(
+          color = MaterialTheme.colors.primary,
+          text = stringResource(R.string.dial_number),
+          modifier =
+            modifier.padding(horizontal = 10.dp).clickable {
+              onDismissDialog()
+              forgotPassword()
+            }
+        )
+      }
+    },
+    modifier = Modifier.testTag(PASSWORD_FORGOT_DIALOG)
+  )
+}
+
+@PreviewWithBackgroundExcludeGenerated
+@Composable
+fun LoginScreenPreview() {
+  LoginPage(
+    applicationConfiguration =
+      ApplicationConfiguration(
+        appId = "appId",
+        configType = "application",
+        appTitle = "FHIRCore App"
+      ),
+    username = "",
+    onUsernameChanged = {},
+    password = "",
+    onPasswordChanged = {},
+    forgotPassword = {},
+    onLoginButtonClicked = {},
+    appVersionPair = Pair(1, "0.0.1")
+  )
+}
+
+@PreviewWithBackgroundExcludeGenerated
+@Composable
+fun LoginScreenPreviewDarkMode() {
+  LoginPage(
+    applicationConfiguration =
+      ApplicationConfiguration(
+        appId = "appId",
+        configType = "application",
+        appTitle = "FHIRCore App"
+      ),
+    username = "",
+    onUsernameChanged = {},
+    password = "",
+    onPasswordChanged = {},
+    forgotPassword = {},
+    onLoginButtonClicked = {},
+    appVersionPair = Pair(1, "0.0.1")
+  )
+}
--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/shared/components/List.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/shared/components/List.kt
@@ -19,7 +19,6 @@ package org.smartregister.fhircore.quest.ui.shared.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -33,20 +32,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.flowlayout.FlowRow
-import org.hl7.fhir.r4.model.ResourceType
-import org.smartregister.fhircore.engine.configuration.register.NoResultsConfig
-import org.smartregister.fhircore.engine.configuration.register.RegisterCardConfig
-import org.smartregister.fhircore.engine.configuration.view.CompoundTextProperties
-import org.smartregister.fhircore.engine.configuration.view.ListOrientation
 import org.smartregister.fhircore.engine.configuration.view.ListProperties
-import org.smartregister.fhircore.engine.configuration.view.ViewAlignment
 import org.smartregister.fhircore.engine.domain.model.ResourceData
-import org.smartregister.fhircore.engine.domain.model.ViewType
 import org.smartregister.fhircore.engine.ui.theme.DefaultColor
 import org.smartregister.fhircore.engine.ui.theme.DividerColor
-import org.smartregister.fhircore.engine.util.annotation.PreviewWithBackgroundExcludeGenerated
 import org.smartregister.fhircore.engine.util.extension.interpolate
 import org.smartregister.fhircore.engine.util.extension.parseColor
 import org.smartregister.fhircore.quest.R
@@ -61,123 +50,41 @@ fun List(
   val currentListResourceData = resourceData.listResourceDataMap[viewProperties.id]
 
   Column(
-    modifier = modifier
-      .background(
-        viewProperties.backgroundColor
-          ?.interpolate(resourceData.computedValuesMap)
-          .parseColor()
-      )
-      .padding(
-        horizontal = viewProperties.padding.dp, vertical = viewProperties.padding.div(4).dp
-      )
+    modifier =
+      modifier
+        .background(
+          viewProperties.backgroundColor?.interpolate(resourceData.computedValuesMap).parseColor()
+        )
+        .padding(
+          horizontal = viewProperties.padding.dp,
+          vertical = viewProperties.padding.div(4).dp
+        )
   ) {
     if (currentListResourceData.isNullOrEmpty()) {
       Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxSize()) {
         Text(
           text = viewProperties.emptyList?.message ?: stringResource(id = R.string.no_visits),
-          modifier = modifier
-            .padding(8.dp)
-            .align(Alignment.Center),
+          modifier = modifier.padding(8.dp).align(Alignment.Center),
           color = DefaultColor,
           fontStyle = FontStyle.Italic
         )
       }
     } else {
       currentListResourceData.forEachIndexed { index, listResourceData ->
-        when (viewProperties.orientation) {
-          ListOrientation.VERTICAL -> Column {
-            DisplayListItem(
-              modifier = modifier,
-              viewProperties = viewProperties,
-              listResourceData = listResourceData,
+        Column {
+          Spacer(modifier = modifier.height(5.dp))
+          Box {
+            ViewRenderer(
+              viewProperties = viewProperties.registerCard.views,
+              resourceData = listResourceData,
               navController = navController,
-              index = index,
-              currentListResourceData = currentListResourceData
             )
           }
-          ListOrientation.HORIZONTAL -> FlowRow {
-            DisplayListItem(
-              modifier = modifier,
-              viewProperties = viewProperties,
-              listResourceData = listResourceData,
-              navController = navController,
-              index = index,
-              currentListResourceData = currentListResourceData
-            )
-          }
+          Spacer(modifier = modifier.height(5.dp))
+          if (index < currentListResourceData.lastIndex && viewProperties.showDivider)
+            Divider(color = DividerColor, thickness = 0.5.dp)
         }
       }
     }
   }
-}
-
-@Composable
-private fun DisplayListItem(
-  modifier: Modifier,
-  viewProperties: ListProperties,
-  listResourceData: ResourceData,
-  navController: NavController,
-  index: Int,
-  currentListResourceData: List<ResourceData>
-) {
-  Spacer(modifier = modifier.height(5.dp))
-  Box {
-    ViewRenderer(
-      viewProperties = viewProperties.registerCard.views,
-      resourceData = listResourceData,
-      navController = navController,
-    )
-  }
-  Spacer(modifier = modifier.height(5.dp))
-  if (index < currentListResourceData.lastIndex && viewProperties.showDivider && viewProperties.orientation == ListOrientation.VERTICAL) Divider(
-    color = DividerColor, thickness = 0.5.dp
-  )
-
-}
-
-@PreviewWithBackgroundExcludeGenerated
-@Composable
-private fun ListInWithRowsPreview() {
-  Column {
-    ViewRenderer(
-      navController = rememberNavController(), resourceData = ResourceData(
-        "id", ResourceType.CarePlan, mapOf("1" to "Family", "2" to "Home"), mapOf(
-          "listId" to listOf(
-            ResourceData(
-              baseResourceId = "1", ResourceType.CarePlan, mapOf(), emptyMap(),
-            )
-          )
-        )
-
-      ), viewProperties = listOf(
-        ListProperties(
-          viewType = ViewType.LIST,
-          orientation = ListOrientation.VERTICAL,
-          id = "listId",
-          padding = 10,
-          borderRadius = 10,
-          emptyList = NoResultsConfig(
-            message = "No care Plans"
-          ),
-          baseResource = ResourceType.CarePlan,
-          alignment = ViewAlignment.NONE,
-          fillMaxHeight = true,
-          registerCard = RegisterCardConfig(
-            views = listOf(
-              CompoundTextProperties(
-                viewType = ViewType.COMPOUND_TEXT,
-                primaryText = "Family Planning",
-                primaryTextColor = "#508BE8",
-              ), CompoundTextProperties(
-                viewType = ViewType.COMPOUND_TEXT,
-                primaryText = "Malaria",
-                primaryTextColor = "#508BE8",
-              )
-            ),
-          )
-        )
-      )
-    )
-  }
-
 }
