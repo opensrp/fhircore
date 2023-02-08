1cc2814a209a21cc7388747c8377a55a47d2fd2b
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-02-08 09:07:05 +0200

    update str maps

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/patient/profile/TracingTestsProfile.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/patient/profile/TracingTestsProfile.kt
@@ -16,108 +16,120 @@
 
 package org.smartregister.fhircore.quest.ui.patient.profile
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import org.smartregister.fhircore.engine.ui.components.CircularProgressBar
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun TracingTestsProfile(viewModel: TracingTestsViewModel = hiltViewModel()) {
-  val context = LocalContext.current
-  val isOnTracing by viewModel.hasTracing.observeAsState(initial = false)
+    val context = LocalContext.current
+    val isOnTracing by viewModel.hasTracing.observeAsState(initial = false)
 
-  if (viewModel.patientId.isBlank()) {
-    CircularProgressBar()
-  } else {
-    Scaffold(
-      modifier = Modifier.fillMaxWidth(),
-      topBar = {
-        TopAppBar(
-          navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-              Icon(Icons.Default.ArrowBack, contentDescription = "")
-            }
-          },
-          title = { Text(text = "Tracing Stuff") }
-        )
-      },
-      bottomBar = {
-        Column(Modifier.fillMaxWidth()) {
-          Button(onClick = { viewModel.clearAllTracingData() }) {
-            Text(text = "Clear Tracing Data for All")
-          }
-          Box(
-            modifier = Modifier
-              .height(1.dp)
-              .background(Color.Blue)
-              .fillMaxWidth()
-          )
-          Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-          ) {
-            Button(
-              onClick = { viewModel.updateUserWithTracing(isHomeTracing = true) },
-              enabled = !isOnTracing
-            ) {
-              Text(text = "Add Home Tracing")
-            }
-            Button(
-              onClick = { viewModel.updateUserWithTracing(isHomeTracing = false) },
-              enabled = !isOnTracing
-            ) {
-              Text(text = "Add Phone Tracing")
-            }
-          }
-        }
-      }
-    ) {
-      LazyColumn(Modifier.fillMaxWidth()) {
-        items(TracingTestsViewModel.testItems) { item ->
-          when (item) {
-            is TestItem.DividerItem -> {
-              Box(
-                Modifier
-                  .fillMaxWidth()
-                  .padding(horizontal = 12.dp)) {
-                Box(
-                  Modifier
-                    .height(1.dp)
-                    .fillMaxWidth()
-                    .background(Color.Black)) {
+    if (viewModel.patientId.isBlank()) {
+        CircularProgressBar()
+    } else {
+        Scaffold(modifier = Modifier.fillMaxWidth(), topBar = {
+            TopAppBar(navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "")
                 }
-              }
+            }, title = { Text(text = "Tracing Stuff") })
+        }, bottomBar = {
+            Column(Modifier.fillMaxWidth()) {
+                Button(onClick = { viewModel.clearAllTracingData() }) {
+                    Text(text = "Clear Tracing Data for All")
+                }
+                Box(modifier = Modifier
+                        .height(1.dp)
+                        .background(Color.Blue)
+                        .fillMaxWidth())
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                    Button(onClick = { viewModel.updateUserWithTracing(isHomeTracing = true) }, enabled = !isOnTracing) {
+                        Text(text = "Add Home Tracing")
+                    }
+                    Button(onClick = { viewModel.updateUserWithTracing(isHomeTracing = false) }, enabled = !isOnTracing) {
+                        Text(text = "Add Phone Tracing")
+                    }
+                }
             }
-            is TestItem.QuestItem -> {
-              Button(
-                onClick = { viewModel.open(context, item) },
-                modifier = Modifier.fillMaxWidth()
-              ) {
-                Text(text = item.title)
-              }
-            }
-          }
+        }) { paddingValues ->
+            LazyColumn(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(paddingValues)
+                    .padding(horizontal = 12.dp)
+                    .padding(top = 8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                items(TracingTestsViewModel.testItems) { item ->
+                    when (item) {
+                        is TestItem.DividerItem -> {
+                            Box(Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 12.dp)) {
+                                Box(Modifier
+                                        .height(1.dp)
+                                        .fillMaxWidth()
+                                        .background(Color.Black)) {}
+                            }
+                        }
+                        is TestItem.QuestItem -> {
+                            Card(onClick = { viewModel.open(context, item) }, border = BorderStroke(1.dp, MaterialTheme.colors.secondary), elevation = 0.dp, contentColor = MaterialTheme.colors.onSurface, modifier = Modifier.fillMaxWidth()) {
+                                Column(Modifier.padding(12.dp)) {
+                                    Text(text = item.title)
+                                    if (item.tracingList.isNotEmpty()) {
+                                        QuestContainer("Tracing",item.tracingList)
+                                    }
+                                    if (item.appointmentList.isNotEmpty()) {
+                                        QuestContainer("Appointments",item.tracingList)
+                                    }
+                                }
 
+                            }
+                        }
+                    }
+                }
+            }
         }
-      }
     }
-  }
+}
+
+@Composable
+fun QuestContainer(title: String, list: List<String>) {
+    val annotatedString = buildAnnotatedString {
+        withStyle(SpanStyle(textDecoration =  TextDecoration.Underline)) {
+            append("${title}: ")
+        }
+        list.forEach { item ->
+            withStyle(style = if (!item.contains("-")) SpanStyle(background = MaterialTheme.colors.primary, color = MaterialTheme.colors.onPrimary) else SpanStyle(background = MaterialTheme.colors.error, color = MaterialTheme.colors.onError)) {
+                append(item.replace("-", ""))
+            }
+            append(" ")
+        }
+    }
+    Text(text = annotatedString, modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 6.dp))
 }
