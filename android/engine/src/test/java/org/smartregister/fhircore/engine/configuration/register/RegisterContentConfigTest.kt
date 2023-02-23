ca1a7693155c7be021e032f8f93acd203590c3f2
qaziabubakar-vd <72507786+qaziabubakar-vd@users.noreply.github.com>
2023-02-23 22:06:17 +0500

    1895 - Increase test coverage on engine module (#1916)
    * increase test coverage
    
    * Add more tests - navigation configs
    
    * Add more tests - profile configs
    
    * Add more tests for configurations
    
    * add missing bracket
    
    * update header
    
    * update to match new class structures
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Qazi Abubakar <qma_2007@yahoo.com>
    Co-authored-by: Peter Lubell-Doughtie <peter@ona.io>

---
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/configuration/register/RegisterContentConfigTest.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021-2023 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.engine.configuration.register
+
+import org.junit.Assert
+import org.junit.Test
+import org.smartregister.fhircore.engine.domain.model.RuleConfig
+
+class RegisterContentConfigTest {
+  private val rulesList = listOf(RuleConfig(name = "test rule", actions = listOf("actions")))
+
+  private val registerContentConfig =
+    RegisterContentConfig("separator", "display", rulesList, visible = false, computedRules = null)
+
+  @Test
+  fun testRegisterContentConfig() {
+    Assert.assertEquals("separator", registerContentConfig.separator)
+    Assert.assertEquals("display", registerContentConfig.display)
+    Assert.assertEquals(rulesList, registerContentConfig.rules)
+    Assert.assertEquals(false, registerContentConfig.visible)
+    Assert.assertEquals(null, registerContentConfig.computedRules)
+  }
+}
