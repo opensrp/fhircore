5dfdb01f6150f2390a7aa1f6c1d7e0a266d91d6f
SebaMutuku <sebastian.muthiani@gmail.com>
2023-03-13 17:37:10 +0300

    Refactor FhirTaskExpireUtil

--- android/engine/src/main/java/org/smartregister/fhircore/engine/task/FhirTaskExpireUtil.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/task/FhirTaskExpireUtil.kt
@@ -70,9 +70,7 @@ constructor(@ApplicationContext val appContext: Context, val fhirEngine: FhirEng
           count = tasksCount
           sort(Task.AUTHORED_ON, Order.ASCENDING)
         }
-        .asSequence()
         .filter { it.isPastExpiry() }
-        .toList()
         .onEach { task ->
           task.status = Task.TaskStatus.CANCELLED
           fhirEngine.update(task)
