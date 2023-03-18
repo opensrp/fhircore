fc791b280443db20d3ab3f7a9a90f538c619cd7c
Sebastian <36365043+SebaMutuku@users.noreply.github.com>
2023-03-18 20:24:17 +0300

    Fix Issue 2147 [Update the ProfileViewModel.kt to allow launching of a profile from another profile] (#2175)
    * Initial stages of issue fixing
    
    * Add test for testOnEventOpenProfileOpensAProfile
    
    * Add encounter screens
    
    * Encounter screens
    * Add householdencounter_profile_config
    * Add individualpastencounter_profile_config
    * Add individualupcomingencounters_profile_config
    
    Child Health Card
    * Add childhealthcard_profile_config
    
    * Remove refactored code
    
    ---------
    
    Co-authored-by: Benjamin Mwalimu <dubdabasoduba@gmail.com>

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/profile/ProfileScreen.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/profile/ProfileScreen.kt
@@ -207,7 +207,7 @@ private fun SimpleTopAppBar(
         AnimatedVisibility(visible = !lazyListState.isScrollingDown()) {
           CompoundText(
             compoundTextProperties = titleTextProperties,
-            resourceData = profileUiState.resourceData!!,
+            resourceData = profileUiState.resourceData,
             navController = navController
           )
         }
