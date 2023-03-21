5526f636fe80ed6e4e8e9ff7ec07d8bf882d96a2
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-03-21 08:40:46 +0200

    add generic tracing list screen

---
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/tracing/components/TracingDataList.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.quest.ui.tracing.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.flow.StateFlow
+import org.smartregister.fhircore.engine.ui.components.ErrorMessage
+import org.smartregister.fhircore.quest.ui.patient.register.components.BoxedCircularProgressBar
+import org.smartregister.fhircore.quest.util.GeneralListViewModel
+import timber.log.Timber
+
+@Composable
+fun <T : Any> TracingDataScaffoldList(
+  title: String,
+  navController: NavHostController,
+  viewModel: GeneralListViewModel<T>,
+  content: (@Composable() (value: T) -> Unit)
+) {
+  Scaffold(
+    topBar = {
+      TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+          IconButton(onClick = { navController.popBackStack() }) {
+            Icon(Icons.Filled.ArrowBack, null)
+          }
+        },
+      )
+    }
+  ) { innerPadding ->
+    val pagingItems: LazyPagingItems<T> =
+      viewModel.paginateData.collectAsState().value.collectAsLazyPagingItems()
+
+    TracingDataList(
+      modifier = Modifier.padding(innerPadding).fillMaxSize(),
+      pagingItems = pagingItems,
+      refreshing = viewModel.isRefreshing,
+      refresh = { viewModel.refresh() },
+      content = content
+    )
+  }
+}
+
+@Composable
+fun <T : Any> TracingDataList(
+  pagingItems: LazyPagingItems<T>,
+  modifier: Modifier = Modifier,
+  refreshing: StateFlow<Boolean>,
+  refresh: () -> Unit,
+  content: (@Composable() (value: T) -> Unit)
+) {
+  Box(modifier = modifier) {
+    val isRefreshing by refreshing.collectAsState()
+    SwipeRefresh(
+      state = rememberSwipeRefreshState(isRefreshing),
+      onRefresh = refresh,
+      //        indicator = { _, _ -> }
+      ) {
+      LazyColumn(
+        modifier = modifier.padding(horizontal = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+      ) {
+        item { Spacer(modifier = Modifier.height(8.dp)) }
+        items(pagingItems) { history -> if (history != null) content(history) }
+        pagingItems.apply {
+          when {
+            loadState.refresh is LoadState.Loading ->
+              item { BoxedCircularProgressBar(progressMessage = "Refreshing") }
+            loadState.append is LoadState.Loading ->
+              item { BoxedCircularProgressBar(progressMessage = "Loading") }
+            loadState.refresh is LoadState.Error -> {
+              val loadStateError = pagingItems.loadState.refresh as LoadState.Error
+              item {
+                ErrorMessage(
+                  message = loadStateError.error.also { Timber.e(it) }.localizedMessage!!,
+                  onClickRetry = { retry() }
+                )
+              }
+            }
+            loadState.append is LoadState.Error -> {
+              val error = pagingItems.loadState.append as LoadState.Error
+              item {
+                ErrorMessage(message = error.error.localizedMessage!!, onClickRetry = { retry() })
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/tracing/history/TracingHistoryScreen.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/tracing/history/TracingHistoryScreen.kt
@@ -17,27 +17,13 @@
 package org.smartregister.fhircore.quest.ui.tracing.history
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -46,109 +32,38 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.paging.LoadState
-import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import org.smartregister.fhircore.engine.domain.model.TracingHistory
-import org.smartregister.fhircore.engine.ui.components.ErrorMessage
 import org.smartregister.fhircore.engine.ui.theme.SuccessColor
 import org.smartregister.fhircore.engine.util.extension.asDdMmYyyy
 import org.smartregister.fhircore.quest.R
-import org.smartregister.fhircore.quest.ui.patient.register.components.BoxedCircularProgressBar
 import org.smartregister.fhircore.quest.ui.tracing.components.OutlineCard
-import timber.log.Timber
+import org.smartregister.fhircore.quest.ui.tracing.components.TracingDataScaffoldList
 
 @Composable
 fun TracingHistoryScreen(
   navController: NavHostController,
   viewModel: TracingHistoryViewModel = hiltViewModel()
 ) {
-  Scaffold(
-    topBar = {
-      TopAppBar(
-        title = { Text(stringResource(id = R.string.tracing_history)) },
-        navigationIcon = {
-          IconButton(onClick = { navController.popBackStack() }) {
-            Icon(Icons.Filled.ArrowBack, null)
-          }
-        },
+  val context = LocalContext.current
+  TracingDataScaffoldList(
+    title = stringResource(id = R.string.tracing_history),
+    navController = navController,
+    viewModel = viewModel,
+    content = { history ->
+      TracingHistoryCard(
+        history = history,
+        onClick = {
+          viewModel.onEvent(
+            TracingHistoryEvent.OpenOutComesScreen(
+              context = context,
+              navController = navController,
+              historyId = history.historyId
+            )
+          )
+        }
       )
     }
-  ) { innerPadding ->
-    TracingHistoryScreenContainer(
-      navController = navController,
-      modifier = Modifier.padding(innerPadding).fillMaxSize(),
-      viewModel = viewModel
-    )
-  }
-}
-
-@Composable
-fun TracingHistoryScreenContainer(
-  navController: NavHostController,
-  modifier: Modifier = Modifier,
-  viewModel: TracingHistoryViewModel,
-) {
-  val context = LocalContext.current
-  val pagingItems: LazyPagingItems<TracingHistory> =
-    viewModel.paginateData.collectAsState().value.collectAsLazyPagingItems()
-
-  Box(modifier = modifier) {
-    val isRefreshing by viewModel.isRefreshing.collectAsState()
-    SwipeRefresh(
-      state = rememberSwipeRefreshState(isRefreshing),
-      onRefresh = { viewModel.refresh() },
-      //        indicator = { _, _ -> }
-      ) {
-      LazyColumn(
-        modifier = modifier.padding(horizontal = 12.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-      ) {
-        item { Spacer(modifier = Modifier.height(8.dp)) }
-        items(pagingItems) { history ->
-          if (history != null)
-            TracingHistoryCard(
-              history = history,
-              onClick = {
-                viewModel.onEvent(
-                  TracingHistoryEvent.OpenOutComesScreen(
-                    context = context,
-                    navController = navController,
-                    historyId = history.historyId
-                  )
-                )
-              }
-            )
-        }
-        pagingItems.apply {
-          when {
-            loadState.refresh is LoadState.Loading ->
-              item { BoxedCircularProgressBar(progressMessage = "Refreshing") }
-            loadState.append is LoadState.Loading ->
-              item { BoxedCircularProgressBar(progressMessage = "Loading") }
-            loadState.refresh is LoadState.Error -> {
-              val loadStateError = pagingItems.loadState.refresh as LoadState.Error
-              item {
-                ErrorMessage(
-                  message = loadStateError.error.also { Timber.e(it) }.localizedMessage!!,
-                  onClickRetry = { retry() }
-                )
-              }
-            }
-            loadState.append is LoadState.Error -> {
-              val error = pagingItems.loadState.append as LoadState.Error
-              item {
-                ErrorMessage(message = error.error.localizedMessage!!, onClickRetry = { retry() })
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+  )
 }
 
 @Composable
