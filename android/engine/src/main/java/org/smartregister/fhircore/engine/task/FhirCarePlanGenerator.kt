2d5da6185ee8c5941c35eb58a9193885cf5f6e61
Richard Kareko <rkareko@ona.io>
2023-03-24 07:04:39 +0300

    Run spotles apply

--- android/engine/src/main/java/org/smartregister/fhircore/engine/task/FhirCarePlanGenerator.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/task/FhirCarePlanGenerator.kt
@@ -308,7 +308,12 @@ constructor(
     questionnaireConfig.planDefinitions?.forEach { planDefinition ->
       val carePlan =
         fhirEngine
-          .search<CarePlan> { filter(CarePlan.INSTANTIATES_CANONICAL, { value = "${PlanDefinition().fhirType()}/$planDefinition" }) }
+          .search<CarePlan> {
+            filter(
+              CarePlan.INSTANTIATES_CANONICAL,
+              { value = "${PlanDefinition().fhirType()}/$planDefinition" }
+            )
+          }
           .firstOrNull()
           ?: return@forEach
 
