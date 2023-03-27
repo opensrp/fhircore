86c388d7a8a0cfc1bf0d6cfe2572991e1919ba3e
Richard Kareko <rkareko@ona.io>
2023-03-27 08:43:26 +0300

    Add fhir careplan worker initial logic

---
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/task/FhirCarePlanWorker.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021-2023 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.fhircore.engine.task
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.google.android.fhir.FhirEngine
+import com.google.android.fhir.search.search
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import org.hl7.fhir.r4.model.CarePlan
+import org.hl7.fhir.r4.model.ResourceType
+import org.smartregister.fhircore.engine.util.extension.extractId
+
+@HiltWorker
+class FhirCarePlanWorker
+@AssistedInject
+constructor(
+  @Assisted val context: Context,
+  @Assisted workerParams: WorkerParameters,
+  val fhirEngine: FhirEngine,
+  val fhirCarePlanGenerator: FhirCarePlanGenerator
+) : CoroutineWorker(context, workerParams) {
+  override suspend fun doWork(): Result {
+    fhirEngine
+      .search<CarePlan> {
+        filter(
+          CarePlan.STATUS,
+          { value = of(CarePlan.CarePlanStatus.REVOKED.toCode()) },
+          { value = of(CarePlan.CarePlanStatus.COMPLETED.toCode()) }
+        )
+      }
+      .forEach { carePlan ->
+        // fetch associated tasks
+        carePlan
+          .activity
+          .flatMap { it.outcomeReference }
+          .filter { it.reference.startsWith(ResourceType.Task.name) }
+          .mapNotNull { fhirCarePlanGenerator.getTask(it.extractId()) }
+          .forEach { task ->
+            // process task status
+          }
+
+        // complete careplan if all tasks are cancelled
+
+        // complete careplan if all tasks are a mix of completed or cancelled
+      }
+    return Result.success()
+  }
+}
