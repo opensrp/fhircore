4b1732019f326081e3610b9e877d11c7d9a4b1dd
owais-vd <owais.ali@venturedive.com>
2023-03-31 17:08:53 +0500

    :bug: :hammer: added a subject filter to find a careplan

--- android/engine/src/main/java/org/smartregister/fhircore/engine/task/FhirCarePlanGenerator.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/task/FhirCarePlanGenerator.kt
@@ -98,6 +98,7 @@ constructor(
       fhirEngine
         .search<CarePlan> {
           filter(CarePlan.INSTANTIATES_CANONICAL, { value = planDefinition.referenceValue() })
+          filter(CarePlan.SUBJECT, { value = subject.logicalId })
         }
         .firstOrNull()
         ?: CarePlan().apply {
