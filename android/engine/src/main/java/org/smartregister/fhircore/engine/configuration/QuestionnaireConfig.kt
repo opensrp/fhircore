ed409e4727da77cf1f5be8f238fc1f1bfb32fca5
Elly Kitoto <junkmailstoelly@gmail.com>
2023-04-13 10:31:24 +0300

    Interpolate planDefinitions property on QuesitonnaireConfig
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/QuestionnaireConfig.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/QuestionnaireConfig.kt
@@ -75,5 +75,6 @@ fun QuestionnaireConfig.interpolate(computedValuesMap: Map<String, Any>) =
         title = confirmationDialog.title.interpolate(computedValuesMap),
         message = confirmationDialog.message.interpolate(computedValuesMap),
         actionButtonText = confirmationDialog.actionButtonText.interpolate(computedValuesMap)
-      )
+      ),
+    planDefinitions = planDefinitions?.map { it.interpolate(computedValuesMap) }
   )
