0b631e7b995d6c1ba6f0144027c4c2f3b5c34eee
Francis Odhiambo Otieno <4540684+f-odhiambo@users.noreply.github.com>
2023-04-20 12:58:52 +0300

    Overflow menu items plus new profiles

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/shared/QuestionnaireHandler.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/shared/QuestionnaireHandler.kt
@@ -25,7 +25,6 @@ import androidx.core.os.bundleOf
 import org.smartregister.fhircore.engine.configuration.QuestionnaireConfig
 import org.smartregister.fhircore.engine.domain.model.ActionParameter
 import org.smartregister.fhircore.quest.ui.questionnaire.QuestionnaireActivity
-import timber.log.Timber
 
 interface QuestionnaireHandler {
 
@@ -38,7 +37,7 @@ interface QuestionnaireHandler {
     actionParams: List<ActionParameter> = emptyList()
   ) {
     if (questionnaireConfig != null)
-    startForResult.launch(
+      startForResult.launch(
         Intent(context, QuestionnaireActivity::class.java)
           .putExtras(
             QuestionnaireActivity.intentArgs(
