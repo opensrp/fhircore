d7e9b2fff68e1dc19bf4fb2dfe1b2c2449f9f2ea
Lentumunai-Mark <lentumunai.mark@students.jkuat.ac.ke>
2023-04-20 12:16:00 +0300

    Test status color populated correctly

--- android/engine/src/test/java/org/smartregister/fhircore/engine/configuration/ButtonPropertiesTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/configuration/ButtonPropertiesTest.kt
@@ -23,30 +23,34 @@ import org.smartregister.fhircore.engine.configuration.view.ButtonType
 import org.smartregister.fhircore.engine.configuration.view.ViewAlignment
 import org.smartregister.fhircore.engine.domain.model.ViewType
 import org.smartregister.fhircore.engine.robolectric.RobolectricTest
+import org.smartregister.fhircore.engine.ui.theme.DangerColor
+import org.smartregister.fhircore.engine.ui.theme.DefaultColor
+import org.smartregister.fhircore.engine.ui.theme.InfoColor
+import org.smartregister.fhircore.engine.ui.theme.WarningColor
 
 class ButtonPropertiesTest : RobolectricTest() {
+  private val buttonProperties =
+    ButtonProperties(
+      viewType = ViewType.BUTTON,
+      weight = 0f,
+      backgroundColor = "@{backgroundColor}",
+      padding = 0,
+      borderRadius = 2,
+      alignment = ViewAlignment.NONE,
+      fillMaxWidth = true,
+      fillMaxHeight = false,
+      clickable = "false",
+      visible = "true",
+      enabled = "@{enabled}",
+      text = "@{text}",
+      status = "@{status}",
+      smallSized = false,
+      fontSize = 14.0f,
+      actions = emptyList(),
+      buttonType = ButtonType.MEDIUM,
+    )
   @Test
   fun testInterpolateInButtonProperties() {
-    val buttonProperties =
-      ButtonProperties(
-        viewType = ViewType.BUTTON,
-        weight = 0f,
-        backgroundColor = "@{backgroundColor}",
-        padding = 0,
-        borderRadius = 2,
-        alignment = ViewAlignment.NONE,
-        fillMaxWidth = true,
-        fillMaxHeight = false,
-        clickable = "false",
-        visible = "true",
-        enabled = "@{enabled}",
-        text = "@{text}",
-        status = "@{status}",
-        smallSized = false,
-        fontSize = 14.0f,
-        actions = emptyList(),
-        buttonType = ButtonType.MEDIUM,
-      )
     val map = mutableMapOf<String, String>()
     map["status"] = "DUE"
     map["backgroundColor"] = "#FFA500"
@@ -57,5 +61,24 @@ class ButtonPropertiesTest : RobolectricTest() {
     Assert.assertEquals("#FFA500", interpolatedButton.backgroundColor)
     Assert.assertEquals("true", interpolatedButton.enabled)
     Assert.assertEquals("ANC Visit", interpolatedButton.text)
+  }
+  @Test
+  fun testStatusColorIsPopulatedCorrectly() {
+    val map = mutableMapOf<String, String>()
+    map["status"] = "DUE"
+    val statusColorDue = buttonProperties.statusColor(map)
+    Assert.assertEquals(statusColorDue, InfoColor)
+    map["status"] = "OVERDUE"
+    val statusColorOverdue = buttonProperties.statusColor(map)
+    Assert.assertEquals(statusColorOverdue, DangerColor)
+    map["status"] = "COMPLETED"
+    val statusColorCompleted = buttonProperties.statusColor(map)
+    Assert.assertEquals(statusColorCompleted, DefaultColor)
+    map["status"] = "IN_PROGRESS"
+    val statusColorInProgress = buttonProperties.statusColor(map)
+    Assert.assertEquals(statusColorInProgress, WarningColor)
+    map["status"] = "UPCOMING"
+    val statusColorUpcoming = buttonProperties.statusColor(map)
+    Assert.assertEquals(statusColorUpcoming, DefaultColor)
   }
 }
