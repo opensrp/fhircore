2719e8202bb2bffff4cfe66e8ad8223415a2a60a
Maryann Wambui <mwerumaryann@gmail.com>
2023-05-24 18:00:51 +0300

    Fix app crash when switching to dark/light mode

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/theme/Themes.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/theme/Themes.kt
@@ -16,8 +16,11 @@
 
 package org.smartregister.fhircore.engine.ui.theme
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 /**
  * TODO fix issue with ktfmt formatting annotated high order functions. Current workaround below:
@@ -25,7 +28,47 @@ import androidx.compose.runtime.Composable
  *
  * To enable app theme set darkTheme = isSystemInDarkTheme
  */
+//@Composable
+//fun AppTheme(darkTheme: Boolean = false, content: (@Composable() () -> Unit)) {
+//  MaterialTheme(colors = if (darkTheme) DarkColors else LightColors, content = content)
+//}
+
+class ThemeViewModel : ViewModel() {
+  var isSystemInDarkTheme by mutableStateOf(false)
+    private set
+
+  fun updateTheme(isDarkTheme: Boolean) {
+    isSystemInDarkTheme = isDarkTheme
+    val theme = if (isSystemInDarkTheme) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+    AppCompatDelegate.setDefaultNightMode(theme)
+  }
+}
+
 @Composable
-fun AppTheme(darkTheme: Boolean = false, content: (@Composable() () -> Unit)) {
-  MaterialTheme(colors = if (darkTheme) DarkColors else LightColors, content = content)
+fun AppTheme(content: @Composable () -> Unit) {
+  val themeViewModel: ThemeViewModel = viewModel()
+
+  // Set the app's theme based on the system theme
+  val theme = if (themeViewModel.isSystemInDarkTheme) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+  AppCompatDelegate.setDefaultNightMode(theme)
+
+  CompositionLocalProvider(LocalThemeViewModel provides themeViewModel) {
+    content()
+  }
+}
+
+val LocalThemeViewModel = staticCompositionLocalOf<ThemeViewModel> {
+  error("No ThemeViewModel provided")
+}
+
+@Composable
+fun ChildComposable() {
+  val themeViewModel = LocalThemeViewModel.current
+
+  // Access and observe the system theme state from the ThemeViewModel
+
+  val isSystemInDarkTheme by themeViewModel::isSystemInDarkTheme
+
+  // Use the system theme state in your composables
+  // ...
 }
