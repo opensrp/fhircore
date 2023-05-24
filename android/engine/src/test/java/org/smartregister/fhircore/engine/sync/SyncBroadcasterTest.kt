77829ba88993e0842c75830947ece6e863abe857
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-05-24 11:10:36 +0200

    Update SyncBroadcasterTest.kt

--- android/engine/src/test/java/org/smartregister/fhircore/engine/sync/SyncBroadcasterTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/sync/SyncBroadcasterTest.kt
@@ -21,7 +21,6 @@ import com.google.android.fhir.sync.SyncJobStatus
 import dagger.hilt.android.testing.HiltTestApplication
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.toList
@@ -35,6 +34,7 @@ import org.smartregister.fhircore.engine.app.fakes.Faker
 import org.smartregister.fhircore.engine.robolectric.RobolectricTest
 import org.smartregister.fhircore.engine.rule.CoroutineTestRule
 import org.smartregister.fhircore.engine.trace.FakePerformanceReporter
+import org.smartregister.fhircore.engine.trace.PerformanceReporter
 
 @ExperimentalCoroutinesApi
 class SyncBroadcasterTest : RobolectricTest() {
@@ -42,11 +42,36 @@ class SyncBroadcasterTest : RobolectricTest() {
   private lateinit var syncBroadcaster: SyncBroadcaster
 
   @Test
-  fun runSync() = runTest {
-    syncBroadcaster = mockk()
-    every { syncBroadcaster.runSync() } returns Unit
-    syncBroadcaster.runSync()
-    verify { syncBroadcaster.runSync() }
+  fun runSync_shouldInvokeSyncAndEmitStatusWhenNetworkAvailable() = runTest {
+    val configurationRegistry = Faker.buildTestConfigurationRegistry(mockk())
+    val context = ApplicationProvider.getApplicationContext<HiltTestApplication>()
+    val configService = AppConfigService(context = context)
+    val sharedSyncStatus: MutableSharedFlow<SyncJobStatus> = MutableSharedFlow()
+
+    val tracer: PerformanceReporter = mockk()
+
+    syncBroadcaster =
+      SyncBroadcaster(
+        configurationRegistry,
+        configService,
+        fhirEngine = mockk(),
+        sharedSyncStatus,
+        dispatcherProvider = CoroutineTestRule().testDispatcherProvider,
+        appContext = context,
+        tracer = tracer
+      )
+
+    every { tracer.startTrace(SyncBroadcaster.SYNC_TRACE) } returns Unit
+    every { tracer.stopTrace(SyncBroadcaster.SYNC_TRACE) } returns Unit
+
+    val collectedSyncStatusList = mutableListOf<SyncJobStatus>()
+    val job =
+      launch(UnconfinedTestDispatcher(testScheduler)) {
+        sharedSyncStatus.toList(collectedSyncStatusList)
+      }
+    syncBroadcaster.runSync { false }
+    Assert.assertTrue(collectedSyncStatusList.first() is SyncJobStatus.Failed)
+    job.cancel()
   }
 
   @Test
