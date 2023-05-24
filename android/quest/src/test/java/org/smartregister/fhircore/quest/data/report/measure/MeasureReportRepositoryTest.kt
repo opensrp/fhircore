e7c57ed0571f77e2ee5745e6ccf7ece3b9f5fdd6
SebaMutuku <sebastian.muthiani@gmail.com>
2023-05-24 17:36:51 +0300

    Fix SonarQube errors

--- android/quest/src/test/java/org/smartregister/fhircore/quest/data/report/measure/MeasureReportRepositoryTest.kt
+++ android/quest/src/test/java/org/smartregister/fhircore/quest/data/report/measure/MeasureReportRepositoryTest.kt
@@ -96,7 +96,8 @@ class MeasureReportRepositoryTest : RobolectricTest() {
           dispatcherProvider = DefaultDispatcherProvider(),
           sharedPreferencesHelper = mockk(),
           configurationRegistry = configurationRegistry,
-          configService = mockk()
+          configService = mockk(),
+          dataQueryRulesExecutor = mockk()
         )
       )
 
