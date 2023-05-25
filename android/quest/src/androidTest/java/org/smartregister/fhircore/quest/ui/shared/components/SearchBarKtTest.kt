9d571650649a82a96261420261207a82ae72b70b
Fikri Milano <fikrimilano1@gmail.com>
2023-05-25 21:32:54 +0700

    Add test (#2380)

--- android/quest/src/androidTest/java/org/smartregister/fhircore/quest/ui/shared/components/SearchBarKtTest.kt
+++ android/quest/src/androidTest/java/org/smartregister/fhircore/quest/ui/shared/components/SearchBarKtTest.kt
@@ -17,11 +17,17 @@
 package org.smartregister.fhircore.quest.ui.shared.components
 
 import androidx.compose.foundation.background
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.text.input.TextFieldValue
 import org.junit.Rule
 import org.junit.Test
 import org.smartregister.fhircore.engine.ui.theme.AppTheme
@@ -32,8 +38,52 @@ class SearchBarKtTest {
 
   @Test
   fun testSearchRendersHintCorrectly() {
-
     composeTestRule.setContent { AppTheme { SearchHint(Modifier.background(color = Color.White)) } }
     composeTestRule.onNodeWithText("Search name or ID").assertIsDisplayed()
+  }
+
+  @Test
+  fun testSearchBarHasTrailingIconWhenHasTextInput() {
+    composeTestRule.setContent {
+      AppTheme {
+        val searchTextState = remember { mutableStateOf(TextFieldValue(text = "A text yoo")) }
+        SearchBar(onTextChanged = {}, onBackPress = {}, searchTextState = searchTextState)
+      }
+    }
+    composeTestRule
+      .onNodeWithTag(SEARCH_BAR_TRAILING_ICON_TEST_TAG, useUnmergedTree = true)
+      .assertIsDisplayed()
+  }
+
+  @Test
+  fun testSearchBarHasNoTrailingIconWhenHasNoTextInput() {
+    composeTestRule.setContent {
+      AppTheme {
+        val searchTextState = remember { mutableStateOf(TextFieldValue(text = "")) }
+        SearchBar(onTextChanged = {}, onBackPress = {}, searchTextState = searchTextState)
+      }
+    }
+    composeTestRule
+      .onNodeWithTag(SEARCH_BAR_TRAILING_ICON_TEST_TAG, useUnmergedTree = true)
+      .assertDoesNotExist()
+  }
+
+  @Test
+  fun testSearchBarOnClickTrailingIconShouldClearTextInput() {
+    composeTestRule.setContent {
+      AppTheme {
+        val searchTextState = remember { mutableStateOf(TextFieldValue(text = "A text yoo")) }
+        SearchBar(onTextChanged = {}, onBackPress = {}, searchTextState = searchTextState)
+      }
+    }
+    composeTestRule
+      .onNodeWithTag(SEARCH_BAR_TRAILING_TEXT_FIELD_TEST_TAG, useUnmergedTree = true)
+      .assertTextEquals("A text yoo")
+    composeTestRule
+      .onNodeWithTag(SEARCH_BAR_TRAILING_ICON_BUTTON_TEST_TAG, useUnmergedTree = true)
+      .performClick()
+    composeTestRule
+      .onNodeWithTag(SEARCH_BAR_TRAILING_TEXT_FIELD_TEST_TAG, useUnmergedTree = true)
+      .assertTextEquals("")
   }
 }
