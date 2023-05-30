e00ffcfc869360ecc1a00bb6d2382870d7d6a3da
Ephraim Kigamba <ekigamba@ona.io>
2023-05-30 15:00:08 +0300

    Fix bug preventing saving of location IDs, careteam IDs and organisation IDs
    Fixes #2401

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/login/LoginViewModel.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/login/LoginViewModel.kt
@@ -131,12 +131,14 @@ constructor(
               }
             },
             onFetchPractitioner = { bundleResult ->
-              _showProgressBar.postValue(false)
               if (bundleResult.isSuccess) {
-                updateNavigateHome(true)
                 val bundle = bundleResult.getOrDefault(FhirR4ModelBundle())
-                savePractitionerDetails(bundle)
+                savePractitionerDetails(bundle) {
+                  _showProgressBar.postValue(false)
+                  updateNavigateHome(true)
+                }
               } else {
+                _showProgressBar.postValue(false)
                 Timber.e(bundleResult.exceptionOrNull())
                 Timber.e(bundleResult.getOrNull().valueToString())
                 _loginErrorState.postValue(LoginErrorState.ERROR_FETCHING_USER)
@@ -232,7 +234,7 @@ constructor(
     }
   }
 
-  fun savePractitionerDetails(bundle: FhirR4ModelBundle) {
+  fun savePractitionerDetails(bundle: FhirR4ModelBundle, postProcess: () -> Unit) {
     if (bundle.entry.isNullOrEmpty()) return
     viewModelScope.launch {
       val practitionerDetails = bundle.entry.first().resource as PractitionerDetails
@@ -275,6 +277,8 @@ constructor(
         SharedPreferenceKey.PRACTITIONER_LOCATION_HIERARCHIES.name,
         locationHierarchies
       )
+
+      postProcess()
     }
   }
 
