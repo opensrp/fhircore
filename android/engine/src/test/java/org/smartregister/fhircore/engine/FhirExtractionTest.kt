b8ee009d4b92947795bcbe5911f6c924ed5862d1
maimoonak <4829880+maimoonak@users.noreply.github.com>
2023-06-01 23:41:09 -0400

    add launch context functionality (#2411)
    * add launch context functionality
    
    * Update data capture version
    
    * Handle and update record all functionality tests
    
    ---------
    
    Co-authored-by: Peter Lubell-Doughtie <peter@ona.io>

--- android/engine/src/test/java/org/smartregister/fhircore/engine/FhirExtractionTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/FhirExtractionTest.kt
@@ -37,6 +37,7 @@ import io.mockk.slot
 import java.util.Date
 import javax.inject.Inject
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
 import junit.framework.Assert.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -46,7 +47,9 @@ import org.hl7.fhir.r4.model.Immunization
 import org.hl7.fhir.r4.model.Patient
 import org.hl7.fhir.r4.model.Questionnaire
 import org.hl7.fhir.r4.model.QuestionnaireResponse
+import org.hl7.fhir.r4.model.Reference
 import org.hl7.fhir.r4.model.Resource
+import org.hl7.fhir.r4.model.ResourceType
 import org.hl7.fhir.r4.model.StructureMap
 import org.hl7.fhir.r4.model.Task
 import org.hl7.fhir.r4.utils.StructureMapUtilities
@@ -57,6 +60,8 @@ import org.smartregister.fhircore.engine.data.local.DefaultRepository
 import org.smartregister.fhircore.engine.robolectric.RobolectricTest
 import org.smartregister.fhircore.engine.util.extension.decodeResourceFromString
 import org.smartregister.fhircore.engine.util.extension.encodeResourceToString
+import org.smartregister.fhircore.engine.util.extension.extractId
+import org.smartregister.fhircore.engine.util.extension.extractLogicalIdUuid
 import org.smartregister.fhircore.engine.util.extension.makeItReadable
 import org.smartregister.fhircore.engine.util.extension.referenceValue
 import org.smartregister.fhircore.engine.util.helper.TransformSupportServices
@@ -103,6 +108,28 @@ class FhirExtractionTest : RobolectricTest() {
     val encounter = result.entry.find { it.resource is Encounter }!!.resource as Encounter
     result.entry.filter { it.resource is Task }.also { taskList ->
       assertTrue(taskList.size == 3)
+
+      questionnaireResponse.item.find { it.linkId == "vaccines" }!!.answer
+        .map { it.value as Reference }
+        .forEach { taskReference ->
+          val outputTask =
+            taskList
+              .find { (it.resource as Task).id.extractLogicalIdUuid() == taskReference.extractId() }
+              ?.resource as?
+              Task
+          assertNotNull(outputTask)
+          assertTrue(outputTask!!.output.size == 3)
+          assertNotNull(
+            outputTask.output.find {
+              it.castToReference(it.value).reference.startsWith(ResourceType.Immunization.name)
+            }
+          )
+          assertNotNull(
+            outputTask.output.find {
+              it.castToReference(it.value).reference.startsWith(ResourceType.Encounter.name)
+            }
+          )
+        }
 
       val firstTask = taskList.first().resource as Task
       assertTrue(
