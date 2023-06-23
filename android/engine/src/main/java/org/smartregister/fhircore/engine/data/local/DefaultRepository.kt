0803e4a33a0c7257a72843ba3c2a400ad6bc7083
Richard Kareko <rkareko@ona.io>
2023-06-23 16:27:59 +0300

    Run spotless Apply

--- android/engine/src/main/java/org/smartregister/fhircore/engine/data/local/DefaultRepository.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/data/local/DefaultRepository.kt
@@ -688,7 +688,7 @@ constructor(
       is CarePlan -> resource.status = CarePlan.CarePlanStatus.COMPLETED
       is Procedure -> resource.status = Procedure.ProcedureStatus.STOPPED
       is Condition -> {
-        //TODO Remove the hardcoded custom logic for closing PNC Condition i.e remove if block
+        // TODO Remove the hardcoded custom logic for closing PNC Condition i.e remove if block
         /**
          * The logic for closing PNC Condition makes 2 assumptions
          * 1. The eventResource id value is "pncConditionToClose"
@@ -722,9 +722,7 @@ constructor(
                 )
             }
         }
-
       }
-
       is ServiceRequest -> resource.status = ServiceRequest.ServiceRequestStatus.REVOKED
     }
     fhirEngine.update(resource)
