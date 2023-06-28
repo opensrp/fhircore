21a6f18ff26de8ebc408b171694bab728634c9b7
Lentumunai-Mark <lentumunai.mark@students.jkuat.ac.ke>
2023-06-28 19:56:02 +0300

    Run spotlessApply
    Signed-off-by: Lentumunai-Mark <lentumunai.mark@students.jkuat.ac.ke>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/view/BorderProperties.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/view/BorderProperties.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021-2023 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.smartregister.fhircore.engine.configuration.view
 
 import kotlinx.serialization.Serializable
@@ -6,22 +22,22 @@ import org.smartregister.fhircore.engine.util.extension.interpolate
 
 @Serializable
 data class BorderProperties(
-    override val viewType: ViewType = ViewType.SPACER,
-    override val weight: Float = 0f,
-    override val backgroundColor: String? = "#FFFFFF",
-    override val padding: Int = 0,
-    override val borderRadius: Int = 2,
-    override val alignment: ViewAlignment = ViewAlignment.NONE,
-    override val fillMaxWidth: Boolean = false,
-    override val fillMaxHeight: Boolean = false,
-    override val clickable: String = "false",
-    override val visible: String = "true",
-    val thickness: Float? = null,
-): ViewProperties(){
-    override fun interpolate(computedValuesMap: Map<String, Any>): BorderProperties {
-        return this.copy(
-            backgroundColor = backgroundColor?.interpolate(computedValuesMap),
-            visible = visible.interpolate(computedValuesMap)
-        )
-    }
+  override val viewType: ViewType = ViewType.SPACER,
+  override val weight: Float = 0f,
+  override val backgroundColor: String? = "#FFFFFF",
+  override val padding: Int = 0,
+  override val borderRadius: Int = 2,
+  override val alignment: ViewAlignment = ViewAlignment.NONE,
+  override val fillMaxWidth: Boolean = false,
+  override val fillMaxHeight: Boolean = false,
+  override val clickable: String = "false",
+  override val visible: String = "true",
+  val thickness: Float? = null,
+) : ViewProperties() {
+  override fun interpolate(computedValuesMap: Map<String, Any>): BorderProperties {
+    return this.copy(
+      backgroundColor = backgroundColor?.interpolate(computedValuesMap),
+      visible = visible.interpolate(computedValuesMap)
+    )
+  }
 }
