f2cca6eaabf27afc61cd79e6b350ac93c78e5054
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2023-06-29 19:18:47 +0000

    Fix overdue service status (#2507)
    * Add the Service Card color test
    
    * :bug: Fix overdue status 0.2.4
    
    * :rewind: Remove unwated tests
    
    * :sparkles: Mark all the non ready, in-progres, requested, completed or cancelled as UPCOMING
    
    * null tasks are upcoming
    
    ---------
    
    Co-authored-by: pld <peter@ona.io>

--- android/engine/src/test/java/org/smartregister/fhircore/engine/rulesengine/RulesEngineServiceTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/rulesengine/RulesEngineServiceTest.kt
@@ -19,6 +19,8 @@ package org.smartregister.fhircore.engine.rulesengine
 import com.google.android.fhir.search.Order
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 import javax.inject.Inject
 import org.hl7.fhir.r4.model.Enumerations
@@ -174,10 +176,13 @@ class RulesEngineServiceTest : RobolectricTest() {
   }
 
   @Test
-  fun `generateTaskServiceStatus() should return DUE when Task#status is NULL`() {
+  fun `generateTaskServiceStatus() should return UPCOMING when Task#status is NULL`() {
     val task = Task().apply { status = Task.TaskStatus.NULL }
 
-    Assert.assertEquals(ServiceStatus.DUE.name, rulesEngineService.generateTaskServiceStatus(task))
+    Assert.assertEquals(
+      ServiceStatus.UPCOMING.name,
+      rulesEngineService.generateTaskServiceStatus(task)
+    )
   }
 
   @Test
@@ -223,6 +228,52 @@ class RulesEngineServiceTest : RobolectricTest() {
 
     Assert.assertEquals(
       ServiceStatus.EXPIRED.name,
+      rulesEngineService.generateTaskServiceStatus(task)
+    )
+  }
+
+  @Test
+  fun `generateTaskServiceStatus() should return OVERDUE when Task#executionPeriod#hasEnd() and Task#executionPeriod#end#before(today())`() {
+
+    val sdf = SimpleDateFormat("dd/MM/yyyy")
+    val startDate: Date? = sdf.parse("01/01/2023")
+    val endDate: Date? = sdf.parse("01/02/2023")
+
+    val task =
+      Task().apply {
+        status = Task.TaskStatus.INPROGRESS
+        executionPeriod =
+          Period().apply {
+            start = startDate
+            end = endDate
+          }
+      }
+
+    Assert.assertEquals(
+      ServiceStatus.OVERDUE.name,
+      rulesEngineService.generateTaskServiceStatus(task)
+    )
+  }
+
+  @Test
+  fun `generateTaskServiceStatus() should not return OVERDUE when Task#executionPeriod#hasEnd() andTask#executionPeriod#end#before(today()) and Task#status is not INPROGRESS or READY`() {
+
+    val sdf = SimpleDateFormat("dd/MM/yyyy")
+    val startDate: Date? = sdf.parse("01/01/2023")
+    val endDate: Date? = sdf.parse("01/02/2023")
+
+    val task =
+      Task().apply {
+        status = Task.TaskStatus.REQUESTED
+        executionPeriod =
+          Period().apply {
+            start = startDate
+            end = endDate
+          }
+      }
+
+    Assert.assertEquals(
+      ServiceStatus.UPCOMING.name,
       rulesEngineService.generateTaskServiceStatus(task)
     )
   }
