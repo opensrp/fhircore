8d62fc09b233f5208c812187bb21970495a957ee
Elly Kitoto <junkmailstoelly@gmail.com>
2023-06-30 17:52:02 +0300

    Refactor sync
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/app/ApplicationConfiguration.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/configuration/app/ApplicationConfiguration.kt
@@ -28,7 +28,7 @@ data class ApplicationConfiguration(
   val remoteSyncPageSize: Int = 100,
   val languages: List<String> = listOf("en"),
   val useDarkTheme: Boolean = false,
-  val syncInterval: Long = 30,
+  val syncInterval: Long = 15,
   val syncStrategies: List<String> = listOf(),
   val loginConfig: LoginConfig = LoginConfig(),
   val deviceToDeviceSync: DeviceToDeviceSyncConfig? = null,
