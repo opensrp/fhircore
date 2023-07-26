db74635d6bd37955c4bf57f821061406fc2bc708
Elly Kitoto <junkmailstoelly@gmail.com>
2023-07-26 20:45:02 +0300

    Reinstate code for setting inital expression
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/questionnaire/QuestionnaireActivity.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/questionnaire/QuestionnaireActivity.kt
@@ -27,6 +27,7 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
 import androidx.lifecycle.lifecycleScope
 import com.google.android.fhir.datacapture.QuestionnaireFragment
+import com.google.android.fhir.datacapture.extensions.flattened
 import com.google.android.fhir.logicalId
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.Serializable
@@ -41,7 +42,9 @@ import org.smartregister.fhircore.engine.domain.model.ActionParameter
 import org.smartregister.fhircore.engine.domain.model.ActionParameterType
 import org.smartregister.fhircore.engine.ui.base.AlertDialogue
 import org.smartregister.fhircore.engine.ui.base.BaseMultiLanguageActivity
+import org.smartregister.fhircore.engine.util.callSuspendFunctionOnField
 import org.smartregister.fhircore.engine.util.extension.encodeResourceToString
+import org.smartregister.fhircore.engine.util.extension.initialExpression
 import org.smartregister.fhircore.engine.util.extension.parcelable
 import org.smartregister.fhircore.engine.util.extension.parcelableArrayList
 import org.smartregister.fhircore.engine.util.extension.showToast
@@ -116,6 +119,7 @@ class QuestionnaireActivity : BaseMultiLanguageActivity() {
           setNavigationIcon(R.drawable.ic_arrow_back)
           setNavigationOnClickListener { handleBackPress() }
         }
+
         questionnaire = viewModel.retrieveQuestionnaire(questionnaireConfig, actionParameters)
 
         if (questionnaire == null) {
@@ -273,6 +277,36 @@ class QuestionnaireActivity : BaseMultiLanguageActivity() {
     (supportFragmentManager.findFragmentByTag(QUESTIONNAIRE_FRAGMENT_TAG) as QuestionnaireFragment?)
       ?.getQuestionnaireResponse()
 
+  // TODO This maybe required or Not. To be called before Questionnaire is launched
+  suspend fun setInitialExpression(questionnaire: Questionnaire, fragment: QuestionnaireFragment) {
+    // TODO Remove once SDK PR https://github.com/google/android-fhir/pull/2045 is merged
+    questionnaire.item.flattened().forEach { item ->
+      item.initialExpression
+        ?.takeIf { it.language == "application/x-fhir-query" }
+        ?.let { expression ->
+          val answerOptions: List<Questionnaire.QuestionnaireItemAnswerOptionComponent> =
+            QuestionnaireFragment::class.callSuspendFunctionOnField(
+              fragment,
+              "viewModel",
+              "loadAnswerExpressionOptions",
+              item,
+              expression,
+            ) as List<Questionnaire.QuestionnaireItemAnswerOptionComponent>
+          answerOptions
+        }
+        ?.let {
+          it.let {
+            item.initial =
+              it.map {
+                Questionnaire.QuestionnaireItemInitialComponent().apply {
+                  value = it.castToType(it.value)
+                }
+              }
+          }
+        }
+    }
+  }
+
   companion object {
 
     const val QUESTIONNAIRE_FRAGMENT_TAG = "questionnaireFragment"
@@ -281,7 +315,6 @@ class QuestionnaireActivity : BaseMultiLanguageActivity() {
     const val QUESTIONNAIRE_RESPONSE = "questionnaireResponse"
     const val QUESTIONNAIRE_ACTION_PARAMETERS = "questionnaireActionParameters"
     const val QUESTIONNAIRE_POPULATION_RESOURCES = "questionnairePopulationResources"
-    const val QUESTIONNAIRE_BARCODE = "pa"
 
     fun intentArgs(
       questionnaireConfig: QuestionnaireConfig,
