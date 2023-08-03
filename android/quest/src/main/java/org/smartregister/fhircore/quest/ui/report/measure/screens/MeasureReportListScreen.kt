bd5fcf31c6eff00a2eb242ca317067785399ad5b
Rkareko <47570855+Rkareko@users.noreply.github.com>
2023-08-03 15:09:55 +0300

    Add progress bar for report generation (#2636)

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/report/measure/screens/MeasureReportListScreen.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/report/measure/screens/MeasureReportListScreen.kt
@@ -17,10 +17,14 @@
 package org.smartregister.fhircore.quest.ui.report.measure.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -30,9 +34,13 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -46,7 +54,8 @@ fun MeasureReportListScreen(
   navController: NavController,
   dataList: Flow<PagingData<ReportConfiguration>>,
   onReportMeasureClicked: (List<ReportConfiguration>) -> Unit,
-  modifier: Modifier = Modifier
+  modifier: Modifier = Modifier,
+  showProgressIndicator: Boolean = false,
 ) {
   val lazyReportItems = dataList.collectAsLazyPagingItems().itemSnapshotList.groupBy { it?.module }
 
@@ -65,14 +74,33 @@ fun MeasureReportListScreen(
     }
   ) { innerPadding ->
     Box(modifier = modifier.padding(innerPadding)) {
-      LazyColumn(modifier = modifier.background(Color.White).fillMaxSize()) {
-        lazyReportItems.keys.forEach { key ->
-          item {
-            key?.let { it1 ->
-              MeasureReportRow(
-                it1,
-                { onReportMeasureClicked(lazyReportItems[key] as List<ReportConfiguration>) }
-              )
+      if (showProgressIndicator) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+          Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+          ) {
+            CircularProgressIndicator(
+              modifier = modifier.size(40.dp).testTag(SHOW_PROGRESS_INDICATOR_TAG),
+              strokeWidth = 2.dp
+            )
+            Text(
+              text = stringResource(R.string.please_wait),
+              textAlign = TextAlign.Center,
+              modifier = modifier.padding(vertical = 16.dp).testTag(PLEASE_WAIT_TEST_TAG)
+            )
+          }
+        }
+      } else {
+        LazyColumn(modifier = modifier.background(Color.White).fillMaxSize()) {
+          lazyReportItems.keys.forEach { key ->
+            item {
+              key?.let { it1 ->
+                MeasureReportRow(
+                  it1,
+                  { onReportMeasureClicked(lazyReportItems[key] as List<ReportConfiguration>) }
+                )
+              }
             }
           }
         }
