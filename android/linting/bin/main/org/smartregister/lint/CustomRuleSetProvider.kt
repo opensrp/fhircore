91c0489d7fe47021d29bee98ab0bb6dc0264ace6
Elly Kitoto <junkmailstoelly@gmail.com>
2023-08-10 19:40:52 +0300

    Rearrange app module androidTest packages (#2670)
    * Rearrange androidTest packages
    
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>
    
    * Update ci configs
    
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>
    
    ---------
    
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

---
+++ android/linting/bin/main/org/smartregister/lint/CustomRuleSetProvider.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021-2023 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartregister.lint
+
+import com.pinterest.ktlint.cli.ruleset.core.api.RuleSetProviderV3
+import com.pinterest.ktlint.rule.engine.core.api.RuleProvider
+import com.pinterest.ktlint.rule.engine.core.api.RuleSetId
+
+internal val CUSTOM_RULE_SET_ID = "opensrp-lint"
+
+class CustomRuleSetProvider : RuleSetProviderV3(RuleSetId(CUSTOM_RULE_SET_ID)) {
+  override fun getRuleProviders(): Set<RuleProvider> {
+    return setOf(
+      RuleProvider { FhirEngineCreateUpdateMethodCallRule() },
+    )
+  }
+}
