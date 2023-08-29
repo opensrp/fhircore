5e4d3a96a56876a80dd5374dc406cd1b951f5506
Ephraim Kigamba <ekigamba@ona.io>
2023-08-29 17:49:20 +0300

    Remove macrobenchmark from CI and remove SSH debugging

--- .github/workflows/performance_tests.sh
+++ .github/workflows/performance_tests.sh
@@ -6,7 +6,4 @@
 /Users/runner/Library/Android/sdk/platform-tools/adb shell am instrument -w -e package org.smartregister.fhircore.performance -e "androidx.benchmark.suppressErrors" ACTIVITY-MISSING,CODE-COVERAGE,DEBUGGABLE,UNLOCKED,EMULATOR -e additionalTestOutputDir "/sdcard/Download/" org.smartregister.opensrp.ecbis.test/org.smartregister.fhircore.quest.QuestTestRunner && \
 /Users/runner/Library/Android/sdk/platform-tools/adb shell ls /sdcard/Download/ && \
 /Users/runner/Library/Android/sdk/platform-tools/adb pull /sdcard/Download/org.smartregister.opensrp.ecbis-benchmarkData.json quest/ && \
-./gradlew :quest:assembleOpensrpDebug --stacktrace && \
-/Users/runner/Library/Android/sdk/platform-tools/adb install quest/build/outputs/apk/opensrp/debug/quest-opensrp-debug.apk && \
-./gradlew :macrobenchmark:connectedBenchmarkAndroidTest --stacktrace || true && delay 108000 && \
-cp macrobenchmark/build/outputs/connected_android_test_additional_output/benchmark/connected/*/org.smartregister.opensrp.quest.macrobenchmark-benchmarkData.json macrobenchmark/benchmark-results.json
+./gradlew :quest:assembleOpensrpDebug --stacktrace
