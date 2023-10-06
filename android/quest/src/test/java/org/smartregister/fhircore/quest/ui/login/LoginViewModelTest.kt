13deeb219abd36619674ef63a127c7c6cfe9b1fe
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2023-10-06 16:38:48 +0300

    Add loginViewModel tests on multi-user with 'location' syncStrategy

--- android/quest/src/test/java/org/smartregister/fhircore/quest/ui/login/LoginViewModelTest.kt
+++ android/quest/src/test/java/org/smartregister/fhircore/quest/ui/login/LoginViewModelTest.kt
@@ -32,9 +32,13 @@ import java.net.UnknownHostException
 import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import okhttp3.internal.http.RealResponseBody
 import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.Location
 import org.hl7.fhir.r4.model.Organization
+import org.hl7.fhir.r4.model.ResourceType
 import org.hl7.fhir.r4.model.StringType
 import org.junit.After
 import org.junit.Assert
@@ -179,12 +183,31 @@ internal class LoginViewModelTest : RobolectricTest() {
   @Test
   fun testSuccessfulOnlineLoginWithActiveSessionWithSavedPractitionerDetails() {
     updateCredentials()
+    coEvery {
+      tokenAuthenticator.fetchAccessToken(thisUsername, thisPassword.toCharArray())
+    } returns
+      Result.success(
+        OAuthResponse(
+          accessToken = "very_new_top_of_the_class_access_token",
+          tokenType = "you_guess_it",
+          refreshToken = "another_very_refreshing_token",
+          refreshExpiresIn = 540000,
+          scope = "open_my_guy",
+        ),
+      )
     secureSharedPreference.saveCredentials(thisUsername, thisPassword.toCharArray())
+    val practitioner =
+      practitionerDetails().apply { fhirPractitionerDetails.id = "$thisUsername-practitioner-id" }
     sharedPreferencesHelper.write(
       SharedPreferenceKey.PRACTITIONER_DETAILS.name,
       PractitionerDetails(),
     )
+    sharedPreferencesHelper.write(
+      SharedPreferenceKey.PRACTITIONER_ID.name,
+      practitioner.fhirPractitionerDetails?.id,
+    )
     every { tokenAuthenticator.sessionActive() } returns true
+
     loginViewModel.login(mockedActivity(isDeviceOnline = true))
     Assert.assertFalse(loginViewModel.showProgressBar.value!!)
     Assert.assertTrue(loginViewModel.navigateToHome.value!!)
@@ -199,14 +222,108 @@ internal class LoginViewModelTest : RobolectricTest() {
   }
 
   @Test
-  fun testUnSuccessfulOnlineLoginUsingDifferentUsername() {
+  fun testSuccessfulOnlineLoginForDifferentUserInTheSameLocationWithSyncStrategyLocation() =
+    runTest {
+      updateCredentials()
+      secureSharedPreference.saveCredentials("nativeUser", "n4t1veP5wd".toCharArray())
+      sharedPreferencesHelper.write(ResourceType.Location.name, listOf("test-location"))
+      sharedPreferencesHelper.write(
+        SharedPreferenceKey.PRACTITIONER_ID.name,
+        "nativeUser-practitioner-id"
+      )
+
+      coEvery {
+        tokenAuthenticator.fetchAccessToken(thisUsername, thisPassword.toCharArray())
+      } returns
+        Result.success(
+          OAuthResponse(
+            accessToken = "very_new_top_of_the_class_access_token",
+            tokenType = "you_guess_it",
+            refreshToken = "another_very_refreshing_token",
+            refreshExpiresIn = 540000,
+            scope = "open_my_guy",
+          ),
+        )
+      coEvery { keycloakService.fetchUserInfo() } returns
+        Response.success(UserInfo(keycloakUuid = "awesome_uuid"))
+      val bundle =
+        Bundle().apply {
+          addEntry(
+            Bundle.BundleEntryComponent().apply {
+              resource =
+                practitionerDetails().apply {
+                  fhirPractitionerDetails =
+                    FhirPractitionerDetails().apply {
+                      practitionerId = StringType("my-test-practitioner-id")
+                      locations = mutableListOf(Location().apply { id = "test-location" })
+                    }
+                }
+            },
+          )
+        }
+      coEvery { fhirResourceService.getResource(any()) } returns bundle
+      every { tokenAuthenticator.sessionActive() } returns false
+
+      loginViewModel.login(mockedActivity(isDeviceOnline = true))
+      advanceUntilIdle()
+
+      Assert.assertFalse(loginViewModel.showProgressBar.value!!)
+      Assert.assertTrue(loginViewModel.navigateToHome.value!!)
+
+      // Login was successful savePractitionerDetails was called
+      val practitionerDetailsSlot = slot<PractitionerDetails>()
+      verify {
+        loginViewModel.savePractitionerDetails(capture(practitionerDetailsSlot), any(), any())
+      }
+
+      Assert.assertNotNull(practitionerDetailsSlot.captured)
+    }
+
+  @Test
+  fun testUnSuccessfulOnlineLoginForDifferentUserInDifferentLocationWithSyncStrategyLocation() {
     updateCredentials()
     secureSharedPreference.saveCredentials("nativeUser", "n4t1veP5wd".toCharArray())
+    sharedPreferencesHelper.write(ResourceType.Location.name, listOf("test-location"))
+    sharedPreferencesHelper.write(
+      SharedPreferenceKey.PRACTITIONER_ID.name,
+      "nativeUser-practitioner-id"
+    )
+
+    coEvery {
+      tokenAuthenticator.fetchAccessToken(thisUsername, thisPassword.toCharArray())
+    } returns
+      Result.success(
+        OAuthResponse(
+          accessToken = "very_new_top_of_the_class_access_token",
+          tokenType = "you_guess_it",
+          refreshToken = "another_very_refreshing_token",
+          refreshExpiresIn = 540000,
+          scope = "open_my_guy",
+        ),
+      )
+    coEvery { keycloakService.fetchUserInfo() } returns
+      Response.success(UserInfo(keycloakUuid = "awesome_uuid"))
+    val bundle =
+      Bundle().apply {
+        addEntry(
+          Bundle.BundleEntryComponent().apply {
+            resource =
+              practitionerDetails().apply {
+                fhirPractitionerDetails =
+                  FhirPractitionerDetails().apply {
+                    practitionerId = StringType("my-test-practitioner-id")
+                    locations = mutableListOf(Location().apply { id = "another-test-location" })
+                  }
+              }
+          },
+        )
+      }
+    coEvery { fhirResourceService.getResource(any()) } returns bundle
     every { tokenAuthenticator.sessionActive() } returns false
+
     loginViewModel.login(mockedActivity(isDeviceOnline = true))
-    Assert.assertFalse(loginViewModel.showProgressBar.value!!)
     Assert.assertEquals(
-      LoginErrorState.MULTI_USER_LOGIN_ATTEMPT,
+      LoginErrorState.ERROR_MATCHING_SYNC_STRATEGY,
       loginViewModel.loginErrorState.value!!,
     )
   }
@@ -253,12 +370,12 @@ internal class LoginViewModelTest : RobolectricTest() {
     Assert.assertTrue(loginViewModel.navigateToHome.value!!)
 
     // Login was successful savePractitionerDetails was called
-    val bundleSlot = slot<Bundle>()
-    verify { loginViewModel.savePractitionerDetails(capture(bundleSlot), any(), any()) }
+    val practitionerDetailsSlot = slot<PractitionerDetails>()
+    verify {
+      loginViewModel.savePractitionerDetails(capture(practitionerDetailsSlot), any(), any())
+    }
 
-    Assert.assertNotNull(bundleSlot.captured)
-    Assert.assertTrue(bundleSlot.captured.entry.isNotEmpty())
-    Assert.assertTrue(bundleSlot.captured.entry[0].resource is PractitionerDetails)
+    Assert.assertNotNull(practitionerDetailsSlot.captured)
   }
 
   @Test
@@ -489,18 +606,10 @@ internal class LoginViewModelTest : RobolectricTest() {
   fun testSavePractitionerDetails() {
     coEvery { defaultRepository.create(true, any()) } returns listOf()
     loginViewModel.savePractitionerDetails(
-      Bundle()
-        .addEntry(
-          Bundle.BundleEntryComponent().apply {
-            resource =
-              practitionerDetails().apply {
-                fhirPractitionerDetails =
-                  FhirPractitionerDetails().apply {
-                    practitionerId = StringType("my-test-practitioner-id")
-                  }
-              }
-          },
-        ),
+      practitionerDetails().apply {
+        fhirPractitionerDetails =
+          FhirPractitionerDetails().apply { practitionerId = StringType("my-test-practitioner-id") }
+      },
       UserInfo(),
     ) {}
     Assert.assertNotNull(
