7595cc315d0115dd4a165fd7b25f25e4c9e6bac1
Raynafs <raynafs90@gmail.com>
2023-11-20 15:52:29 +0300

    Adds refresh with translations and insights page

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/usersetting/UserSettingInsightScreen.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/usersetting/UserSettingInsightScreen.kt
@@ -16,11 +16,13 @@
 
 package org.smartregister.fhircore.quest.ui.usersetting
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -28,9 +30,15 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.primarySurface
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -39,62 +47,104 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import org.smartregister.fhircore.quest.R
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun UserSettingInsightScreen(
   unsyncedResources: List<Pair<String, Int>>,
   onDismissRequest: () -> Unit,
 ) {
-  Box(Modifier.clip(RectangleShape).fillMaxWidth().background(Color.White)) {
-    Dialog(onDismissRequest = onDismissRequest) {
-      Column(
-        modifier = Modifier.fillMaxWidth().wrapContentHeight().background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-      ) {
-        Text(
-          text = stringResource(id = R.string.unsynced_resources),
-          modifier = Modifier.padding(11.dp),
-          style = TextStyle(color = Color.Black, fontSize = 20.sp),
-          fontWeight = FontWeight.Light,
-        )
-        LazyColumn(modifier = Modifier.wrapContentHeight()) {
-          items(unsyncedResources) { language ->
-            Box(Modifier.fillMaxWidth().padding(15.dp)) {
-              Text(
-                text = language.first,
-                modifier = Modifier.align(Alignment.CenterStart),
-                fontWeight = FontWeight.Light,
-              )
-              Text(
-                text = language.second.toString(),
-                modifier = Modifier.align(Alignment.CenterEnd),
-              )
-            }
-            Spacer(modifier = Modifier.padding(1.dp))
+
+  Scaffold(
+    topBar = {
+      TopAppBar(
+        title = { Text(text = stringResource(org.smartregister.fhircore.engine.R.string.insights)) },
+        navigationIcon = {
+          IconButton(onClick = { }) {
+            Icon(Icons.Filled.ArrowBack, null)
           }
+        },
+        contentColor = Color.White,
+        backgroundColor = MaterialTheme.colors.primary,
+      )
+    },
+    backgroundColor = colorResource(id = org.smartregister.fhircore.engine.R.color.backgroundGray),
+  ) {
+  Box(
+    Modifier
+      .clip(RectangleShape)
+      .fillMaxSize()
+      .background(Color.White)) {
+//    Dialog(onDismissRequest = onDismissRequest) {
+    Column(
+      modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+        .background(Color.White),
+      horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+      Text(
+        text = stringResource(id = R.string.unsynced_resources),
+        modifier = Modifier.padding(11.dp),
+        style = TextStyle(color = Color.Black, fontSize = 20.sp),
+        fontWeight = FontWeight.Light,
+      )
+      LazyColumn(modifier = Modifier.wrapContentHeight()) {
+        items(unsyncedResources) { language ->
+          Box(
+            Modifier
+              .fillMaxWidth()
+              .padding(15.dp)
+          ) {
+            Text(
+              text = language.first,
+              modifier = Modifier.align(Alignment.CenterStart),
+              fontWeight = FontWeight.Light,
+            )
+            Text(
+              text = language.second.toString(),
+              modifier = Modifier.align(Alignment.CenterEnd),
+            )
+          }
+          Spacer(modifier = Modifier.padding(1.dp))
         }
-        Column(Modifier.wrapContentWidth().wrapContentHeight().padding(4.dp)) {
-          Surface(shape = RoundedCornerShape(0.dp)) {
-            OutlinedButton(
-              onClick = onDismissRequest,
-              border = BorderStroke(0.7.dp, MaterialTheme.colors.primarySurface),
-            ) {
-              Text(
-                text = stringResource(R.string.dismiss),
-                modifier = Modifier.padding(6.dp),
-                style = TextStyle(color = MaterialTheme.colors.primarySurface, fontSize = 14.sp),
-              )
-            }
+      }
+      Column(
+        Modifier
+          .wrapContentWidth()
+          .wrapContentHeight()
+          .padding(4.dp)
+      ) {
+        Surface(shape = RoundedCornerShape(0.dp)) {
+          OutlinedButton(
+            onClick = onDismissRequest,
+            border = BorderStroke(0.7.dp, MaterialTheme.colors.primarySurface),
+          ) {
+            Text(
+              text = stringResource(R.string.refresh),
+              modifier = Modifier.padding(6.dp),
+              style = TextStyle(color = MaterialTheme.colors.primarySurface, fontSize = 14.sp),
+            )
           }
         }
       }
     }
+  }
+  }
+}
+
+@Preview
+@Composable
+fun UserSettingInsightScreenPreview() {
+  UserSettingInsightScreen(unsyncedResources = listOf( Pair("Sync", 5), Pair("User", 4))) {
   }
 }
