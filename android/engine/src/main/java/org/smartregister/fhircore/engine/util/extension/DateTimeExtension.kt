f3329d6ca9b0869de0a45364c018f1d85563fb4b
Simon Njoroge <snjoroge@ona.io>
2023-11-20 19:47:19 +0300

    Remove manual age calculation

--- android/engine/src/main/java/org/smartregister/fhircore/engine/util/extension/DateTimeExtension.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/util/extension/DateTimeExtension.kt
@@ -147,27 +147,5 @@ fun calculateAge(date: Date, context: Context, localDateNow: LocalDate = LocalDa
   }.trim()
 }
 
-fun calculateAgeFromDays(ageInDays: Int, context: Context): String {
-  val period = Period.ofDays(ageInDays)
-  val years = period.days / 365
-  val months = (period.days % 365) / 30
-  val weeks = ((period.days % 365) % 30) / 7
-  val days = ((period.days % 365) % 30) % 7
-
-  return when {
-    years in 1..4 ->
-      context.abbreviateString(R.string.year, years) +
-        context.abbreviateString(R.string.month, months)
-    years >= 5 -> context.abbreviateString(R.string.year, years)
-    months > 0 ->
-      context.abbreviateString(R.string.month, months) +
-        context.abbreviateString(R.string.weeks, weeks)
-    weeks > 0 ->
-      context.abbreviateString(R.string.weeks, weeks) +
-        context.abbreviateString(R.string.days, days)
-    else -> "$days${context.getString(R.string.days).lowercase().abbreviate()} "
-  }.trim()
-}
-
 private fun Context.abbreviateString(resourceId: Int, content: Int) =
   if (content > 0) "$content${this.getString(resourceId).lowercase().abbreviate()} " else ""
