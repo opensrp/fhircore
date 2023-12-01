6bb27c7d27bff7ad47c8e5aa81ee44f3573c525a
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2023-12-01 07:02:34 +0300

    Update the fhircore version

--- android/quest/build.gradle.kts
+++ android/quest/build.gradle.kts
@@ -57,7 +57,7 @@ android {
     minSdk = 26
     targetSdk = 34
     versionCode = 7
-    versionName = "1.0.1"
+    versionName = "1.2.0"
     multiDexEnabled = true
 
     buildConfigField("boolean", "SKIP_AUTH_CHECK", "false")
