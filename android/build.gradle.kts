cb51e8b3d03ae8f70ba55c0c61be00d5e6e2ea1f
Raynafs <raynafs90@gmail.com>
2023-12-22 14:27:03 +0300

    Migrate a few plugins to version catalog

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,6 @@
 import com.diffplug.gradle.spotless.SpotlessExtension
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
 
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -6,6 +8,7 @@ buildscript {
   dependencies {
     classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
     classpath("com.android.tools.build:gradle:7.1.3")
+    classpath("org.jetbrains.dokka:dokka-base:1.8.20")
   }
 }
 
@@ -25,6 +28,9 @@ tasks.dokkaHtmlMultiModule {
   moduleName.set("OpenSRP")
   moduleVersion.set(project.version.toString())
   outputDirectory.set(file(buildDir.resolve("dokka")))
+  pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+    footerMessage = "(c) Ona Systems Inc"
+  }
 }
 
 allprojects {
