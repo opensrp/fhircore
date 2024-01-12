0ddbe2ae9499abf346cad554b7685230bef3fe6a
Brandy <brandyariana99@gmail.com>
2024-01-12 17:09:30 +0300

    sets color opacity on disable button (#2931)
    * sets ActionableButton background
    
    * sets ActionableButton text and icon opacity
    
    * adds opacity on the Button Properties
    
    * adds opacity on the Button Properties
    
    * adds colorOpacity for the button background and text
    
    * fixes failing test on user setting screen and app drawer
    
    * fixes failing test on questionnaire
    
    * changes the failing test on QuestionnaireActivityTest
    
    * gradle issues
    
    * reverted ti initial gradle versions
    
    * changes on questionnair null test
    
    * runs spotless check
    
    ---------
    
    Co-authored-by: Sharon Akinyi <79141719+sharon2719@users.noreply.github.com>

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/shared/components/ActionableButton.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/shared/components/ActionableButton.kt
@@ -78,6 +78,8 @@ fun ActionableButton(
     val backgroundColor = buttonProperties.backgroundColor.parseColor()
     val isButtonEnabled = buttonProperties.enabled.toBoolean()
     val clickable = buttonProperties.clickable.toBoolean()
+    val backgroundOpacity = buttonProperties.backgroundOpacity
+    val colorOpacity = buttonProperties.colorOpacity
     OutlinedButton(
       onClick = {
         if (
@@ -99,8 +101,14 @@ fun ActionableButton(
               statusColor.copy(alpha = 0.08f)
             },
           contentColor = statusColor,
-          disabledBackgroundColor = DefaultColor.copy(alpha = 0.08f),
-          disabledContentColor = DefaultColor,
+          disabledBackgroundColor =
+            if (backgroundOpacity == 0f) {
+              DefaultColor.copy(alpha = 0.08f)
+            } else {
+              backgroundColor.copy(alpha = backgroundOpacity)
+            },
+          disabledContentColor =
+            if (colorOpacity == 0f) DefaultColor else statusColor.copy(alpha = colorOpacity),
         ),
       modifier =
         modifier
@@ -140,7 +148,7 @@ fun ActionableButton(
             else -> statusColor
           }
         } else {
-          DefaultColor
+          if (colorOpacity == 0f) DefaultColor else statusColor.copy(alpha = colorOpacity)
         }
       if (buttonProperties.startIcon != null) {
         Image(
@@ -168,7 +176,9 @@ fun ActionableButton(
               else -> statusColor
             }
           } else {
-            DefaultColor.copy(0.9f)
+            if (colorOpacity == 0.0f) {
+              DefaultColor.copy(alpha = 0.9f)
+            } else statusColor.copy(alpha = colorOpacity)
           },
         textAlign = TextAlign.Start,
         overflow = TextOverflow.Ellipsis,
@@ -223,7 +233,9 @@ fun DisabledActionableButtonPreview() {
           status = ServiceStatus.UPCOMING.name,
           text = "Issue household bed-nets",
           contentColor = "#700f2b",
-          enabled = "true",
+          enabled = "false",
+          backgroundOpacity = 0.06f,
+          colorOpacity = 0.6f,
           buttonType = ButtonType.MEDIUM,
           startIcon = ImageConfig(reference = "ic_walk", type = ICON_TYPE_LOCAL),
         ),
