af047b076770c37bff9e8f8ac6f839f0db6c949b
Martin Ndegwa <mndegwa@ona.io>
2024-01-24 13:54:38 +0300

    [Workaround] Migrate to latest SDK Artifacts 💚 - - Fix issue missing SDK dependencies on reliant app graph

--- android/engine/build.gradle.kts
+++ android/engine/build.gradle.kts
@@ -1,3 +1,4 @@
+import Dependencies.removeIncompatibleDependencies
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 buildscript {
@@ -118,6 +119,7 @@ configurations {
     exclude(group = "javax", module = "javaee-api")
     exclude(group = "xml-apis")
     exclude(group = "xpp3")
+    removeIncompatibleDependencies()
   }
 }
 
@@ -262,6 +264,27 @@ dependencies {
   androidTestImplementation(libs.hilt.android.testing)
   androidTestImplementation(libs.benchmark.junit)
 
+  /**
+   * This is an SDK Dependency graph bug workaround file HAPI FHIR Dependencies missing at runtime
+   * after building FHIRCore application module
+   *
+   * To be included in the engine/build.gradle.kts file via apply {}
+   */
+  api(Dependencies.HapiFhir.structuresR4) { exclude(module = "junit") }
+  implementation(Dependencies.HapiFhir.guavaCaching)
+  implementation(Dependencies.HapiFhir.validationR4)
+  implementation(Dependencies.HapiFhir.validation) {
+    exclude(module = "commons-logging")
+    exclude(module = "httpclient")
+  }
+
+  constraints {
+    Dependencies.hapiFhirConstraints().forEach { (libName, constraints) ->
+      implementation(libName, constraints)
+    }
+  }
+
+  /** End SDK Dependency Graph workaround * */
   ktlint(libs.ktlint.main) {
     attributes { attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL)) }
   }
