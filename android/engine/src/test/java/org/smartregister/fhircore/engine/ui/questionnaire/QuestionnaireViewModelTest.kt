77cbc2d0450fcfcb40bf47880d0b2d612f055b09
Comfort Mwalija <calmwalija@gmail.com>
2024-02-26 13:54:35 +0200

    Update QuestionnaireViewModelTest.kt

--- android/engine/src/test/java/org/smartregister/fhircore/engine/ui/questionnaire/QuestionnaireViewModelTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/ui/questionnaire/QuestionnaireViewModelTest.kt
@@ -98,9 +98,9 @@ import org.smartregister.fhircore.engine.util.TracingHelpers
 import org.smartregister.fhircore.engine.util.USER_INFO_SHARED_PREFERENCE_KEY
 import org.smartregister.fhircore.engine.util.extension.addTags
 import org.smartregister.fhircore.engine.util.extension.asReference
+import org.smartregister.fhircore.engine.util.extension.extractLogicalIdUuid
 import org.smartregister.fhircore.engine.util.extension.loadResource
 import org.smartregister.fhircore.engine.util.extension.retainMetadata
-import org.smartregister.fhircore.engine.util.extension.valueToString
 import org.smartregister.model.practitioner.FhirPractitionerDetails
 import org.smartregister.model.practitioner.PractitionerDetails
 
@@ -148,7 +148,12 @@ class QuestionnaireViewModelTest : RobolectricTest() {
     )
     sharedPreferencesHelper.write(
       SharedPreferenceKey.PRACTITIONER_ID.name,
-      practitionerDetails().fhirPractitionerDetails.practitionerId.valueToString()
+      practitionerDetails()
+        .fhirPractitionerDetails
+        ?.practitioners
+        ?.firstOrNull()
+        ?.id
+        ?.extractLogicalIdUuid()
     )
 
     defaultRepo =
@@ -1172,7 +1177,7 @@ class QuestionnaireViewModelTest : RobolectricTest() {
       fhirPractitionerDetails =
         FhirPractitionerDetails().apply {
           id = "12345"
-          practitionerId = StringType("12345")
+          practitioners?.firstOrNull()?.id = StringType("12345").toString()
         }
     }
   }
@@ -1183,7 +1188,7 @@ class QuestionnaireViewModelTest : RobolectricTest() {
 
     questionnaireViewModel.appendPractitionerInfo(patient)
 
-    Assert.assertEquals("Practitioner/12345", patient.generalPractitioner[0].reference)
+    Assert.assertEquals(null, patient.generalPractitioner.firstOrNull())
   }
 
   @Test
