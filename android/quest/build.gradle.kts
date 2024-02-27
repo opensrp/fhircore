426c0d303bc0aa57648a9b41200fa79f33d2d8a9
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2024-02-27 14:53:41 +0300

    Move custom .kts to buildSrc as precompiled plugins (#3101)
    * Move custom .kts to buildSrc as precompiled plugins
    
    https://docs.gradle.org/8.2/userguide/custom_plugins.html#sec:precompiled_plugins
    
    * Just throw FileNotFoundException if .properties file not found
    
    * Use empty local.properties and keystore.properties for CI
    
    ---------
    
    Co-authored-by: Peter Lubell-Doughtie <peter@ona.io>
    Co-authored-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/quest/build.gradle.kts
+++ android/quest/build.gradle.kts
@@ -7,13 +7,10 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.json.JSONArray
 import org.json.JSONObject
 
-buildscript {
-  apply(from = "../jacoco.gradle.kts")
-  apply(from = "../properties.gradle.kts")
-  apply(from = "../ktlint.gradle.kts")
-}
-
 plugins {
+  `jacoco-report`
+  `project-properties`
+  `ktlint`
   id("com.android.application")
   id("kotlin-android")
   id("kotlin-kapt")
