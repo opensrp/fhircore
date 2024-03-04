e8967f5232b25c6f89b2aa1e545657361cb03b4f
Elly Kitoto <junkmailstoelly@gmail.com>
2024-03-04 22:37:39 +0300

    Use TristateCheckbox on MultiSelect view
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/main/java/org/smartregister/fhircore/engine/ui/multiselect/MultiSelectView.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/ui/multiselect/MultiSelectView.kt
@@ -25,9 +25,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.Stable
@@ -37,6 +37,7 @@ import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.util.LinkedList
@@ -44,29 +45,33 @@ import java.util.LinkedList
 @Stable
 class TreeNode<T>(
   val id: String,
+  val parentId: String?,
   val data: T,
-  val children: List<TreeNode<T>>? = null,
+  val children: List<String>? = null,
 )
 
 @Composable
 fun <T> ColumnScope.MultiSelectView(
-  treeNode: TreeNode<T>,
-  selectedNodes: SnapshotStateMap<String, Boolean>,
+  rootNodeId: String,
+  treeNodeMap: SnapshotStateMap<String, TreeNode<T>>,
+  selectedNodes: SnapshotStateMap<String, ToggleableState>,
   depth: Int = 0,
   content: @Composable (TreeNode<T>) -> Unit,
 ) {
   val collapsedState = remember { mutableStateOf(false) }
   MultiSelectCheckbox(
     selectedNodes = selectedNodes,
-    treeNode = treeNode,
+    treeNodeMap = treeNodeMap,
+    currentTreeNode = treeNodeMap.getValue(rootNodeId),
     depth = depth,
     content = content,
     collapsedState = collapsedState,
   )
   if (collapsedState.value) {
-    treeNode.children?.forEach {
+    treeNodeMap.getValue(rootNodeId).children?.forEach {
       MultiSelectView(
-        treeNode = it,
+        rootNodeId = it,
+        treeNodeMap = treeNodeMap,
         selectedNodes = selectedNodes,
         depth = depth + 16,
         content = content,
@@ -77,18 +82,20 @@ fun <T> ColumnScope.MultiSelectView(
 
 @Composable
 fun <T> MultiSelectCheckbox(
-  selectedNodes: SnapshotStateMap<String, Boolean>,
-  treeNode: TreeNode<T>,
+  selectedNodes: SnapshotStateMap<String, ToggleableState>,
+  treeNodeMap: Map<String, TreeNode<T>>,
+  currentTreeNode: TreeNode<T>,
   depth: Int,
   content: @Composable (TreeNode<T>) -> Unit,
   collapsedState: MutableState<Boolean>,
 ) {
+  val checked = remember { mutableStateOf(false) }
   Column {
     Row(
       modifier = Modifier.fillMaxWidth().padding(start = depth.dp),
       verticalAlignment = Alignment.CenterVertically,
     ) {
-      if (!treeNode.children.isNullOrEmpty()) {
+      if (!currentTreeNode.children.isNullOrEmpty()) {
         Icon(
           imageVector =
             if (collapsedState.value) {
@@ -99,22 +106,46 @@ fun <T> MultiSelectCheckbox(
           modifier = Modifier.clickable { collapsedState.value = !collapsedState.value },
         )
       }
-      Checkbox(
-        checked = selectedNodes[treeNode.id] ?: false,
-        onCheckedChange = { checked ->
-          selectedNodes[treeNode.id] = checked
+
+      TriStateCheckbox(
+        state = selectedNodes[currentTreeNode.id] ?: ToggleableState.Off,
+        onClick = {
+          checked.value = !checked.value
+          selectedNodes[currentTreeNode.id] = ToggleableState(checked.value)
+
+          var toggleableState: ToggleableState
+          var parentId = currentTreeNode.parentId
+
+          while (!parentId.isNullOrEmpty()) {
+            toggleableState = ToggleableState.Indeterminate
+            val parentNode = treeNodeMap[parentId]
+            if (
+              parentNode?.children?.all {
+                selectedNodes[it] == ToggleableState.Off || selectedNodes[it] == null
+              } == true
+            ) {
+              toggleableState = ToggleableState.Off
+            }
+            if (parentNode?.children?.all { selectedNodes[it] == ToggleableState.On } == true) {
+              toggleableState = ToggleableState.On
+            }
+            selectedNodes[parentId] = toggleableState
+            parentId = treeNodeMap[parentId]?.parentId
+          }
+
           // Select all the nested checkboxes
-          val linkedList = treeNode.children?.let { LinkedList(it) }
+          val linkedList =
+            currentTreeNode.children?.map { treeNodeMap.getValue(it) }?.let { LinkedList(it) }
 
           while (!linkedList.isNullOrEmpty()) {
             val currentNode = linkedList.removeFirst()
-            selectedNodes[currentNode.id] = checked
-            currentNode.children?.let { linkedList.addAll(it) }
+            selectedNodes[currentNode.id] = ToggleableState(checked.value)
+            currentNode.children?.forEach { linkedList.add(treeNodeMap.getValue(it)) }
           }
         },
         modifier = Modifier.padding(0.dp),
       )
-      content(treeNode)
+      content(currentTreeNode)
     }
   }
 }
@@ -125,51 +156,79 @@ fun MultiSelectViewPreview() {
   Column {
     MultiSelectView(
       selectedNodes = SnapshotStateMap(),
-      treeNode =
-        TreeNode(
-          id = "Kenya",
-          data = "Kenya",
-          children =
-            listOf(
-              TreeNode(
-                id = "nairobi",
-                data = "Nairobi",
-                children =
-                  listOf(
-                    TreeNode(
-                      id = "sampleId1",
-                      data = "Kibera - Clinic",
-                      children =
-                        listOf(
-                          TreeNode(
-                            id = "sampleId1.1",
-                            data = "Clinic Village 1",
-                            children =
-                              listOf(
-                                TreeNode(
-                                  id = "sampleId1.1.1",
-                                  data = "Sub village 1",
-                                ),
-                                TreeNode(
-                                  id = "sampleId1.1.2",
-                                  data = "Sub village 2",
-                                ),
-                              ),
-                          ),
-                          TreeNode(
-                            id = "sampleId3",
-                            data = "Clinic Village 2",
-                            children =
-                              listOf(
-                                TreeNode(id = "sampleId2.1", data = "Branch 2.1"),
-                              ),
-                          ),
-                        ),
-                    ),
-                  ),
-              ),
+      treeNodeMap =
+        SnapshotStateMap<String, TreeNode<String>>().apply {
+          put(
+            "kenya",
+            TreeNode(
+              id = "kenya",
+              parentId = null,
+              data = "Kenya",
+              children = listOf("nairobi"),
             ),
-        ),
+          )
+          put(
+            "nairobi",
+            TreeNode(
+              id = "nairobi",
+              parentId = "kenya",
+              data = "Nairobi",
+              children = listOf("sampleId1"),
+            ),
+          )
+          put(
+            "sampleId1",
+            TreeNode(
+              id = "sampleId1",
+              parentId = "nairobi",
+              data = "Kibera - Clinic",
+              children = listOf("sampleId1.1", "sampleId3"),
+            ),
+          )
+          put(
+            "sampleId1.1",
+            TreeNode(
+              id = "sampleId1.1",
+              parentId = "sampleId1",
+              data = "Clinic Village 1",
+              children = listOf("sampleId1.1.1", "sampleId1.1.2"),
+            ),
+          )
+          put(
+            "sampleId1.1.1",
+            TreeNode(
+              id = "sampleId1.1.1",
+              parentId = "sampleId1.1",
+              data = "Sub village 1",
+            ),
+          )
+          put(
+            "sampleId1.1.2",
+            TreeNode(
+              id = "sampleId1.1.2",
+              parentId = "sampleId1.1",
+              data = "Sub village 2",
+            ),
+          )
+          put(
+            "sampleId3",
+            TreeNode(
+              id = "sampleId3",
+              parentId = "sampleId1",
+              data = "Clinic Village 2",
+              children = listOf("sampleId2.1"),
+            ),
+          )
+          put(
+            "sampleId2.1",
+            TreeNode(
+              id = "sampleId2.1",
+              parentId = "sampleId3",
+              data = "Sub village 2",
+            ),
+          )
+        },
+      rootNodeId = "kenya",
       depth = 0,
       content = { treeNode -> Column { Text(text = treeNode.data) } },
     )
