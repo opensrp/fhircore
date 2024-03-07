9ecc63e948a89ee3f397ddbda4c3cf4a8fb5ed8d
Raynafs <raynafs90@gmail.com>
2024-03-07 11:44:34 +0300

    Add pagination to list resource

--- android/quest/src/androidTest/java/org/smartregister/fhircore/quest/integration/ui/register/RegisterScreenTest.kt
+++ android/quest/src/androidTest/java/org/smartregister/fhircore/quest/integration/ui/register/RegisterScreenTest.kt
@@ -139,7 +139,7 @@ class RegisterScreenTest {
         screenTitle = "Register101",
         isFirstTimeSync = true,
         registerConfiguration =
-          configurationRegistry.retrieveConfiguration(ConfigType.Register, "childRegisterR"),
+          configurationRegistry.retrieveConfiguration(ConfigType.Register, "childRegister"),
         registerId = "register101",
         totalRecordsCount = 0,
         filteredRecordsCount = 0,
