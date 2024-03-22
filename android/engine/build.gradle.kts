e559fc212a6b84efacddcb77feed02bb55b89364
L≡ZRS <12814349+LZRS@users.noreply.github.com>
2024-03-23 01:46:57 +0300

    Upgrade to engine v1.0.0-preview5-SNAPSHOT

--- android/engine/build.gradle.kts
+++ android/engine/build.gradle.kts
@@ -193,7 +193,7 @@ dependencies {
 
   api("org.smartregister:common:0.1.0-alpha05-preview3-SNAPSHOT") { isTransitive = true }
 
-  api("org.smartregister:engine:1.0.0-preview4-SNAPSHOT") {
+  api("org.smartregister:engine:1.0.0-preview5-SNAPSHOT") {
     isTransitive = true
     exclude(group = "com.google.android.fhir", module = "common")
     exclude(group = "com.github.ben-manes.caffeine")
