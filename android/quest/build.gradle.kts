72f599b86293e447678267afeeb8f19ff88b0f85
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-04-02 08:09:45 +0200

    Update build.gradle.kts

--- android/quest/build.gradle.kts
+++ android/quest/build.gradle.kts
@@ -155,7 +155,7 @@ android {
       applicationIdSuffix = ".mwcoreDev"
       versionNameSuffix = "-mwcoreDev"
       versionCode = 33
-      versionName = "0.1.22-beta1"
+      versionName = "0.1.22-beta2"
     }
     create("mwcoreProd") {
       dimension = "apps"
