d429701c69087fdbd0963b0cc5e67ccfe4b150af
Aurangzaib Umer <aurangzaib.umer@venturedive.com>
2024-05-14 12:57:30 +0500

    updated tests for geowidgetviewmodel

--- android/geowidget/src/test/java/org/smartregister/fhircore/geowidget/screens/GeoWidgetViewModelTest.kt
+++ android/geowidget/src/test/java/org/smartregister/fhircore/geowidget/screens/GeoWidgetViewModelTest.kt
@@ -18,7 +18,6 @@ package org.smartregister.fhircore.geowidget.screens
 
 import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.platform.app.InstrumentationRegistry
 import ca.uhn.fhir.parser.IParser
 import com.google.android.fhir.FhirEngine
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -28,24 +27,32 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.spyk
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.smartregister.fhircore.engine.configuration.ConfigurationRegistry
 import org.smartregister.fhircore.engine.configuration.app.ConfigService
 import org.smartregister.fhircore.engine.data.local.DefaultRepository
 import org.smartregister.fhircore.engine.rulesengine.ConfigRulesExecutor
+import org.smartregister.fhircore.engine.util.DispatcherProvider
 import org.smartregister.fhircore.engine.util.SharedPreferencesHelper
 import org.smartregister.fhircore.engine.util.fhirpath.FhirPathDataExtractor
 import org.smartregister.fhircore.geowidget.model.Coordinates
 import org.smartregister.fhircore.geowidget.model.Feature
 import org.smartregister.fhircore.geowidget.model.Geometry
+import org.smartregister.fhircore.geowidget.model.ServicePointType
 import org.smartregister.fhircore.geowidget.rule.CoroutineTestRule
 import java.util.UUID
 import javax.inject.Inject
+
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.O_MR1], application = HiltTestApplication::class)
@@ -74,8 +81,6 @@ class GeoWidgetViewModelTest {
 
     private val fhirEngine = mockk<FhirEngine>()
 
-    private val context = InstrumentationRegistry.getInstrumentation().targetContext
-
     private val configRulesExecutor: ConfigRulesExecutor = mockk()
 
     @Inject
@@ -83,9 +88,15 @@ class GeoWidgetViewModelTest {
 
     @Inject
     lateinit var parser: IParser
+    private lateinit var viewModel: GeoWidgetViewModel
+
+    @Mock
+    private lateinit var dispatcherProvider: DispatcherProvider
 
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = GeoWidgetViewModel(dispatcherProvider)
         hiltRule.inject()
         sharedPreferencesHelper = mockk()
         configurationRegistry = mockk()
@@ -109,13 +120,82 @@ class GeoWidgetViewModelTest {
     }
 
     @Test
+    fun `test adding locations to map`() {
+        // Given
+        val feature1 = Feature(
+            geometry = Geometry(coordinates = listOf(Coordinates(0.0, 0.0))),
+            id = "id1",
+            type = "type1",
+            serverVersion = 1
+        )
+        val feature2 = Feature(
+            geometry = Geometry(coordinates = listOf(Coordinates(0.0, 0.0))),
+            id = "id2",
+            type = "type2",
+            serverVersion = 2
+        )
+        val features = setOf(feature1, feature2)
+
+        // When
+        viewModel.addLocationsToMap(features)
+
+        // Then
+        val result = runBlocking { viewModel.featuresFlow.first() }
+        assertEquals(2, result.size)
+    }
+
+    @Test
+    fun `test clearing locations`() {
+        // Given
+        val feature1 = Feature(
+            geometry = Geometry(coordinates = listOf(Coordinates(0.0, 0.0))),
+            id = "id1",
+            type = "type1",
+            serverVersion = 1
+        )
+        val feature2 = Feature(
+            geometry = Geometry(coordinates = listOf(Coordinates(0.0, 0.0))),
+            id = "id2",
+            type = "type2",
+            serverVersion = 2
+        )
+        val features = setOf(feature1, feature2)
+        viewModel.addLocationsToMap(features)
+
+        // When
+        viewModel.clearLocations()
+
+        // Then
+        val result = runBlocking { viewModel.featuresFlow.first() }
+        assertEquals(0, result.size)
+    }
+
+    fun `test mapping service point keys to types`() {
+        // Given
+        val expectedMap = mutableMapOf<String, ServicePointType>()
+        ServicePointType.values().forEach {
+            expectedMap[it.name.lowercase()] = it
+        }
+
+        // When
+        val result = viewModel.getServicePointKeyToType()
+
+        // Then
+        assertEquals(expectedMap.size, result.size)
+        expectedMap.forEach { (key, expectedValue) ->
+            val actualValue = result[key]
+            assertEquals(expectedValue.name, actualValue?.name)
+        }
+    }
+
+    @Test
     fun `add location to map`() {
         val serverVersion = (1..10).random()
         val locations = setOf(
             Feature(
                 id = UUID.randomUUID().toString(),
                 geometry = Geometry(
-                    coordinates = listOf(Coordinates(34.76,68.23))
+                    coordinates = listOf(Coordinates(34.76, 68.23))
                 ),
                 properties = mapOf(),
                 serverVersion = serverVersion
@@ -123,7 +203,7 @@ class GeoWidgetViewModelTest {
             Feature(
                 id = UUID.randomUUID().toString(),
                 geometry = Geometry(
-                    coordinates = listOf(Coordinates(34.76,68.23))
+                    coordinates = listOf(Coordinates(34.76, 68.23))
                 ),
                 properties = mapOf(),
                 serverVersion = serverVersion
@@ -131,61 +211,9 @@ class GeoWidgetViewModelTest {
         )
         geoWidgetViewModel.addLocationsToMap(locations)
 
-        assertEquals(geoWidgetViewModel.featuresFlow.value.size,locations.size)
+        assertEquals(geoWidgetViewModel.featuresFlow.value.size, locations.size)
     }
-    /*
-  @Test
-  fun getFamiliesFeatureCollectionShouldCallGetFamiliesAndGenerateFeatureCollection() {
-    val families: List<Pair<Group, Location>> = emptyList()
-    coEvery { geoWidgetViewModel.getFamilies() } returns families
-    val featureCollection = runBlocking { geoWidgetViewModel.getFamiliesFeatureCollection(context) }
 
-    coVerify { geoWidgetViewModel.getFamilies() }
-    Assert.assertNotNull(featureCollection)
-  }
-
-  @Test
-  fun getFamiliesFeatureCollectionStreamShouldCallGetFamilyFeaturesCollection() {
-    val featureCollection = mockk<FeatureCollection>()
-
-    coEvery { geoWidgetViewModel.getFamiliesFeatureCollection(context) } returns featureCollection
-
-    val featureCollectionLiveData = geoWidgetViewModel.getFamiliesFeatureCollectionStream(context)
-
-    coVerify { geoWidgetViewModel.getFamiliesFeatureCollection(context) }
-    Assert.assertEquals(featureCollection, featureCollectionLiveData.value)
-  }
-
-  @Test
-  fun getFamiliesShouldCallFhirEngineSearchAndPairLocationWithFamily() {
-    val locationJson =
-      """{"resourceType":"Location","id":"136702","meta":{"versionId":"3","lastUpdated":"2022-07-28T18:21:39.739+00:00","source":"#18c074df71ca7366"},"status":"active","name":"Kenyatta Hospital Visitors Parking","description":"Parking Lobby","telecom":[{"system":"phone","value":"020 2726300"},{"system":"phone","value":"(+254)0709854000"},{"system":"phone","value":"(+254)0730643000"},{"system":"email","value":"knhadmin@knh.or.ke"}],"address":{"line":["P.O. Box 20723"],"city":"Nairobi","postalCode":"00202","country":"Kenya"},"physicalType":{"coding":[{"system":"http://terminology.hl7.org/CodeSystem/location-physical-type","code":"area","display":"Area"}]},"position":{"longitude":36.80826008319855,"latitude":-1.301070677485388},"managingOrganization":{"reference":"Organization/400"},"partOf":{"reference":"Location/136710"}}"""
-    val groupJson =
-      """{"resourceType":"Group","id":"1122f50c-5499-4eaa-bd53-a5364371a2ba","meta":{"versionId":"5","lastUpdated":"2022-06-23T14:55:37.217+00:00","source":"#75f9db2107ef0977"},"identifier":[{"use":"official","value":"124"},{"use":"secondary","value":"c90cd5e3-a1c4-4040-9745-433aea9fe174"}],"active":true,"type":"person","code":{"coding":[{"system":"https://www.snomed.org","code":"35359004","display":"Family"}]},"name":"new family","managingEntity":{"reference":"Organization/105"},"characteristic":[{"valueReference":{"reference":"Location/136702"}}],"member":[{"entity":{"reference":"Patient/7d84a2d0-8706-485a-85f5-8313f16bafa1"}},{"entity":{"reference":"Patient/0beaa1e3-64a9-436f-91af-36cbdaff5628"}},{"entity":{"reference":"Patient/a9e466a6-6237-46e0-bcda-c66036414aed"}},{"entity":{"reference":"Patient/7e62cc99-d992-484c-ace8-a43dba87ed22"}},{"entity":{"reference":"Patient/cd1c9616-bdfd-4947-907a-5f08e2bcd8a9"}}]}"""
-    val location = locationJson.decodeResourceFromString<Location>()
-    val group = groupJson.decodeResourceFromString<Group>()
-
-    coEvery { fhirEngine.search<Group>(any()) } returns
-      listOf(SearchResult(resource = group, null, null))
-    coEvery { fhirEngine.get(ResourceType.Location, any()) } returns location
-
-    val familiesWithLocations = runBlocking { geoWidgetViewModel.getFamilies() }
-
-    Assert.assertEquals(group, familiesWithLocations[0].first)
-    Assert.assertEquals(location, familiesWithLocations[0].second)
-  }
-
-  @Test
-  fun saveLocationShouldCallDefaultRepositorySave() {
-    val locationJson =
-      """{"resourceType":"Location","id":"136702","meta":{"versionId":"3","lastUpdated":"2022-07-28T18:21:39.739+00:00","source":"#18c074df71ca7366"},"status":"active","name":"Kenyatta Hospital Visitors Parking","description":"Parking Lobby","telecom":[{"system":"phone","value":"020 2726300"},{"system":"phone","value":"(+254)0709854000"},{"system":"phone","value":"(+254)0730643000"},{"system":"email","value":"knhadmin@knh.or.ke"}],"address":{"line":["P.O. Box 20723"],"city":"Nairobi","postalCode":"00202","country":"Kenya"},"physicalType":{"coding":[{"system":"http://terminology.hl7.org/CodeSystem/location-physical-type","code":"area","display":"Area"}]},"position":{"longitude":36.80826008319855,"latitude":-1.301070677485388},"managingOrganization":{"reference":"Organization/400"},"partOf":{"reference":"Location/136710"}}"""
-    val location = locationJson.decodeResourceFromString<Location>()
-    coEvery { defaultRepository.create(true, location) } returns listOf("")
-
-    val locationLiveData = geoWidgetViewModel.saveLocation(location)
-
-    coVerify { defaultRepository.create(true, location) }
-    Assert.assertTrue(locationLiveData.value!!)
-  }
-}*/
 }
+
+
