c693746c20dfef4ffe5a12e32d655171e904fd28
Aurangzaib Umer <aurangzaib.umer@venturedive.com>
2024-05-24 13:24:03 +0500

    removed extra code of WIP search functionality of Map

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/launcher/GeoWidgetLauncherFragment.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/launcher/GeoWidgetLauncherFragment.kt
@@ -163,7 +163,6 @@ class GeoWidgetLauncherFragment : Fragment() {
                 fragmentManager = childFragmentManager,
                 fragment = fragment,
                 geoWidgetConfiguration = geoWidgetConfiguration,
-                searchText = geoWidgetLauncherViewModel.searchText,
               )
             }
           }
@@ -249,11 +248,6 @@ class GeoWidgetLauncherFragment : Fragment() {
         )
       }
     }
-  }
-
-  override fun onStop() {
-    super.onStop()
-    geoWidgetLauncherViewModel.searchText.value = ""
   }
 
   companion object {
