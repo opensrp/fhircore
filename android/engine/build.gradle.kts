241e03e76501f276ded3d36314e919481aa0c139
Martin Ndegwa <mndegwa@ona.io>
2024-08-09 16:04:15 +0300

    Replace JWT token parser library

--- android/engine/build.gradle.kts
+++ android/engine/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   `jacoco-report`
-  `ktlint`
+  ktlint
   id("com.android.library")
   id("kotlin-android")
   id("kotlin-kapt")
@@ -162,7 +162,7 @@ dependencies {
   api(libs.glide)
   api(libs.knowledge) { exclude(group = "org.slf4j", module = "jcl-over-slf4j") }
   api(libs.p2p.lib)
-  api(libs.jjwt)
+  api(libs.java.jwt)
   api(libs.fhir.common.utils) { exclude(group = "org.slf4j", module = "jcl-over-slf4j") }
   api(libs.runtime.livedata)
   api(libs.foundation)
