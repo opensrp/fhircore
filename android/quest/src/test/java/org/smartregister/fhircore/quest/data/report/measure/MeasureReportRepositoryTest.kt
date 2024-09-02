1107d3067f219540cfaa631336d491881ebff9f9
Martin Ndegwa <mndegwa@ona.io>
2024-09-02 15:45:50 +0300

    Fix MeasureReportRepositoryTest

--- android/quest/src/test/java/org/smartregister/fhircore/quest/data/report/measure/MeasureReportRepositoryTest.kt
+++ android/quest/src/test/java/org/smartregister/fhircore/quest/data/report/measure/MeasureReportRepositoryTest.kt
@@ -135,7 +135,7 @@ class MeasureReportRepositoryTest : RobolectricTest() {
         fhirPathDataExtractor = mockk(),
         parser = parser,
         context = ApplicationProvider.getApplicationContext(),
-        dispatcherProvider = dispatcherProvider
+        dispatcherProvider = dispatcherProvider,
       )
   }
 
