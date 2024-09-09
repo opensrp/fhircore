551292be9b7239a2a897e4813899a52735440121
owais-vd <owais.ali@venturedive.com>
2024-09-09 14:22:54 +0500

    :construction: handle db operation command

--- android/quest/src/main/java/org/smartregister/fhircore/quest/FCTProvider.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/FCTProvider.kt
@@ -4,10 +4,15 @@ import android.content.ContentProvider
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
+import android.database.DatabaseUtils
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.net.Uri
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.paging.liveData
 import ca.uhn.fhir.context.FhirContext
+import com.google.android.fhir.FhirEngine
 import org.smartregister.fhircore.engine.configuration.ConfigurationRegistry
 import org.smartregister.fhircore.engine.data.local.register.RegisterRepository
 import org.smartregister.fhircore.engine.rulesengine.ResourceDataRulesExecutor
@@ -27,17 +32,23 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.internal.wait
+import okio.ByteString.Companion.toByteString
+import org.json.JSONArray
+import org.json.JSONObject
 import org.smartregister.fhircore.engine.configuration.register.RegisterConfiguration
 import org.smartregister.fhircore.quest.ui.register.RegisterFilterState
 import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.util.Base64
 import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
 
 class FCTProvider : ContentProvider() {
     //private val registerViewModel by viewModels<RegisterViewModel>()
 
     private lateinit var registerViewModel: RegisterViewModel
     private val parser = FhirContext.forR4Cached().newJsonParser()
+    private lateinit var fhirEngine: FhirEngine
    /* @Inject
     lateinit var registerRepository: RegisterRepository
     @Inject
@@ -50,11 +61,20 @@ class FCTProvider : ContentProvider() {
     @InstallIn(SingletonComponent::class)
     @EntryPoint
     interface FCTProviderEntryPoint {
+        fun getFhirEngine(): FhirEngine
         fun registerRepository(): RegisterRepository
         fun configurationRegistry(): ConfigurationRegistry
         fun sharedPreferenceHelper(): SharedPreferencesHelper
         fun dispatcherProvider(): DispatcherProvider
         fun resourceDataRulesExecutor(): ResourceDataRulesExecutor
+    }
+
+    private fun getFhirEngine(appContext: Context): FhirEngine {
+        val hiltEntryPoint = EntryPointAccessors.fromApplication(
+            appContext,
+            FCTProviderEntryPoint::class.java
+        )
+        return hiltEntryPoint.getFhirEngine()
     }
 
     private fun getRegisterRepository(appContext: Context): RegisterRepository {
@@ -103,9 +123,10 @@ class FCTProvider : ContentProvider() {
             registerRepository = getRegisterRepository(context!!),
             configurationRegistry = getConfigurationRegistry(context!!),
             sharedPreferencesHelper = getSharedPreferencesHelper(context!!),
-            dispatcherProvider = getDispatcherProvider(context!!),
+            //dispatcherProvider = getDispatcherProvider(context!!),
             resourceDataRulesExecutor = getResourceDataRulesExecutor(context!!)
         )
+        fhirEngine = getFhirEngine(context!!)
         return true
     }
 
@@ -117,7 +138,56 @@ class FCTProvider : ContentProvider() {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     override fun call(method: String, arg: String?, extras: Bundle?): Bundle {
-        Timber.d("arg: $arg")
+        Timber.d("--method: $method --arg: $arg --extras: $extras")
+
+        if (method == "db_operation") {
+
+            val dbHelper = DBHelper(context, getDatabaseVersion())
+            val db = dbHelper.writableDatabase
+
+            val decodedArg = arg!!.decompress()
+            val data = when(getQueryType(decodedArg)) {
+                QueryType.SELECT, QueryType.UNKNOWN -> {
+
+                    var cursor: Cursor? = null
+                    var result = ""
+                    try {
+                        cursor = db.rawQuery(decodedArg, null)
+                        result = extractAllRecords(cursor)
+                    } catch (ex: Exception) {
+                        val jsonObject = JSONObject().apply {
+                            put("success", false)
+                            put("error", ex.localizedMessage ?: ex.message ?: ex.toString())
+                        }
+                        result = jsonObject.toString()
+                    } finally {
+                        cursor?.close()
+                    }
+                    result
+                }
+                else -> {
+                    val jsonObject = JSONObject()
+                    try {
+                        db.execSQL(decodedArg)
+                        jsonObject.put("success", true)
+                    } catch (ex: Exception) {
+                        jsonObject.put("success", false)
+                        jsonObject.put("error", ex.localizedMessage ?: ex.message ?: ex.toString())
+                    }
+                    jsonObject.toString()
+                }
+            }
+           // val cursor = db.rawQuery(arg?.replace("+", " "), null)
+
+            //DatabaseUtils.dumpCursor(cursor)
+
+            //cursor.close()
+
+            return Bundle().apply {
+                putString("data", data.compress())
+            }
+        }
+
         val result = runBlocking {
             this.async {
 
@@ -176,5 +246,89 @@ class FCTProvider : ContentProvider() {
             putString("data", out.toString())
         }
 
+    }
+
+    private fun getQueryType(query: String) : QueryType {
+        return when {
+            isFound(query, "^SELECT\\s+") -> QueryType.SELECT
+            isFound(query, "^INSERT\\s+") -> QueryType.INSERT
+            isFound(query, "^UPDATE\\s+") -> QueryType.UPDATE
+            isFound(query, "^DELETE\\s+") -> QueryType.DELETE
+            else -> QueryType.UNKNOWN
+        }
+    }
+
+    private fun isFound(query: String, pattern: String): Boolean {
+        return pattern.toRegex(RegexOption.IGNORE_CASE).find(query.trim()) != null
+    }
+
+    private fun extractAllRecords(cursor: Cursor): String {
+        val jsonObject = JSONObject()
+        val jsonArray = JSONArray()
+
+        if (cursor.moveToFirst()) {
+            do {
+                val obj = JSONObject()
+                cursor.columnNames.forEachIndexed { columnIndex, columName ->
+                    when (cursor.getType(columnIndex)) {
+                        Cursor.FIELD_TYPE_BLOB -> {
+                            obj.put(columName, cursor.getBlob(columnIndex).toByteString().hex())
+                        }
+                        Cursor.FIELD_TYPE_INTEGER -> {
+                            obj.put(columName, cursor.getInt(columnIndex))
+                        }
+                        Cursor.FIELD_TYPE_FLOAT -> {
+                            obj.put(columName, cursor.getFloat(columnIndex))
+                        }
+                        else -> {
+                            obj.put(columName, cursor.getString(columnIndex))
+                        }
+                    }
+                }
+                jsonArray.put(obj)
+            } while (cursor.moveToNext())
+        }
+
+        val columnNameJsonArray = JSONArray()
+        cursor.columnNames.forEach {
+            columnNameJsonArray.put(it)
+        }
+
+        jsonObject.put("success", true)
+        jsonObject.put("data", jsonArray)
+        jsonObject.put("columnNames", columnNameJsonArray)
+        return jsonObject.toString()
+    }
+
+    private fun getDatabaseVersion(): Int {
+        val fhirDatabase = fhirEngine.javaClass.getDeclaredField("database").apply { isAccessible = true }.get(fhirEngine)
+        val resourceDatabaseImpl = fhirDatabase.javaClass.getDeclaredField("db").apply { isAccessible = true }.get(fhirDatabase)
+        val sqliteOpenHelper = resourceDatabaseImpl.javaClass.getMethod("getOpenHelper").invoke(resourceDatabaseImpl)
+        val supportSqliteDatabase = sqliteOpenHelper.javaClass.getMethod("getReadableDatabase").invoke(sqliteOpenHelper)
+        val dbVersion = supportSqliteDatabase.javaClass.getDeclaredMethod("getVersion").invoke(supportSqliteDatabase)
+        return dbVersion as Int
+    }
+
+    fun String.compress(): String {
+        val byteStream = ByteArrayOutputStream()
+        GZIPOutputStream(byteStream).bufferedWriter().use { it.write(this) }
+        return Base64.getEncoder().encodeToString(byteStream.toByteArray())
+    }
+
+    fun String.decompress(): String {
+        val compressedBytes = Base64.getDecoder().decode(this)
+        val byteArrayInputStream = ByteArrayInputStream(compressedBytes)
+        return GZIPInputStream(byteArrayInputStream).bufferedReader().use { it.readText() }
+    }
+
+    class DBHelper(context: Context?, version: Int) : SQLiteOpenHelper(context, "resources.db", null, version) {
+        override fun onCreate(db: SQLiteDatabase?) {}
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+
+    }
+
+    enum class QueryType {
+        SELECT, INSERT, UPDATE, DELETE, UNKNOWN
     }
 }
