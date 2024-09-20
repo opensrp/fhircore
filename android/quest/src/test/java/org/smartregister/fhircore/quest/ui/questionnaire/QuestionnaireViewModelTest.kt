f694d0baa2112cfd110221456d44d0e02ebac31f
Martin Ndegwa <mndegwa@ona.io>
2024-09-20 16:32:35 +0300

    Fix build

--- android/quest/src/test/java/org/smartregister/fhircore/quest/ui/questionnaire/QuestionnaireViewModelTest.kt
+++ android/quest/src/test/java/org/smartregister/fhircore/quest/ui/questionnaire/QuestionnaireViewModelTest.kt
@@ -1889,7 +1889,7 @@ class QuestionnaireViewModelTest : RobolectricTest() {
       val questionnaireViewModelInstance =
         QuestionnaireViewModel(
           defaultRepository = defaultRepository,
-          dispatcherProvider = defaultRepository.dispatcherProvider,
+          dispatcherProvider = dispatcherProvider,
           fhirCarePlanGenerator = fhirCarePlanGenerator,
           resourceDataRulesExecutor = resourceDataRulesExecutor,
           transformSupportServices = mockk(),
@@ -1898,6 +1898,7 @@ class QuestionnaireViewModelTest : RobolectricTest() {
           fhirValidatorProvider = fhirValidatorProvider,
           fhirPathDataExtractor = fhirPathDataExtractor,
           configurationRegistry = configurationRegistry,
+          knowledgeManager = knowledgeManager,
         )
       val questionnaireWithDefaultDate =
         Questionnaire().apply {
