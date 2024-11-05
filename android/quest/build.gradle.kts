692380dafb07e412acd2c8a719c2a6878582b2c8
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2024-11-05 12:14:50 +0300

    🐛 Readd the eusm mg and bi flavour

--- android/quest/build.gradle.kts
+++ android/quest/build.gradle.kts
@@ -321,12 +321,20 @@ android {
       manifestPlaceholders["appLabel"] = "PSI WFA"
     }
 
-    create("eusm") {
+    create("eusmMg") {
       dimension = "apps"
-      applicationIdSuffix = ".eusm"
-      versionNameSuffix = "-eusm"
-      manifestPlaceholders["appLabel"] = "EUSM"
+      applicationIdSuffix = ".eusmMg"
+      versionNameSuffix = "-eusmMg"
+      manifestPlaceholders["appLabel"] = "EUSM Madagascar"
     }
+
+    create("eusmBi") {
+      dimension = "apps"
+      applicationIdSuffix = ".eusmBi"
+      versionNameSuffix = "-eusmBi"
+      manifestPlaceholders["appLabel"] = "EUSM Burundi"
+    }
+
 
     create("demoEir") {
       dimension = "apps"
