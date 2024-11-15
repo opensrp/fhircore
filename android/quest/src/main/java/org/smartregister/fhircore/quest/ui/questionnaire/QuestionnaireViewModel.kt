8617cd97827b8e9a0194ea4ecf5a0b783e0bd819
Rkareko <47570855+Rkareko@users.noreply.github.com>
2024-11-15 16:30:02 +0300

    Re populate questionnaire response ID when removing unanswered questions (#3614)
    * Re populate questionnaire response ID when removing unanswered questions
    
    * Verify that populateQuestionnaire returns a questionnaire response that has an id value

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/questionnaire/QuestionnaireViewModel.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/questionnaire/QuestionnaireViewModel.kt
@@ -1167,6 +1167,7 @@ constructor(
           )
           ?.let {
             QuestionnaireResponse().apply {
+              id = it.id
               item = it.item.removeUnAnsweredItems()
               // Clearing the text prompts the SDK to re-process the content, which includes HTML
               clearText()
