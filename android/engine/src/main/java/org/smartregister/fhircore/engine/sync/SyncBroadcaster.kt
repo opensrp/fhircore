69609d605d72e1e5de046a7b5ade3326c123f766
Martin Ndegwa <mndegwa@ona.io>
2024-11-18 09:33:20 +0300

    Update sync jobs configuration

--- android/engine/src/main/java/org/smartregister/fhircore/engine/sync/SyncBroadcaster.kt
+++ android/engine/src/main/java/org/smartregister/fhircore/engine/sync/SyncBroadcaster.kt
@@ -17,15 +17,19 @@
 package org.smartregister.fhircore.engine.sync
 
 import android.content.Context
+import androidx.work.BackoffPolicy
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import com.google.android.fhir.FhirEngine
+import com.google.android.fhir.sync.BackoffCriteria
 import com.google.android.fhir.sync.CurrentSyncJobStatus
 import com.google.android.fhir.sync.PeriodicSyncConfiguration
 import com.google.android.fhir.sync.PeriodicSyncJobStatus
 import com.google.android.fhir.sync.RepeatInterval
+import com.google.android.fhir.sync.RetryConfiguration
 import com.google.android.fhir.sync.Sync
 import com.google.android.fhir.sync.SyncJobStatus
 import com.google.android.fhir.sync.download.ResourceParamsBasedDownloadWorkManager
@@ -76,6 +80,12 @@ constructor(
         .setConstraints(
           Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build(),
         )
+        .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+        .setBackoffCriteria(
+          BackoffPolicy.LINEAR,
+          10,
+          TimeUnit.SECONDS,
+        )
         .build(),
     )
   }
@@ -94,6 +104,16 @@ constructor(
             syncConstraints =
               Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build(),
             repeat = RepeatInterval(interval = interval, timeUnit = TimeUnit.MINUTES),
+            retryConfiguration =
+              RetryConfiguration(
+                backoffCriteria =
+                  BackoffCriteria(
+                    backoffDelay = 10,
+                    timeUnit = TimeUnit.SECONDS,
+                    backoffPolicy = BackoffPolicy.EXPONENTIAL,
+                  ),
+                maxRetries = 3,
+              ),
           ),
       )
       .handlePeriodicSyncJobStatus(this)
