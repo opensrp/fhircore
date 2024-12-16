ce5a557901cab03c280c56c8f1512d9769399938
Elly Kitoto <junkmailstoelly@gmail.com>
2024-12-16 13:48:02 +0300

    Disable apk minification (#3660)
    * Disable apk minification
    
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>
    
    * Format code
    
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>
    
    ---------
    
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/quest/build.gradle.kts
+++ android/quest/build.gradle.kts
@@ -116,8 +116,8 @@ android {
     }
 
     getByName("release") {
-      isMinifyEnabled = true
-      isShrinkResources = true
+      isMinifyEnabled = false
+      isShrinkResources = false
       proguardFiles(
         getDefaultProguardFile("proguard-android-optimize.txt"),
         "proguard-rules.pro",
