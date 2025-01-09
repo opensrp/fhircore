106b04c4f51daee6a1d86f34e733b9303e3dbbd6
pld <peter@ona.io>
2025-01-09 14:19:49 -0500

    turn class into interface, do not override autogen getter

--- android/quest/src/main/java/org/smartregister/fhircore/quest/audiointerface/models/LlmModel.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/audiointerface/models/LlmModel.kt
@@ -18,15 +18,8 @@ package org.smartregister.fhircore.quest.audiointerface.models
 
 import com.google.ai.client.generativeai.GenerativeModel
 
-abstract class LlmModel {
-  abstract val model: GenerativeModel
+interface LlmModel {
+  val model: GenerativeModel
 
-  /**
-   * Returns the configured GenerativeModel instance.
-   *
-   * @return The GenerativeModel instance.
-   */
-  fun getModel(): GenerativeModel = model
-
-  abstract suspend fun generateContent(prompt: String): String?
+  suspend fun generateContent(prompt: String): String?
 }
