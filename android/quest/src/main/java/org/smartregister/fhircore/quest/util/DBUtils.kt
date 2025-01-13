f32f9b3444f96a7bfca6c7b8cfbdcd19a501a168
Simon Njoroge <snjoroge@ona.io>
2025-01-13 20:02:46 +0300

    Run spotlessApply

--- android/quest/src/main/java/org/smartregister/fhircore/quest/util/DBUtils.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/util/DBUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 Ona Systems, Inc
+ * Copyright 2021-2024 Ona Systems, Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,108 +36,108 @@ import timber.log.Timber
 
 object DBUtils {
 
-    private const val ANDROID_KEYSTORE_NAME = "AndroidKeyStore"
+  private const val ANDROID_KEYSTORE_NAME = "AndroidKeyStore"
 
-    private const val MESSAGE_TO_BE_SIGNED = "Android FHIR SDK rocks!"
+  private const val MESSAGE_TO_BE_SIGNED = "Android FHIR SDK rocks!"
 
-    @Synchronized
-    fun getEncryptionPassphrase(keyName: String): ByteArray {
-        val keyStore =
-            try {
-                KeyStore.getInstance(ANDROID_KEYSTORE_NAME)
-            } catch (exception: KeyStoreException) {
-                throw exception.databaseEncryptionException
-            }
+  @Synchronized
+  fun getEncryptionPassphrase(keyName: String): ByteArray {
+    val keyStore =
+      try {
+        KeyStore.getInstance(ANDROID_KEYSTORE_NAME)
+      } catch (exception: KeyStoreException) {
+        throw exception.databaseEncryptionException
+      }
 
-        val hmac =
-            try {
-                Mac.getInstance(KeyProperties.KEY_ALGORITHM_HMAC_SHA256)
-            } catch (exception: NoSuchAlgorithmException) {
-                throw DatabaseEncryptionException(
-                    exception,
-                    DatabaseEncryptionException.DatabaseEncryptionErrorCode.UNSUPPORTED
-                )
-            }
+    val hmac =
+      try {
+        Mac.getInstance(KeyProperties.KEY_ALGORITHM_HMAC_SHA256)
+      } catch (exception: NoSuchAlgorithmException) {
+        throw DatabaseEncryptionException(
+          exception,
+          DatabaseEncryptionException.DatabaseEncryptionErrorCode.UNSUPPORTED,
+        )
+      }
 
-        try {
-            keyStore.load(/* param = */ null)
-            val signingKey: SecretKey =
-                keyStore.getKey(keyName, /* password= */ null) as SecretKey?
-                    ?: run {
-                        val keyGenerator =
-                            KeyGenerator.getInstance(
-                                KeyProperties.KEY_ALGORITHM_HMAC_SHA256,
-                                ANDROID_KEYSTORE_NAME
-                            )
-                        keyGenerator.init(
-                            KeyGenParameterSpec.Builder(keyName, KeyProperties.PURPOSE_SIGN).build()
-                        )
-                        keyGenerator.generateKey()
-                    }
-            hmac.init(signingKey)
-            return hmac.doFinal(MESSAGE_TO_BE_SIGNED.toByteArray(StandardCharsets.UTF_8))
-        } catch (exception: KeyStoreException) {
-            throw exception.databaseEncryptionException
-        }
-    }
-
-    fun copyUnencryptedDb(databaseFile: File, backupFile: File): Boolean {
-        return try {
-            val src = FileInputStream(databaseFile).channel
-            val dst = FileOutputStream(backupFile).channel
-            dst.transferFrom(src, 0, src.size())
-            src.close()
-            dst.close()
-
-            true
-        } catch (e: FileNotFoundException) {
-            Timber.e(e, "File does not exist")
-            false
-        } catch (e: SecurityException) {
-            Timber.e(e, "No permissions to delete file")
-            false
-        }
-    }
-
-    fun decryptDb(databaseFile: File, backupFile: File, passphrase: ByteArray?): Boolean {
-        if (backupFile.parentFile.canWrite() && databaseFile.exists()) {
-            val encryptedDb =
-                SQLiteDatabase.openDatabase(databaseFile.absolutePath, passphrase, null, 0, null, null)
-
-            // create an empty database
-            android.database.sqlite.SQLiteDatabase.openOrCreateDatabase(
-                backupFile.absolutePath,
-                null,
-                null
+    try {
+      keyStore.load(null)
+      val signingKey: SecretKey =
+        keyStore.getKey(keyName, null) as SecretKey?
+          ?: run {
+            val keyGenerator =
+              KeyGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_HMAC_SHA256,
+                ANDROID_KEYSTORE_NAME,
+              )
+            keyGenerator.init(
+              KeyGenParameterSpec.Builder(keyName, KeyProperties.PURPOSE_SIGN).build(),
             )
-                .close()
-
-            val statement = encryptedDb.compileStatement("ATTACH DATABASE ? AS plaintext KEY ''")
-
-            statement.bindString(1, backupFile.absolutePath)
-            statement.execute()
-            encryptedDb.rawExecSQL("SELECT sqlcipher_export('plaintext')")
-            encryptedDb.rawExecSQL("DETACH DATABASE plaintext")
-
-            val version = encryptedDb.version
-
-            statement.close()
-            encryptedDb.close()
-
-            val plaintTextDb =
-                android.database.sqlite.SQLiteDatabase.openOrCreateDatabase(
-                    backupFile.absolutePath,
-                    null,
-                    null
-                )
-
-            plaintTextDb.version = version
-            plaintTextDb.close()
-
-            return true
-        } else {
-            Timber.e(databaseFile.absolutePath + " not found")
-            return false
-        }
+            keyGenerator.generateKey()
+          }
+      hmac.init(signingKey)
+      return hmac.doFinal(MESSAGE_TO_BE_SIGNED.toByteArray(StandardCharsets.UTF_8))
+    } catch (exception: KeyStoreException) {
+      throw exception.databaseEncryptionException
     }
+  }
+
+  fun copyUnencryptedDb(databaseFile: File, backupFile: File): Boolean {
+    return try {
+      val src = FileInputStream(databaseFile).channel
+      val dst = FileOutputStream(backupFile).channel
+      dst.transferFrom(src, 0, src.size())
+      src.close()
+      dst.close()
+
+      true
+    } catch (e: FileNotFoundException) {
+      Timber.e(e, "File does not exist")
+      false
+    } catch (e: SecurityException) {
+      Timber.e(e, "No permissions to delete file")
+      false
+    }
+  }
+
+  fun decryptDb(databaseFile: File, backupFile: File, passphrase: ByteArray?): Boolean {
+    if (backupFile.parentFile.canWrite() && databaseFile.exists()) {
+      val encryptedDb =
+        SQLiteDatabase.openDatabase(databaseFile.absolutePath, passphrase, null, 0, null, null)
+
+      // create an empty database
+      android.database.sqlite.SQLiteDatabase.openOrCreateDatabase(
+          backupFile.absolutePath,
+          null,
+          null,
+        )
+        .close()
+
+      val statement = encryptedDb.compileStatement("ATTACH DATABASE ? AS plaintext KEY ''")
+
+      statement.bindString(1, backupFile.absolutePath)
+      statement.execute()
+      encryptedDb.rawExecSQL("SELECT sqlcipher_export('plaintext')")
+      encryptedDb.rawExecSQL("DETACH DATABASE plaintext")
+
+      val version = encryptedDb.version
+
+      statement.close()
+      encryptedDb.close()
+
+      val plaintTextDb =
+        android.database.sqlite.SQLiteDatabase.openOrCreateDatabase(
+          backupFile.absolutePath,
+          null,
+          null,
+        )
+
+      plaintTextDb.version = version
+      plaintTextDb.close()
+
+      return true
+    } else {
+      Timber.e(databaseFile.absolutePath + " not found")
+      return false
+    }
+  }
 }
