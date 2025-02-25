c61c9b6e42ade0c156a91eb3a3a612d979082b19
Martin Ndegwa <mndegwa@ona.io>
2025-02-25 13:34:55 +0300

    Upgrade build configuration

--- android/buildSrc/src/main/kotlin/BuildConfigs.kt
+++ android/buildSrc/src/main/kotlin/BuildConfigs.kt
@@ -1,12 +1,12 @@
 object BuildConfigs {
     const val minSdk = 26
-    const val compileSdk = 34
-    const val targetSdk = 34
+    const val compileSdk = 35
+    const val targetSdk = 35
     const val versionCode = 15
     const val versionName = "2.1.2"
     const val applicationId = "org.smartregister.opensrp"
     const val jvmToolchain = 17
-    const val kotlinCompilerExtensionVersion = "1.5.14"
+    const val kotlinCompilerExtensionVersion = "1.5.15"
     const val jacocoVersion ="0.8.11"
     const val ktLintVersion = "0.49.0"
     const val enableUnitTestCoverage = true
