c38e8edf488cfc98b24e233d3398aa339454222c
Elly Kitoto <junkmailstoelly@gmail.com>
2025-03-11 11:49:56 +0300

    Fix spotless
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- android/engine/src/test/java/org/smartregister/fhircore/engine/rulesengine/RulesFactoryTest.kt
+++ android/engine/src/test/java/org/smartregister/fhircore/engine/rulesengine/RulesFactoryTest.kt
@@ -1372,7 +1372,7 @@ class RulesFactoryTest : RobolectricTest() {
     val logicalId = "Group/0acda8c9-3fa3-40ae-abcd-7d1fba7098b4/_history/2"
     Assert.assertEquals(
       "0acda8c9-3fa3-40ae-abcd-7d1fba7098b4",
-      rulesEngineService.extractLogicalIdUuid(logicalId)
+      rulesEngineService.extractLogicalIdUuid(logicalId),
     )
     val otherLogicalId = "Group/0acda8c9-3fa3-40ae-abcd-7d1fba7098b4"
     Assert.assertEquals(
