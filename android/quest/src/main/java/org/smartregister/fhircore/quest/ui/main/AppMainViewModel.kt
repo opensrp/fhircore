9af87ceae8b569e65b7d6932971be8b4aae08ccd
Rkareko <rkareko@ona.io>
2025-03-20 10:27:28 +0300

    Fix Periodic sync jobs scheduling failure

--- android/quest/src/main/java/org/smartregister/fhircore/quest/ui/main/AppMainViewModel.kt
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/ui/main/AppMainViewModel.kt
@@ -420,31 +420,6 @@ constructor(
     completed * 100 / if (total > 0) total else 1
 
   suspend fun schedulePeriodicJobs(context: Context) {
-    if (!BuildConfig.SKIP_AUTHENTICATION) {
-      if (context.isDeviceOnline()) {
-        // Do not schedule sync until location selected when strategy is RelatedEntityLocation
-        // Use applicationConfiguration.usePractitionerAssignedLocationOnSync to identify
-        // if we need to trigger sync based on assigned locations or not
-        when {
-          applicationConfiguration.syncStrategy.contains(SyncStrategy.RelatedEntityLocation) -> {
-            if (
-              applicationConfiguration.usePractitionerAssignedLocationOnSync ||
-                context
-                  .retrieveRelatedEntitySyncLocationState(MultiSelectViewAction.SYNC_DATA)
-                  .isNotEmpty()
-            ) {
-              syncBroadcaster.schedulePeriodicSync(applicationConfiguration.syncInterval)
-            }
-          }
-          else -> syncBroadcaster.schedulePeriodicSync(applicationConfiguration.syncInterval)
-        }
-      } else {
-        withContext(dispatcherProvider.main()) {
-          context.showToast(context.getString(R.string.sync_failed), Toast.LENGTH_LONG)
-        }
-      }
-    }
-
     workManager.run {
       schedulePeriodically<FhirTaskStatusUpdateWorker>(
         workId = FhirTaskStatusUpdateWorker.WORK_ID,
@@ -479,6 +454,31 @@ constructor(
               ),
             initialDelay = INITIAL_DELAY,
           )
+        }
+      }
+    }
+
+    if (!BuildConfig.SKIP_AUTHENTICATION) {
+      if (context.isDeviceOnline()) {
+        // Do not schedule sync until location selected when strategy is RelatedEntityLocation
+        // Use applicationConfiguration.usePractitionerAssignedLocationOnSync to identify
+        // if we need to trigger sync based on assigned locations or not
+        when {
+          applicationConfiguration.syncStrategy.contains(SyncStrategy.RelatedEntityLocation) -> {
+            if (
+              applicationConfiguration.usePractitionerAssignedLocationOnSync ||
+                context
+                  .retrieveRelatedEntitySyncLocationState(MultiSelectViewAction.SYNC_DATA)
+                  .isNotEmpty()
+            ) {
+              syncBroadcaster.schedulePeriodicSync(applicationConfiguration.syncInterval)
+            }
+          }
+          else -> syncBroadcaster.schedulePeriodicSync(applicationConfiguration.syncInterval)
+        }
+      } else {
+        withContext(dispatcherProvider.main()) {
+          context.showToast(context.getString(R.string.sync_failed), Toast.LENGTH_LONG)
         }
       }
     }
