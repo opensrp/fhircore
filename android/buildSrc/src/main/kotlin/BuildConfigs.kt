cd5d1e28ab66dc2027e7ead6d74a335a24537137
Rkareko <47570855+Rkareko@users.noreply.github.com>
2025-03-21 15:15:02 +0300

    Fix Periodic sync jobs scheduling failure (#3733)
    * Fix Periodic sync jobs scheduling failure
    
    * Bump up version
    
    * Wrap schedulePeriodicSync in a viewodel scope

--- android/buildSrc/src/main/kotlin/BuildConfigs.kt
+++ android/buildSrc/src/main/kotlin/BuildConfigs.kt
@@ -2,8 +2,8 @@ object BuildConfigs {
     const val minSdk = 26
     const val compileSdk = 34
     const val targetSdk = 34
-    const val versionCode = 15
-    const val versionName = "2.1.2"
+    const val versionCode = 16
+    const val versionName = "2.1.3"
     const val applicationId = "org.smartregister.opensrp"
     const val jvmToolchain = 17
     const val kotlinCompilerExtensionVersion = "1.5.14"
