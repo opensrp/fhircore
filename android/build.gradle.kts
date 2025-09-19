be3f766a541b94b47ee54cc613d8e02a063a3ae2
Rkareko <rkareko@ona.io>
2025-09-19 10:26:19 +0300

    Comment out Spring IO plugins release url

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -46,7 +46,7 @@ allprojects {
     mavenCentral()
     maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
-    maven(url = "https://repo.spring.io/plugins-release")
+    //maven(url = "https://repo.spring.io/plugins-release")
     maven(url = "https://repository.liferay.com/nexus/content/repositories/public")
     maven(url = "https://central.sonatype.com/repository/maven-snapshots")
     apply(plugin = "org.owasp.dependencycheck")
