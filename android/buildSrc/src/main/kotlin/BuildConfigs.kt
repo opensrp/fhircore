63c251fade17a86f6afa7a0f741733a904f96287
Allan Onchuru <16164649+allan-on@users.noreply.github.com>
2026-03-19 12:33:30 +0300

    :arrow_up: Update app version to 2.2.1

--- android/buildSrc/src/main/kotlin/BuildConfigs.kt
+++ android/buildSrc/src/main/kotlin/BuildConfigs.kt
@@ -2,8 +2,8 @@ object BuildConfigs {
     const val minSdk = 26
     const val compileSdk = 35
     const val targetSdk = 35
-    const val versionCode = 17
-    const val versionName = "2.2.0"
+    const val versionCode = 18
+    const val versionName = "2.2.1"
     const val applicationId = "org.smartregister.opensrp"
     const val jvmToolchain = 17
     const val kotlinCompilerExtensionVersion = "1.5.14"
